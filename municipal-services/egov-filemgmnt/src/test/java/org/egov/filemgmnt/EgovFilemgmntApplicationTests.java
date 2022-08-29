d81efadd4b101c94575851cdec02b69dea39aa59
priyamalu <krishnapriya.ikm@kerala.gov.in>
2022-08-29 23:22:51 +0530

    Initial file management version

---
+++ municipal-services/egov-filemgmnt/src/test/java/org/egov/filemgmnt/EgovFilemgmntApplicationTests.java
@@ -0,0 +1,13 @@
+package org.egov.filemgmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgovFilemgmntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
