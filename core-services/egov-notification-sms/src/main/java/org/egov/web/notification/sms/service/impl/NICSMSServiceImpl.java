082b8d656857d2ab9064b1b84f622d822837065c
Vipin Madhummal <vipinmadhummal@Vipins-MacBook-Pro.local>
2022-09-23 09:46:47 +0530

    comitting the SMS cert change

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/NICSMSServiceImpl.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/NICSMSServiceImpl.java
@@ -55,7 +55,7 @@ public class NICSMSServiceImpl extends BaseSMSService {
                 KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
                 //File file = new File(System.getenv("JAVA_HOME")+"/lib/security/cacerts");
                 //File file = new File(getClass().getClassLoader().getResource("smsgwsmsgovin-Mar22.cer").getFile());
-                Resource resource = (Resource) new ClassPathResource("smsgwsmsgovin-Mar22.cer");
+                Resource resource = (Resource) new ClassPathResource("smsgwsmsgovin-sep22.cer");
                 InputStream is = resource.getInputStream(); 
                 //InputStream is = new FileInputStream(file);
                 trustStore.load(is, "changeit".toCharArray());
