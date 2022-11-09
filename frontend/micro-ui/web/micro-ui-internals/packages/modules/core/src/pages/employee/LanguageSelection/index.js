84f2c3c691dcd5e68767777f6d09d450a00aa773
anzarthoughtripples <anzar@thoughtripples>
2022-11-09 12:11:33 +0530

    updates

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/LanguageSelection/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/LanguageSelection/index.js
@@ -58,7 +58,7 @@ const LanguageSelection = () => {
         </div>
         <SubmitBar style={{ width: "35%", borderRadius: ".25rem", fontSize: "14px" }} label={t(`CORE_COMMON_CONTINUE`)} onSubmit={handleSubmit} />
       </Card>
-      <div style={{ width: '100%',  position: "absolute", bottom: 0 }}>
+      {/* <div style={{ width: '100%',  position: "absolute", bottom: 0 }}>
         <div style={{ display: 'flex', justifyContent: 'center', color:"white" }}>
           <img style={{ cursor: "pointer", display: "inline-flex", height: '1.4em' }} alt={"Powered by DIGIT"} src={`${sourceUrl}/digit-footer-bw.png`} onError={"this.src='./../digit-footer.png'"} onClick={() => {
             window.open('https://www.digit.org/', '_blank').focus();
@@ -68,7 +68,7 @@ const LanguageSelection = () => {
           <span style={{ margin: "0 10px" }}>|</span>
           <a style={{ cursor: "pointer", fontSize: "16px", fontWeight: "400"}} href={pdfUrl} target='_blank'>UPYOG License</a>
         </div>
-      </div>
+      </div> */}
     </Background>
   );
 };
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/Login/login.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/Login/login.js
@@ -179,7 +179,7 @@ const Login = ({ config: propsConfig, t, isDisabled }) => {
         {/* <Header /> */}
       </FormComposer>
       {showToast && <Toast error={true} label={t(showToast)} onClose={closeToast} />}
-      <div style={{ width: '100%', position: 'fixed', bottom: 0 }}>
+      {/* <div style={{ width: '100%', position: 'fixed', bottom: 0 }}>
         <div style={{ display: 'flex', justifyContent: 'center', color:"white" }}>
           <img style={{ cursor: "pointer", display: "inline-flex", height: '1.4em' }} alt={"Powered by DIGIT"} src={`${sourceUrl}/digit-footer-bw.png`} onError={"this.src='./../digit-footer.png'"} onClick={() => {
             window.open('https://www.digit.org/', '_blank').focus();
@@ -189,7 +189,7 @@ const Login = ({ config: propsConfig, t, isDisabled }) => {
           <span style={{ margin: "0 10px" }}>|</span>
           <a style={{ cursor: "pointer", fontSize: "16px", fontWeight: "400"}} href={pdfUrl} target='_blank'>UPYOG License</a>
         </div>
-      </div>
+      </div> */}
     </Background>
   );
 };
