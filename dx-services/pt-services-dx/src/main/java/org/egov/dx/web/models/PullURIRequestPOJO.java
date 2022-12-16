c5bfda2011c61ee717dbde861ad9d7e41528a30f
deepikaarora88 <deepika.arora@pwc.com>
2022-12-16 13:38:01 +0000

    mapped xml request body for DigiLocker API

---
+++ dx-services/pt-services-dx/src/main/java/org/egov/dx/web/models/PullURIRequestPOJO.java
@@ -0,0 +1,24 @@
+package org.egov.dx.web.models;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@XStreamAlias("PullURIRequest")
+public class PullURIRequestPOJO {
+
+    
+    @XStreamAlias("DocDetails")
+    private DocDetails docDetails;
+
+     
+}
