53d4430215dcf5b0712db2979d45b10990e9009e
lekshmybl <lekshmybl.ikm@kerala.gov.in>
2023-01-12 18:13:30 +0530

    register all

--- municipal-services/birth-services/src/main/java/org/ksmart/birth/birthapplication/service/RegistryRequestService.java
+++ municipal-services/birth-services/src/main/java/org/ksmart/birth/birthapplication/service/RegistryRequestService.java
@@ -3,10 +3,7 @@ package org.ksmart.birth.birthapplication.service;
 import lombok.extern.slf4j.Slf4j;
 import org.ksmart.birth.birthapplication.model.birth.BirthDetailsRequest;
 import org.ksmart.birth.birthapplication.model.birth.BirthPlace;
-import org.ksmart.birth.birthregistry.model.BirthPdfRegisterRequest;
-import org.ksmart.birth.birthregistry.model.RegisterBirthDetail;
-import org.ksmart.birth.birthregistry.model.RegisterBirthDetailsRequest;
-import org.ksmart.birth.birthregistry.model.RegisterBirthPlace;
+import org.ksmart.birth.birthregistry.model.*;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -50,18 +47,94 @@ public class RegistryRequestService {
         registerBirthDetail.get(0).setRegistrationStatus("ACTIVE");
         registerBirthDetail.get(0).setRegistrationDate(requestBirthReq.getBirthDetails().get(0).getRegistrationDate());
         registerBirthDetail.get(0).setRegisterBirthPlace(createBirthPlace(requestBirthReq));
-
-
         request.setRequestInfo(requestBirthReq.getRequestInfo());
         request.setRegisterBirthDetails(registerBirthDetail);
         return  request;
     }
-
     private RegisterBirthPlace createBirthPlace(BirthDetailsRequest requestBirthReq) {
         RegisterBirthPlace registerBirthPlace = new RegisterBirthPlace();
         registerBirthPlace.setPlaceOfBirthId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getPlaceOfBirthId());
-
+        registerBirthPlace.setHospitalId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHospitalId());
+        registerBirthPlace.setVehicleTypeId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleTypeId());
+        registerBirthPlace.setVehicleRegistrationNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleRegistrationNo());
+        registerBirthPlace.setVehicleFromEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleFromEn());
+        registerBirthPlace.setVehicleToEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleToEn());
+        registerBirthPlace.setVehicleFromMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleFromMl());
+        registerBirthPlace.setVehicleToMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleToMl());
+        registerBirthPlace.setVehicleOtherEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleOtherEn());
+        registerBirthPlace.setVehicleOtherMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleOtherMl());
+        registerBirthPlace.setVehicleAdmitHospitalEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleAdmitHospitalEn());
+        registerBirthPlace.setVehicleAdmitHospitalMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getVehicleAdmitHospitalMl());
+        registerBirthPlace.setPublicPlaceId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getPublicPlaceId());
+        registerBirthPlace.setHoHouseHolderEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoHouseHolderEn());
+        registerBirthPlace.setHoHouseHolderMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoHouseHolderMl());
+        registerBirthPlace.setHoBuildingNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoBuildingNo());
+        registerBirthPlace.setHoResAssoNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoResAssoNo());
+        registerBirthPlace.setHoHousenNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoHousenNo());
+        registerBirthPlace.setHouseNameEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHouseNameEn());
+        registerBirthPlace.setHouseNameMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHouseNameMl());
+        registerBirthPlace.setHoLocalityEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoLocalityEn());
+        registerBirthPlace.setHoLocalityMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoLocalityMl());
+        registerBirthPlace.setHoVillageId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoVillageId());
+        registerBirthPlace.setHoTalukId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoTalukId());
+        registerBirthPlace.setHoDistrictId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoDistrictId());
+        registerBirthPlace.setHoCityEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoCityEn());
+        registerBirthPlace.setHoCityMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoCityMl());
+        registerBirthPlace.setHoStateId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoStateId());
+        registerBirthPlace.setHoPoId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoPoId());
+        registerBirthPlace.setHoPinNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoPinNo());
+        registerBirthPlace.setHoCountryId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getHoCountryId());
+        registerBirthPlace.setWardId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getWardId());
+        registerBirthPlace.setOthDetailsEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getOthDetailsEn());
+        registerBirthPlace.setOthDetailsMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getOthDetailsMl());
+        registerBirthPlace.setInstitutionTypeId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInstitutionTypeId());
+        registerBirthPlace.setInstitutionId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInstitutionId());
+        registerBirthPlace.setAuthOfficerId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getAuthOfficerId());
+        registerBirthPlace.setAuthOfficerDesigId(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getAuthOfficerDesigId());
+        registerBirthPlace.setOthAuthOfficerName(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getOthAuthOfficerName());
+        registerBirthPlace.setOthAuthOfficerDesig(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getOthAuthOfficerDesig());
+        registerBirthPlace.setInformantsNameEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInformantsNameEn());
+        registerBirthPlace.setInformantsNameMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInformantsNameMl());
+        registerBirthPlace.setInformantsAddressEn(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInformantsAddressEn());
+        registerBirthPlace.setInformantsAddressMl(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInformantsAddressMl());
+        registerBirthPlace.setInformantsMobileNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInformantsMobileNo());
+        registerBirthPlace.setInformantsAadhaarNo(requestBirthReq.getBirthDetails().get(0).getBirthPlace().getInformantsAadhaarNo());
         return registerBirthPlace;
+    }
+
+    private RegisterBirthMotherInfo createMotherInfor(BirthDetailsRequest requestBirthReq) {
+        RegisterBirthMotherInfo registerBirthMotherInfo = new RegisterBirthMotherInfo();
+        registerBirthMotherInfo.setFirstNameEn(requestBirthReq.getBirthDetails().get(0).getBirthMotherInfo().getFirstNameEn());
+
+        return registerBirthMotherInfo;
+    }
+
+    private RegisterBirthFatherInfo createFatherInfor(BirthDetailsRequest requestBirthReq) {
+        RegisterBirthFatherInfo registerBirthFatherInfo = new RegisterBirthFatherInfo();
+        registerBirthFatherInfo.setFirstNameEn(requestBirthReq.getBirthDetails().get(0).getBirthFatherInfo().getFirstNameEn());
+
+        return registerBirthFatherInfo;
+    }
+
+    private RegisterBirthPermanentAddress createRegisterBirthPermanentAddress(BirthDetailsRequest requestBirthReq) {
+        RegisterBirthPermanentAddress registerBirthPermanentAddress = new RegisterBirthPermanentAddress();
+        registerBirthPermanentAddress.setPermanentAddress(requestBirthReq.getBirthDetails().get(0).getBirthPermanentAddress().getHouseNo());
+
+        return registerBirthPermanentAddress;
+    }
+
+    private RegisterBirthPresentAddress createRegisterBirthPresentAddress(BirthDetailsRequest requestBirthReq) {
+        RegisterBirthPresentAddress registerBirthPresentAddress = new RegisterBirthPresentAddress();
+        registerBirthPresentAddress.setPresentAddress(requestBirthReq.getBirthDetails().get(0).getBirthPresentAddress().getHouseNameEn());
+
+        return registerBirthPresentAddress;
+    }
+
+    private RegisterBirthStatiticalInformation createRegisterBirthStatiticalInformation(BirthDetailsRequest requestBirthReq) {
+        RegisterBirthStatiticalInformation registerBirthStatiticalInformation = new RegisterBirthStatiticalInformation();
+        registerBirthStatiticalInformation.setDeliveryMethod(requestBirthReq.getBirthDetails().get(0).getBirthStatisticalInformation().getDeliveryMethod());
+
+        return registerBirthStatiticalInformation;
     }
 }
 
