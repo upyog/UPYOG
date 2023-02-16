bf9f1691c71e93617e0c074c4e7fa12fbe372d2b
Lekshmy B L <94520731+lekshmybl@users.noreply.github.com>
2023-02-16 14:55:25 +0530

    Update V20230214152024__idgeneartion_ddl.sql

--- municipal-services/birth-services/src/main/resources/db/migration/main/V20230214152024__idgeneartion_ddl.sql
+++ municipal-services/birth-services/src/main/resources/db/migration/main/V20230214152024__idgeneartion_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE eg_idgeneration(
+CREATE TABLE eg_birth_idgeneration(
                                 idtype character varying(10),
                                 modulecode character varying(10),
                                 tenantid character varying(64),
@@ -6,7 +6,7 @@ CREATE TABLE eg_idgeneration(
                                 lastid int
 );
 
-CREATE OR REPLACE FUNCTION fn_next_id(
+CREATE OR REPLACE FUNCTION fn_next_birth_id(
   _idtype character varying(10),
   _modulecode character varying(10),
   _tenantid character varying(64),
@@ -18,14 +18,14 @@ CREATE OR REPLACE FUNCTION fn_next_id(
 AS $BODY$
 DECLARE  genid bigint;
 BEGIN
-genid := (Select lastid From eg_idgeneration where tenantid = _tenantid AND yearid = _yearid AND idtype=_idtype AND modulecode = _modulecode) ;
+genid := (Select lastid From eg_birth_idgeneration where tenantid = _tenantid AND yearid = _yearid AND idtype=_idtype AND modulecode = _modulecode) ;
 IF ( genid IS NOT NULL ) THEN
 genid :=  genid  + 1 ;
-Update eg_idgeneration SET lastid = genid
+Update eg_birth_idgeneration SET lastid = genid
 WHERE tenantid = _tenantid AND yearid = _yearid AND idtype=_idtype AND modulecode = _modulecode;
 ELSE
 genid := 1  ;
-INSERT INTO eg_idgeneration(idtype, modulecode, tenantid, yearid, lastid)
+INSERT INTO eg_birth_idgeneration(idtype, modulecode, tenantid, yearid, lastid)
 VALUES (_idtype, _modulecode, _tenantid, _yearid, genid);
 END IF ;
 RETURN  genid ;
