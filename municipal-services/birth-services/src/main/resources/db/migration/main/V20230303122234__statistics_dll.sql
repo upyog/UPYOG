7d8e6aa369826bb1320ed5f22bbe15c284e2ad5c
Lekshmy <lekshmy.bl@ikm.gov.in>
2023-03-03 12:58:10 +0530

    pdf

--- municipal-services/birth-services/src/main/resources/db/migration/main/V20230303122234__statistics_dll.sql
+++ municipal-services/birth-services/src/main/resources/db/migration/main/V20230303122234__statistics_dll.sql
@@ -3,19 +3,21 @@ ALTER TABLE eg_birth_statitical_information ALTER COLUMN duration_of_pregnancy_i
 ALTER TABLE eg_birth_statitical_information_audit ALTER COLUMN weight_of_child DROP NOT NULL;
 ALTER TABLE eg_birth_statitical_information_audit ALTER COLUMN duration_of_pregnancy_in_week DROP NOT NULL;
 
-ALTER TABLE eg_birth_present_address
+ALTER TABLE eg_register_birth_present_address
     ADD COLUMN postoffice_en character varying(1000) COLLATE pg_catalog."default",
     ADD COLUMN postoffice_ml character varying(1000) COLLATE pg_catalog."default";
 
-ALTER TABLE eg_birth_present_address_audit
+ALTER TABLE eg_register_birth_present_address_audit
     ADD COLUMN postoffice_en character varying(1000) COLLATE pg_catalog."default",
     ADD COLUMN postoffice_ml character varying(1000) COLLATE pg_catalog."default";
 
-ALTER TABLE eg_birth_permanent_address
+
+ALTER TABLE eg_register_birth_permanent_address
     ADD COLUMN postoffice_en character varying(1000) COLLATE pg_catalog."default",
     ADD COLUMN postoffice_ml character varying(1000) COLLATE pg_catalog."default";
 
-ALTER TABLE eg_birth_permanent_address_audit
+ALTER TABLE eg_register_birth_permanent_address_audit
     ADD COLUMN postoffice_en character varying(1000) COLLATE pg_catalog."default",
     ADD COLUMN postoffice_ml character varying(1000) COLLATE pg_catalog."default";
+
 
