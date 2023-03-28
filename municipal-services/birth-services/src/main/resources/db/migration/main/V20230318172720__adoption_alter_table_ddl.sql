a46ea2c9868737336589bea1154d81a70c49f446
vbvarsha <varsha.ikm@kerala.gov.in>
2023-03-28 18:56:34 +0530

    adoption

--- municipal-services/birth-services/src/main/resources/db/migration/main/V20230318172720__adoption_alter_table_ddl.sql
+++ municipal-services/birth-services/src/main/resources/db/migration/main/V20230318172720__adoption_alter_table_ddl.sql
@@ -2,8 +2,15 @@ ALTER TABLE eg_birth_details
  ADD COLUMN adopt_decree_order_no character varying(64),
  ADD COLUMN adopt_dateoforder_decree bigint,
  ADD COLUMN adopt_agency_contact_person  character varying(64) ,
- ADD COLUMN adopt_agency_contact_person_mobileno character varying(12);
-
+ ADD COLUMN adopt_agency_contact_person_mobileno character varying(12),
+ ADD COLUMN old_birth_reg_no character varying(64),
+ DROP COLUMN adopt_firstname_en,
+ DROP COLUMN adopt_firstname_ml  ,    
+ DROP COLUMN adopt_middlename_en   ,
+ DROP COLUMN adopt_middlename_ml   ,
+ DROP COLUMN adopt_lastname_en  ,
+ DROP COLUMN adopt_lastname_ml  ;
+ 
 ALTER TABLE eg_birth_details_audit
  ADD COLUMN adopt_decree_order_no character varying(64),
  ADD COLUMN adopt_dateoforder_decree bigint,
@@ -11,45 +18,16 @@ ALTER TABLE eg_birth_details_audit
  ADD COLUMN adopt_agency_contact_person_mobileno character varying(12),
  ADD COLUMN adopt_dateoforder_decree bigint,
  ADD COLUMN adopt_agency_contact_person  character varying(64) ,
- ADD COLUMN adopt_agency_contact_person_mobileno character varying(12);
- 
- ALTER TABLE eg_birth_details_audit
-
+ ADD COLUMN adopt_agency_contact_person_mobileno character varying(12),
+ ADD COLUMN old_birth_reg_no character varying(64),
  DROP COLUMN adopt_firstname_en,
  DROP COLUMN adopt_firstname_ml  ,    
  DROP COLUMN adopt_middlename_en   ,
  DROP COLUMN adopt_middlename_ml   ,
  DROP COLUMN adopt_lastname_en  ,
  DROP COLUMN adopt_lastname_ml  ;
- 
- ALTER TABLE eg_birth_details
-
- DROP COLUMN adopt_firstname_en,
- DROP COLUMN adopt_firstname_ml  ,    
- DROP COLUMN adopt_middlename_en   ,
- DROP COLUMN adopt_middlename_ml   ,
- DROP COLUMN adopt_lastname_en  ,
- DROP COLUMN adopt_lastname_ml  ;
- 
- 
- ALTER TABLE eg_birth_details_audit
-
- ADD COLUMN adopt_decree_order_no character varying(64),
- ADD COLUMN adopt_dateoforder_decree bigint,    
- ADD COLUMN adopt_agency_contact_Person  character varying(64) ,
- ADD COLUMN adopt_agency_contact_Person_mobileno character varying(12) ;
- 
- 
- ALTER TABLE eg_birth_details 
-
- ADD COLUMN old_birth_reg_no character varying(64);
- 
- ALTER TABLE eg_birth_details_audit 
-
- ADD COLUMN old_birth_reg_no character varying(64);
  
  ALTER TABLE eg_register_birth_details 
-
  ADD COLUMN    old_birth_reg_no character varying(64),
  ADD COLUMN    adopt_deed_order_no character varying(64),
  ADD COLUMN    adopt_dateoforder_deed bigint,
@@ -64,7 +42,6 @@ ALTER TABLE eg_birth_details_audit
 	
  
  ALTER TABLE eg_register_birth_details_audit 
-
  ADD COLUMN    old_birth_reg_no character varying(64),
  ADD COLUMN    adopt_deed_order_no character varying(64),
  ADD COLUMN    adopt_dateoforder_deed bigint,
