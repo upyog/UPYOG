e565b202bc8d34e6722409d9a032aa324f661c42
binilp <binilp@ikm.gov.in>
2023-04-16 01:12:27 +0530

    added background image and changed font file name for TimesItalic

--- core-services/pdf-service/src/EnvironmentVariables.js
+++ core-services/pdf-service/src/EnvironmentVariables.js
@@ -6,7 +6,7 @@ const envVariables = {
     process.env.EGOV_LOCALISATION_SEARCH || "/localization/messages/v2/_search",
   EGOV_FILESTORE_SERVICE_HOST:
     process.env.EGOV_FILESTORE_SERVICE_HOST || "https://ksmart-dev.lsgkerala.gov.in",
-  SERVER_PORT: process.env.SERVER_PORT || 8033,
+  SERVER_PORT: process.env.SERVER_PORT || 8080,
 
   KAFKA_BROKER_HOST: process.env.KAFKA_BROKER_HOST || "localhost:9092",
   KAFKA_CREATE_JOB_TOPIC:
@@ -17,14 +17,14 @@ const envVariables = {
   DB_USER: process.env.DB_USER || "postgres",
   DB_PASSWORD: process.env.DB_PASSWORD || "ikm",
   DB_HOST: process.env.DB_HOST || "localhost",
-  DB_NAME: process.env.DB_NAME || "birthdb2",
+  DB_NAME: process.env.DB_NAME || "PdfGen",
   DB_PORT: process.env.DB_PORT || 5432,
   EGOV_EXTERNAL_HOST: process.env.EGOV_EXTERNAL_HOST || "https://ksmart-dev.lsgkerala.gov.in/" ,
   DEFAULT_LOCALISATION_LOCALE:
     process.env.DEFAULT_LOCALISATION_LOCALE || "en_IN",
     DEFAULT_LOCALISATION_TENANT:
     process.env.DEFAULT_LOCALISATION_TENANT || "kl",
-    DATA_CONFIG_URLS: process.env.DATA_CONFIG_URLS || "file:///home/lekshmy/Documents/BIRTH-KSMART/KSMART_BIRTH_NEW/KSMART/municipal-services/birth-services/pdf/data-config/birth-certificate-pdf.json",
-    FORMAT_CONFIG_URLS: process.env.FORMAT_CONFIG_URLS || "file:///home/lekshmy/Documents/BIRTH-KSMART/KSMART_BIRTH_NEW/KSMART/municipal-services/birth-services/pdf/format-config/birth-certificate.json"
+    DATA_CONFIG_URLS: process.env.DATA_CONFIG_URLS || "file:///home/hp/ikm/KSMARTConfigs/ksmart-configs/configs/pdf-service/data-config/marriage-certificate.json",
+    FORMAT_CONFIG_URLS: process.env.FORMAT_CONFIG_URLS || "file:///home/hp/ikm/KSMARTConfigs/ksmart-configs/configs/pdf-service/format-config/marriage-certificate.json"
 };
 export default envVariables;
