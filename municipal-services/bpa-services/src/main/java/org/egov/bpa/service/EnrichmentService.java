ce1456e135bddad57c06e1f1a9965c7ffda542b3
vinothrallapalli-egov <vinoth.r@egovernments.org>
2023-04-21 14:24:25 +0530

    Add loggers

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/service/EnrichmentService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/service/EnrichmentService.java
@@ -250,8 +250,8 @@ public class EnrichmentService {
 				Object hgt = context.read("edcrDetail[0].planDetail.blocks[0].building.buildingHeight");
 				log.info("Plot area type::::"+plot.getClass());
 				log.info("Building height type::::"+hgt.getClass());
-				log.info("Plot area------>>>>>>>"+ (Double) plot);
-				Double	plotArea = context.read("edcrDetail[0].planDetail.planInformation.plotArea");
+				log.info("Plot area------>>>>>>>"+ String.valueOf(context.read("edcrDetail[0].planDetail.planInformation.plotArea")));
+				Double	plotArea = Double.valueOf(String.valueOf(context.read("edcrDetail[0].planDetail.planInformation.plotArea")));
 				Double	buildingHeight = context.read("edcrDetail[0].planDetail.blocks[0].building.buildingHeight");
 
 			
