6f399255d5101cdd32bc6cf0a218fd52891c2d8e
hinamakhija-eGov <hina.makhija@egovernments.org>
2023-05-09 11:41:54 +0530

    Added Space acceptance in payerName regex

--- business-services/collection-services/src/main/java/org/egov/collection/model/Payment.java
+++ business-services/collection-services/src/main/java/org/egov/collection/model/Payment.java
@@ -93,7 +93,7 @@ public class Payment {
 
     @SafeHtml
     @Size(max=128)
-    @Pattern(regexp = "^[a-zA-Z]+(([_\\-'`\\. ][a-zA-Z ])?[a-zA-Z]*)*$", message = "Invalid name. Only alphabets and special characters -, ',`, ., _")
+    @Pattern(regexp = "^[a-zA-Z ]+(([_\\-'`\\. ][a-zA-Z ])?[a-zA-Z]*)*$", message = "Invalid name. Only alphabets and special characters -, ',`, ., _")
     @JsonProperty("payerName")
     private String payerName = null;
 
