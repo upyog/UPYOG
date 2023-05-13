4959681d907ace3583d63fd6ff2e14193e1e773a
vbvarsha <varsha.ikm@kerala.gov.in>
2023-05-13 16:49:52 +0530

    birth

--- municipal-services/birth-services/src/main/java/org/ksmart/birth/common/services/PaymentUpdateService.java
+++ municipal-services/birth-services/src/main/java/org/ksmart/birth/common/services/PaymentUpdateService.java
@@ -116,8 +116,7 @@ public class PaymentUpdateService {
 				commonBirth.setIsPaymentSuccess(true);			
 				
 			}
-			 System.out.println("common req "+commonpay.get(0).getApplicationStatus());
-			 System.out.println("common req "+commonpay.get(0).getIsPaymentSuccess());
+		 
 			CommonPayRequest paymentReq =CommonPayRequest.builder().requestInfo(requestInfo)
 					.commonPays(commonpay).build();
 			commonService.updatePaymentWorkflow(paymentReq);
