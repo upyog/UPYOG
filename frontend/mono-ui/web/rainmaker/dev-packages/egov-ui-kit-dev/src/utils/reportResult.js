251cd18df162951e11f1b399ccaf6c9edae4a669
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2023-06-06 16:07:28 +0530

    UM-6109 Change in report title

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/reportResult.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/reportResult.js
@@ -1205,7 +1205,8 @@ class ShowField extends Component {
   };
 
   getReportTitle = (rptName) => {
-    let reportName = rptName || this.state.reportName || "";
+    let { metaData } = this.props;
+    let reportName = rptName || metaData && metaData.reportDetails && metaData.reportDetails.summary || this.state.reportName || "";
     reportName = reportName.toUpperCase();
     let reportTitleArr = reportName && getLocaleLabels(reportName, reportName).split(/(?=[A-Z])/);
     let reportTitle = "";
