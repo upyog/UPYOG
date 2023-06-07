d650be7d50e9a26e918fa7f5339984021f100ca7
unknown <sriranjan.srivastava@pwc.com>
2023-06-07 21:13:26 +0530

    added the css cgange for sidebar static

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/citizen/PTApplicationDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/citizen/PTApplicationDetails.js
@@ -552,7 +552,7 @@ const PTApplicationDetails = () => {
           />
         )}
         </Card>
-        {/* <LinkButton style={{marginLeft:"5%",color:"#F47738"}} label={t("CS_RATE_US")} onClick={() => setpopup(true)} /> */}
+        {/* <LinkButton style={{marginLeft:"5%",color:"#a82227"}} label={t("CS_RATE_US")} onClick={() => setpopup(true)} /> */}
         {/* {popup && (<PopUp>
           <div style={{margin:"0 auto", top:"15%", position:"relative"}}>
           <PTCitizenFeedback popup={true} onClose={setpopup} setShowToast={setShowToast} data={data}/>
