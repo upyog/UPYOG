c07f1257e0eb2c722366b1dad9be521ec48da108
upyog-central-git <115204475+upyog-central-git@users.noreply.github.com>
2023-06-17 10:37:45 +0000

    Update footer.js

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/footer.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/footer.js
@@ -116,7 +116,7 @@ export const callPGService = async (state, dispatch) => {
         billId: get(billPayload, "Bill[0].id"),
         consumerCode: consumerCode,
         productInfo: "Common Payment",
-        gateway: "AXIS",
+        gateway: "PAYGOV",
         taxAndPayments,
         user,
         callbackUrl,
