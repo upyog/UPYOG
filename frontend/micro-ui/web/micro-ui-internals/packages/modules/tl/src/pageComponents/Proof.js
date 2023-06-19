306fad6fb6e8d3b2059ba58ebb05ba7a37a92494
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2023-06-19 17:55:56 +0530

    UM-6182 removal for pdf format acceptance

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pageComponents/Proof.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tl/src/pageComponents/Proof.js
@@ -9,7 +9,7 @@ const Proof = ({ t, config, onSelect, userType, formData }) => {
   const [file, setFile] = useState(formData?.owners?.documents?.OwnerPhotoProof);
   const [error, setError] = useState(null);
   const cityDetails = Digit.ULBService.getCurrentUlb();
-  let acceptFormat = ".jpg,.png,.pdf,.jpeg";
+  let acceptFormat = ".jpg,.png,.jpeg";
 
   const [dropdownValue, setDropdownValue] = useState(formData?.owners?.documents?.OwnerPhotoProof?.documentType || null);
   // let dropdownData = [];
