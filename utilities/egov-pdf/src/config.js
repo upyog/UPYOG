8ded694e1009385ff5fa0c6f376674dd2f042776
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2023-07-20 11:25:18 +0000

    updated sewearge connection search host

--- utilities/egov-pdf/src/config.js
+++ utilities/egov-pdf/src/config.js
@@ -69,8 +69,8 @@ module.exports = {
     workflow: process.env.EGOV_WORKFLOW_HOST || HOST,
     challan: process.env.EGOV_ECHALLAN_HOST || 'http://echallan-services.egov:8080/',
     mcollectBilling: process.env.EGOV_BILLING_HOST || 'http://billing-service.egov:8080/',
-    wns: process.env.EGOV_WNS_HOST || 'http://ws-services.egov:8080/,
-    sw: process.env.EGOV_WNS_HOST || 'http://sw-services.egov:8080/'
+    wns: process.env.EGOV_WNS_HOST || HOST,
+    sw: process.env.EGOV_WNS_HOST || HOST
   },
   paths: {
     pdf_create: "/pdf-service/v1/_createnosave",
