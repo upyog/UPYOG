93112f2a442585ab5a10a65543b93dd853156d23
unknown <sriranjan.srivastava@pwc.com>
2023-08-01 17:09:32 +0530

    Added the wns bill

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/ws/src/pages/employee/SearchIntegrated.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ws/src/pages/employee/SearchIntegrated.js
@@ -1,5 +1,5 @@
 import { Loader, Toast } from "@egovernments/digit-ui-react-components";
-import React, { useState, Fragment, useCallback } from "react";
+import React, { useState, Fragment, useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 const SearchIntegrated = ({ path }) => {
@@ -9,12 +9,11 @@ const SearchIntegrated = ({ path }) => {
   const [payload, setPayload] = useState({});
   const [setLoading, setLoadingState] = useState(false);
   const SWater = Digit.ComponentRegistryService.getComponent("WSSearchWaterConnectionIntegrated");
-  const [propId,setPropId] = useState("")
+
   //const [businessServ, setBusinessServ] = useState([]);
   const getUrlPathName = window.location.pathname;
   const checkPathName = getUrlPathName.includes("water/search");
   const businessServ = checkPathName ? "WS" : "SW";
-
   const [showToast, setShowToast] = useState(null);
   const serviceConfig = {
     WATER: "WATER",
@@ -30,37 +29,14 @@ const SearchIntegrated = ({ path }) => {
         setShowToast(false);
       }, 3000);
     } else {
-      setPropId(propertyId)
       setPayload(
         Object.keys(_data)
           .filter((k) => _data[k])
           .reduce((acc, key) => ({ ...acc, [key]: typeof _data[key] === "object" ? _data[key].code : _data[key] }), {})
       );
-     
     }
   });
 
-const downloadPdf = (blob, fileName) => {
-    if (window.mSewaApp && window.mSewaApp.isMsewaApp() && window.mSewaApp.downloadBase64File) {
-      var reader = new FileReader();
-      reader.readAsDataURL(blob);
-      reader.onloadend = function () {
-        var base64data = reader.result;
-        window.mSewaApp.downloadBase64File(base64data, fileName);
-      };
-    } else {
-      const link = document.createElement("a");
-      // create a blobURI pointing to our Blob
-      link.href = URL.createObjectURL(blob);
-      link.download = fileName;
-      // some browser needs the anchor to be in the doc
-      document.body.append(link);
-      link.click();
-      link.remove();
-      // in case the Blob uses a lot of memory
-      setTimeout(() => URL.revokeObjectURL(link.href), 7000);
-    }
-  };
   const config = {
     enabled: !!(payload && Object.keys(payload).length > 0),
   };
@@ -69,56 +45,34 @@ const downloadPdf = (blob, fileName) => {
   let result = Digit.Hooks.ws.useSearchWS({ tenantId, filters: payload, config, bussinessService: "SW", t ,shortAddress:true});
   
 
-  // let result2 = Digit.WSService.WSSewsearch({ tenantId, propertyId:"PG-PT-2023-01-08-006216",searchType:"CONNECTION3"});
-  // console.log(result2)
-//   let result = Digit.WSService.wnsGroupBill()
-//   console.log(result);
- const downloadIntegratedBill = async(propertyId) => {
-   //console.log("rsult 2",tenantId,_data.propertyId)
-   let response
-   if(result?.data?.length == 0 &&  result12?.data?.length == 0)
-  {
-  
-     console.log("result no pdf");
-  }
-  else {
-    try {
-      response = await Digit.WSService.wnsGroupBill({ propertyId,tenantId })
-      
-       //downloadPdf(new Blob([result.data], { type: "application/pdf" }), `CHALLAN1234.pdf`);
-       downloadPdf(new Blob([response.data], { type: "application/pdf" }), `IntegratedBill.pdf`);
-    }
-    catch(e){
-console.log("errorrrrrr",e)
-    }
-   
-  }
-    
-    
-}
+
   const isMobile = window.Digit.Utils.browser.isMobile();
 
  
-
   const getData = () => {
-    console.log("aaaaaaaaadsddddd",result,result12)
-    
-    if(result12?.data?.length > 0 || result?.data?.length > 0)
-    {
-      downloadIntegratedBill(propId)
-    }
+
     if (result?.data?.length == 0 &&  result12?.data?.length == 0) {
-      return { display: "ES_COMMON_NO_DATA" }
-    } else if (result12?.data?.length > 0 || result?.data?.length > 0) {
+      return { display: "ES_COMMON_NO_CONNECTION" }
+    } else if (result12?.data?.length > 0 &&  result?.data?.length == 0) {
      
-      return [...result?.data,...result12.data]
-    } else {
+      return result12?.data
+    } 
+    else if(result?.data?.length > 0 &&  result12?.data?.length == 0)
+    {
+      return result?.data
+    }
+    else if (result?.data?.length > 0 &&  result12?.data?.length > 0)
+    {
+return [...result.data,...result12.data]
+    }
+      else {
       return [];
     }
     
   }
 
   const isResultsOk = () => {
+   
     return result12?.data?.length > 0 || result?.data?.length > 0? true : false;
   }
 
