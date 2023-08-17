a1629e6b36c80413a76be064b00093f23c74d25a
siyaram-ey <Siyaram.Gupta@in.ey.com>
2023-08-17 12:34:52 +0530

    contract master featur module

---
+++ frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/ContrMaster/ContrView.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const ContrMasterView = () =>{
+
+    return (<div>CONTRACTOR MASTER VIEW component</div>)
+}
+export default ContrMasterView
---
+++ frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/ContrMaster/index.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+
+const ContrMasterAdd = () =>{
+
+    return (<div>CONTRACTOR MASTER Add component</div>)
+}
+export default ContrMasterAdd
