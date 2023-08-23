26b710ab1a867c9c6811f64cfe8407cbb131837f
palak-egov <palak.garg@egovernments.org>
2023-08-23 17:35:38 +0530

    changing config

--- core-services/service-request/src/main/java/digit/config/Configuration.java
+++ core-services/service-request/src/main/java/digit/config/Configuration.java
@@ -87,7 +87,7 @@ public class Configuration {
     @Value("${egov.localization.search.endpoint}")
     private String localizationEndpoint;
 
-    @Value("${ss.notification.fallback.locale}")
+    @Value("${egov.service.notification.notification.fallback.locale}")
     private String fallBackLocale;
 
 }
