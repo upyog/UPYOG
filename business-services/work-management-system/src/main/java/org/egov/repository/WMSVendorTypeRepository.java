523253c8340748ed98c6f7d7a1b1e3d4e309cdcd
Sarfaraz Nayyar <Sarfaraz.Nayyar@in.ey.com>
2023-09-21 15:34:46 +0530

    Vendor type api created
    Vendor type api created

---
+++ business-services/work-management-system/src/main/java/org/egov/repository/WMSVendorTypeRepository.java
@@ -0,0 +1,55 @@
+package org.egov.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.egov.repository.querybuilder.SORApplicationQueryBuilder;
+import org.egov.repository.querybuilder.WMSBankDetailsApplicationQueryBuilder;
+import org.egov.repository.querybuilder.WMSContractorApplicationQueryBuilder;
+import org.egov.repository.querybuilder.WMSContractorSubTypeApplicationQueryBuilder;
+import org.egov.repository.querybuilder.WMSVendorTypeApplicationQueryBuilder;
+import org.egov.repository.querybuilder.WMSWorkApplicationQueryBuilder;
+import org.egov.repository.rowmapper.SORApplicationRowMapper;
+import org.egov.repository.rowmapper.WMSBankDetailsApplicationRowMapper;
+import org.egov.repository.rowmapper.WMSContractorApplicationRowMapper;
+import org.egov.repository.rowmapper.WMSContractorSubTypeApplicationRowMapper;
+import org.egov.repository.rowmapper.WMSVendorTypeApplicationRowMapper;
+import org.egov.repository.rowmapper.WMSWorkApplicationRowMapper;
+import org.egov.web.models.SORApplicationSearchCriteria;
+import org.egov.web.models.ScheduleOfRateApplication;
+import org.egov.web.models.WMSBankDetailsApplication;
+import org.egov.web.models.WMSBankDetailsApplicationSearchCriteria;
+import org.egov.web.models.WMSContractorApplication;
+import org.egov.web.models.WMSContractorApplicationSearchCriteria;
+import org.egov.web.models.WMSContractorSubTypeApplication;
+import org.egov.web.models.WMSContractorSubTypeApplicationSearchCriteria;
+import org.egov.web.models.WMSVendorTypeApplication;
+import org.egov.web.models.WMSVendorTypeApplicationSearchCriteria;
+import org.egov.web.models.WMSWorkApplication;
+import org.egov.web.models.WMSWorkApplicationSearchCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Repository
+public class WMSVendorTypeRepository {
+	
+	@Autowired
+    private WMSVendorTypeApplicationQueryBuilder queryBuilder;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private WMSVendorTypeApplicationRowMapper rowMapper;
+
+    public List<WMSVendorTypeApplication>getApplications(WMSVendorTypeApplicationSearchCriteria searchCriteria){
+        List<Object> preparedStmtList = new ArrayList<>();
+        String query = queryBuilder.getVendorTypeApplicationSearchQuery(searchCriteria, preparedStmtList);
+        log.info("Final query: " + query);
+        return jdbcTemplate.query(query,  rowMapper,preparedStmtList.toArray());
+    }
+
+}
