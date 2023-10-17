84e35c51f226e90a0db60f6c25c399a2774ebd2d
Deepa <deepa.o.sd@pwc.com>
2023-10-17 11:30:58 +0000

    PT rebate Penalty issue in assessment

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/employee/AssessmentDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/employee/AssessmentDetails.js
@@ -171,6 +171,14 @@ const CloseBtn = (props) => {
 function change(){
   var total_amount=ptCalculationEstimateData?.Calculation[0]?.totalAmount
   const [first,second]=[parseInt(first_temp.current.value),parseInt(second_temp.current.value)];
+  var additionalDetails={
+    "adhocPenalty":0,
+    "adhocExemptionReason":null,
+    "adhocPenaltyReason":null,
+    "adhocExemption":0
+  }
+  AssessmentData.additionalDetails=additionalDetails;
+
     if((selectedPenalityReason && first>0)/* &&(!selectedRebateReason) */){
       if(selectPenalityReason.value!=='Others'){
         if(first<total_amount){
@@ -180,10 +188,12 @@ function change(){
             "estimateAmount": ptCalculationEstimateData.Calculation[0].taxHeadEstimates[6]?.estimateAmount+first,
             "category": "TAX"
         }
-        AssessmentData.additionalDetails={
-          "adhocPenalty":additionalPenality,
-          "adhocPenaltyReason":selectedPenalityReason.value,
-        }
+        // AssessmentData.additionalDetails={
+        //   "adhocPenalty":additionalPenality,
+        //   "adhocPenaltyReason":selectedPenalityReason.value,
+        // }
+        AssessmentData.additionalDetails.adhocPenalty=additionalPenality;
+        AssessmentData.additionalDetails.adhocPenaltyReason=selectedPenalityReason.value;
         ptCalculationEstimateData.Calculation[0].totalAmount=ptCalculationEstimateData?.Calculation[0]?.totalAmount+first;
            }
            else{
@@ -198,10 +208,12 @@ function change(){
             "estimateAmount": ptCalculationEstimateData.Calculation[0].taxHeadEstimates[6]?.estimateAmount+first,
             "category": "TAX"
         }
-        AssessmentData.additionalDetails={
-          "adhocPenalty":additionalPenality,
-          "adhocPenaltyReason":fourth_temp.current.value,
-        }
+        // AssessmentData.additionalDetails={
+        //   "adhocPenalty":additionalPenality,
+        //   "adhocPenaltyReason":fourth_temp.current.value,
+        // }
+        AssessmentData.additionalDetails.adhocPenalty=additionalPenality;
+        AssessmentData.additionalDetails.adhocPenaltyReason=fourth_temp.current.value;
         ptCalculationEstimateData.Calculation[0].totalAmount=ptCalculationEstimateData?.Calculation[0]?.totalAmount+first;
            }
            else{
@@ -219,10 +231,12 @@ function change(){
             "estimateAmount": ptCalculationEstimateData.Calculation[0].taxHeadEstimates[5]?.estimateAmount+second,
             "category": "TAX"
         }
-        AssessmentData.additionalDetails={
-          "adhocExemption":second,
-          "adhocExemptionReason":selectedRebateReason.value,
-        }
+        // AssessmentData.additionalDetails={
+        //   "adhocExemption":second,
+        //   "adhocExemptionReason":selectedRebateReason.value,
+        // }
+        AssessmentData.additionalDetails.adhocExemption=second;
+        AssessmentData.additionalDetails.adhocExemptionReason=selectedRebateReason.value;
         ptCalculationEstimateData.Calculation[0].totalAmount=ptCalculationEstimateData?.Calculation[0]?.totalAmount-second;
            }
            else{
@@ -238,10 +252,12 @@ function change(){
             "estimateAmount": ptCalculationEstimateData.Calculation[0].taxHeadEstimates[5]?.estimateAmount-second,
             "category": "TAX"
         }
-        AssessmentData.additionalDetails={
-          "adhocExemption":second,
-          "adhocExemptionReason":third_temp.current.value,
-        }
+        // AssessmentData.additionalDetails={
+        //   "adhocExemption":second,
+        //   "adhocExemptionReason":third_temp.current.value,
+        // }
+        AssessmentData.additionalDetails.adhocExemption=second;
+        AssessmentData.additionalDetails.adhocExemptionReason=third_temp.current.value;
         ptCalculationEstimateData.Calculation[0].totalAmount=ptCalculationEstimateData?.Calculation[0]?.totalAmount-second;
            }
            else{
