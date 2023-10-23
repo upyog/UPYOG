77663b505e2b16af77789532388195beaf9e872c
deepikaarora88 <deepika.arora@pwc.com>
2023-10-23 06:30:32 +0000

    ws reconn payment fic

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/service/PaymentUpdateService.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/service/PaymentUpdateService.java
@@ -87,7 +87,7 @@ public class PaymentUpdateService {
 			boolean isServiceMatched = false;
 			for (PaymentDetail paymentDetail : paymentRequest.getPayment().getPaymentDetails()) {
 				if (WCConstants.WATER_SERVICE_BUSINESS_ID.equals(paymentDetail.getBusinessService()) ||
-            paymentDetail.getBusinessService().equalsIgnoreCase(config.getReceiptBusinessservice())) {
+            paymentDetail.getBusinessService().equalsIgnoreCase(config.getReceiptBusinessservice()) || paymentDetail.getBusinessService().equalsIgnoreCase(config.getReconnectBusinessServiceName())) {
 					isServiceMatched = true;
 				}
 			}
@@ -104,7 +104,7 @@ public class PaymentUpdateService {
 							.connectionNumber(Stream.of(paymentDetail.getBill().getConsumerCode().toString()).collect(Collectors.toSet()))
 							.applicationStatus(Collections.singleton(PENDING_FOR_PAYMENT_STATUS_CODE)).build();
 				}
-				if (paymentDetail.getBusinessService().equalsIgnoreCase(config.getReceiptBusinessservice())) {
+				if (paymentDetail.getBusinessService().equalsIgnoreCase(config.getReceiptReconnectionBusinessservice()) || paymentDetail.getBusinessService().equalsIgnoreCase(config.getReceiptBusinessservice())) {
 					criteria = SearchCriteria.builder()
 							.tenantId(paymentRequest.getPayment().getTenantId())
 							.applicationNumber(Stream.of(paymentDetail.getBill().getConsumerCode().toString()).collect(Collectors.toSet())).build();
