4b17a00a287feff0b29155765ab1a60c9ffd19b6
bdhal1989 <bdhal@deloitte.com>
2023-11-16 13:10:59 +0530

    commit update for property addtion

---
+++ municipal-services/property-services/src/main/resources/db/migration/main/V202311151051__alter_eg_pt_address.sql
@@ -0,0 +1,17 @@
+ALTER TABLE eg_pt_address ADD COLUMN dag_no varchar (256) NOT NULL;
+ALTER TABLE eg_pt_address ADD COLUMN district_name varchar (256) NOT NULL;
+ALTER TABLE eg_pt_address ADD COLUMN town_name varchar(256) NOT NULL;
+ALTER TABLE eg_pt_address ADD COLUMN ward_no varchar (256) NOT NULL;
+ALTER TABLE eg_pt_address ADD COLUMN leikai_name varchar (256) NOT NULL;
+ALTER TABLE eg_pt_address ADD COLUMN village varchar (256) NOT NULL;
+ALTER TABLE eg_pt_address ADD COLUMN patta_no varchar (256) NOT NULL;
+
+
+ALTER TABLE eg_pt_address ADD COLUMN proper_house_no varchar (256) ;
+ALTER TABLE eg_pt_address ADD COLUMN common_name_of_building varchar (256) ;
+ALTER TABLE eg_pt_address ADD COLUMN principal_road_name varchar (256) ;
+ALTER TABLE eg_pt_address ADD COLUMN  sub_side_road_name varchar (256) ;
+ALTER TABLE eg_pt_address ADD COLUMN landmark_name varchar (256) ;
+ALTER TABLE eg_pt_address ADD COLUMN others varchar (256) ;
+
+
