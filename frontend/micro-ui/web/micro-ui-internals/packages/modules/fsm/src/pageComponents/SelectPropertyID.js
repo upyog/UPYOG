dd96110d83acaa905da0e422d55e4993f1ad6bb5
Puli-shilpa <puli.shilpa@pwc.com>
2023-11-22 09:57:47 +0530

    corrected property field appearance on citizen side

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pageComponents/SelectPropertyID.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pageComponents/SelectPropertyID.js
@@ -2,59 +2,43 @@ import React, { useState, useEffect } from "react";
 import { FormStep, TextArea, LabelFieldPair, CardLabel, TextInput } from "@egovernments/digit-ui-react-components";
 import Timeline from "../components/TLTimelineInFSM";
 const SelectPropertyID = ({ t, config, onSelect, formData, userType, setError: setFormError, clearErrors: clearFormErrors }) => {
-  const tenantId = Digit.ULBService.getCurrentTenantId();
-  const stateId = Digit.ULBService.getStateId(); 
-  const [propertyID, setPropertyID] = useState(formData?.propertyID?.propertyID);
-  console.log("formData", formData)
-  
-    const [error, setError] = useState("");
-  
-    const inputs = [
-      {
-        label: "ES_FILE_APPLICATION_PROPERTY_ID_LABEL",
-        type: "text",
-        name: "propertyID",
-        validation: {
-          maxLength: 10,
-          minLength : 10,
-        },
-      },
-    ];
-    const goNext=()=>{
-      onSelect(config.key, { ...formData[config.key], ...data })
-    }
-  
-    useEffect(() => {
-      setPropertyID(formData?.additionalDetails?.propertyID);
-    }, [formData?.additionalDetails?.propertyID]);
-  
-    function onChange(e) {
-      if (e.target.value.length > 10 && e.target.length<10) {
-        setError("CS_COMMON_PROPERTY_ID_MAX_LENGTH");
-      } else {
-        setError(null);
-        setPropertyID(e.target.value);
-        if (userType === "employee") {
-          console.log("propID", propertyID)
-          /*if (propertyID !== "undefined" && propertyID?.length === 0) setFormError(config.key, { type: "required", message: t("CORE_COMMON_REQUIRED_ERRMSG") });
+const [propertyID, setPropertyID] = useState(formData?.propertyID?.propertyID);
+const [error, setError] = useState("");
+const inputs = [
+  {
+    type: "text",
+    name: "propertyID",
+    validation: {
+    maxLength: 10,
+    minLength : 10,
+    },
+  },
+  ];
+  const goNext=()=>{
+    onSelect(config.key, { ...formData[config.key], ...data })
+  }
+  useEffect(() => {
+    setPropertyID(formData?.additionalDetails?.propertyID);
+  }, [formData?.additionalDetails?.propertyID]);
+  function onChange(e) {
+    if (e.target.value.length !==10) {
+      setError("ERR_DEFAULT_INPUT_FIELD_MSG")
+     
+  } else {
+      setPropertyID(e.target.value);
+      if (userType === "employee") {
+        /*if (propertyID !== "undefined" && propertyID?.length === 0) setFormError(config.key, { type: "required", message: t("CORE_COMMON_REQUIRED_ERRMSG") });
           else if (propertyID !== "undefined" && propertyID?.length < 10 || propertyID?.length > 10 || !Number(propertyID)) setFormError(config.key, { type: "invalid", message: t("ERR_DEFAULT_INPUT_FIELD_MSG") });
           else (clearFormErrors(config.key) && setFormError(false));*/
-          const value = e?.target?.value;
-          const key = e?.target?.id;
           onSelect(config.key, { ...formData[config.key], propertyID: e.target.value });
         }
       }
     }
-  
     if (userType === "employee") {
-      
         return (
-          
-            <TextInput className="form-field" id="propertyID" value={propertyID} onChange={onChange} t={t} />
-         
-        );
-     
-    }
+          <TextInput className="form-field" id="propertyID" value={propertyID} onChange={onChange} t={t} />
+         );
+     }
     const onSkip = () => onSelect();
     return (
       <React.Fragment>
@@ -64,11 +48,10 @@ const SelectPropertyID = ({ t, config, onSelect, formData, userType, setError: s
          config={{ ...config, inputs }}
          value={propertyID}
          onSelect={(data) => onSelect(config.key, { ...formData[config.key], ...data })}
-         
          onChange={onChange}
          onSkip={onSkip}
          forcedError={t(error)}
-       isDisabled={propertyID ? false : true}
+         isDisabled={propertyID ? false : true}
        ></FormStep>
       </React.Fragment>
     );
