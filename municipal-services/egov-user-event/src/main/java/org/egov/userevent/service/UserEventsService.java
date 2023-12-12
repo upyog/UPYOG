59d161bba17083c32e296b2826828b4dab4d0114
bdhal1989 <bdhal@deloitte.com>
2023-12-12 15:23:12 +0530

    update for user events

--- municipal-services/egov-user-event/src/main/java/org/egov/userevent/service/UserEventsService.java
+++ municipal-services/egov-user-event/src/main/java/org/egov/userevent/service/UserEventsService.java
@@ -582,10 +582,26 @@ public class UserEventsService {
 		final HttpHeaders headers = new HttpHeaders();
 		headers.add("access_token", authToken);
 		User user=restTemplate.postForObject(authURL, headers, User.class);
-		String userid=Integer.toString(user.getId());
+		System.out.println(user);
+		org.egov.common.contract.request.User u = new org.egov.common.contract.request.User();
+		u.setEmailId(user.getEmailId());
+		u.setId(user.getId());
+		u.setMobileNumber(user.getMobileNumber());
+		u.setName(user.getName());
+		List<Role> r = new ArrayList<>();
+		for(org.egov.userevent.web.contract.Role rl : user.getRoles()) {
+			Role rln = new Role();
+			rln.setCode(rl.getCode());
+			//rln.setId(rl.getCode());
+			rln.setName(rl.getName());
+			r.add(rln);
+		}
+		u.setRoles(r);
+		u.setType(user.getType());
+		requestInfo.setUserInfo(u);
+		String userid=user.getId().toString();
 		List<String> userids = new ArrayList<String>(Arrays.asList(userid));
 		criteria.setUserids(userids);
-		
 		//System.out.println("requestInfo"+requestInfo);
 		//System.out.println("authToken"+authToken);
 		//System.out.println("authToken"+authToken);
