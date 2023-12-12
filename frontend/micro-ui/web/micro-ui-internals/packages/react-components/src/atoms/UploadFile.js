cc38ca95d4e6a5cc42e6973b2f8a3e0ec86f35a9
unknown <sriranjan.srivastava@pwc.com>
2023-12-12 17:05:24 +0530

    added the meri pehchaan button integration

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/UploadFile.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/UploadFile.js
@@ -3,6 +3,7 @@ import ButtonSelector from "./ButtonSelector";
 import { Close } from "./svgindex";
 import { useTranslation } from "react-i18next";
 import RemoveableTag from "./RemoveableTag";
+import { Loader, Modal } from "..";
 
 const getRandomId = () => {
   return Math.floor((Math.random() || 1) * 139);
@@ -138,6 +139,7 @@ const UploadFile = (props) => {
   const inpRef = useRef();
   const [hasFile, setHasFile] = useState(false);
   const [prevSate, setprevSate] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const user_type = Digit.SessionStorage.get("userType");
   let extraStyles = {};
   const handleChange = () => {
@@ -147,6 +149,26 @@ const UploadFile = (props) => {
     }
     else setHasFile(false);
   };
+  const closeModal = () => {
+    setShowModal(false);
+}
+const Heading = (props) => {
+  return <h1 style={{ marginLeft: "22px" }} className="heading-m BPAheading-m">{props.label}</h1>;
+};
+
+const Close = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#0B0C0C" />
+  </svg>
+);
+
+const CloseBtn = (props) => {
+  return (
+      <div className="icon-bg-secondary" onClick={props.onClick} style={{ backgroundColor: "#FFFFFF" }}>
+          <Close />
+      </div>
+  );
+};
 
   // for common aligmnent issues added common styles
   extraStyles = getCitizenStyles("OBPS");
@@ -188,9 +210,12 @@ const UploadFile = (props) => {
   useEffect(() => handleEmpty(), [inpRef?.current?.files])
 
   useEffect(() => handleChange(), [props.message]);
-
+const setModelValue =(e)=>{
+  e.preventDefault()
+  setShowModal(true)
+}
   const showHint = props?.showHint || false;
-
+let url ="https://digilocker.meripehchaan.gov.in/public/oauth2/1/authorize?response_type=code&client_id=IB0DDEFE20&state=oidc_flow&redirect_uri=https%3A%2F%2Fupyog-test.niua.org%2FDigiLocker&code_challenge=lx1QRh0rzTWiVqlOyruU0CNS9WASYNtYQ1atsutrzK4&code_challenge_method=S256&dl_flow=signin&output=embed"
   return (
     <Fragment>
       {showHint && <p className="cell-text">{t(props?.hintText)}</p>}
@@ -203,6 +228,10 @@ const UploadFile = (props) => {
             textStyles={props?.textStyles}
             type={props.buttonType}
           />
+          <span style={{fontWeight:"bold"}}>OR</span>
+          <div className="col col-md-4  text-md-center p-0" style={{width:"40%"}}>
+             <button className="digilocker-btn"type="submit" onClick={(e)=> setModelValue(e)}><img src="https://meripehchaan.gov.in/assets/img/icon/digi.png" class="mr-2" style={{"width":"12%"}}></img>Fetch from DigiLocker</button>
+                </div>
             {props?.uploadedFiles?.map((file, index) => {
               const fileDetailsData = file[1]
               return <div className="tag-container" style={extraStyles ? extraStyles?.tagContainerStyles : null}>
@@ -243,6 +272,37 @@ const UploadFile = (props) => {
       </div>
       {props.iserror && <p style={{color: "red"}}>{props.iserror}</p>}
       {props?.showHintBelow && <p className="cell-text">{t(props?.hintText)}</p>}
+      <div>
+            {showModal ? <Modal
+                headerBarMain={<Heading label={t(`CCF_DIGILOCKER_HEADER`)} />}
+                headerBarEnd={<CloseBtn onClick={closeModal} />}
+                actionCancelOnSubmit={closeModal}
+                formId="modal-action"
+                popupStyles={{ width: "750px", overflow: "auto" }}
+                style={{ minHeight: "45px", height: "auto", width: "160px" }}
+                hideSubmit={true}
+                headerBarMainStyle={{ margin: "0px", height: "35px" }}
+
+            >
+                {url ?
+                    <div style={{ width: "auto", height: "91vh", overflow: "hidden" }}>
+                        <iframe
+                            // allowfullscreen="true"
+                            scrollbar={"none"}
+                            border="none"
+                            width={"100%"}
+                            height={"100%"}
+                            overflow={"auto"}
+                            src={`${url}`}
+                            referrerpolicy="no-referrer|no-referrer-when-downgrade|origin|origin-when-cross-origin|same-origin|strict-origin-when-cross-origin|unsafe-url"
+                        ></iframe>
+                    </div> : 
+                    <div style={{width: "100%", height: "100px", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                        {t("COMMON_URL_NOT_FOUND")}
+                    </div>}
+
+            </Modal> : null}
+        </div>
     </Fragment>
   );
 };
