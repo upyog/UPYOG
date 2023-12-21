99d98ea6ab02d210c5b4f9e8f8b848d68b5c6ac3
hinamakhija-eGov <96222691+hinamakhija-eGov@users.noreply.github.com>
2023-12-21 16:14:02 +0530

    Update User.java

--- municipal-services/fsm/src/main/java/org/egov/fsm/web/model/user/User.java
+++ municipal-services/fsm/src/main/java/org/egov/fsm/web/model/user/User.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
