a6ac7759d33ce53bb4c1c8531b5c239343552f28
Sriranjan <sriranjan.srivastava@pwc.com>
2023-12-23 22:22:02 +0530

    adde mutation owne sequence fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/citizen/MyProperties/propertyInformation.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/citizen/MyProperties/propertyInformation.js
@@ -365,7 +365,7 @@ const handleClick=()=>{
           <CardSubHeader>{t("PT_COMMON_PROPERTY_OWNERSHIP_DETAILS_HEADER")}</CardSubHeader>
           <div className="owner-details">
             {Array.isArray(owners) &&
-              owners.sort((item,item2)=>{return item.additionalDetails.ownerSequence - item2.additionalDetails.ownerSequence}).map((owner, index) => (
+              owners.sort((item,item2)=>{return item?.additionalDetails?.ownerSequence - item2?.additionalDetails?.ownerSequence}).map((owner, index) => (
                 <div key={index} className="owner-details-child">
                   <CardSubHeader>
                     {owners.length != 1 && (
