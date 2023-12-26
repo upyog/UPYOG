ea3e9fdbac8dc8eee92699cbe7e7ea085f66cd84
Mobhatt <mohit.bhatt1@in.ey.com>
2023-12-26 11:01:02 +0530

    created new module project register

---
+++ frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/PR/Create/index.js
@@ -0,0 +1,67 @@
+import { FormComposer, Loader,Header } from "@egovernments/digit-ui-react-components";
+import React, {  useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import { newConfig } from "../../../../components/config/pr-config";
+import { convertEpochToDate, pdfDownloadLink } from "../../../../components/Utils";
+const WmsPrCreate = () => {
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+  const { t } = useTranslation();
+  const history = useHistory();  
+  const [isLoading, setIsLoading] = useState(false);
+  const [canSubmit, setSubmitValve] = useState(false);
+  const [showToast, setShowToast] = useState(null);
+  const onSubmit = (data) => {
+    setIsLoading(true);
+    let PrApplications = 
+      {
+        PrApplication: [{
+          project_name: data?.WmsPrPrjName?.project_name,  
+          work_name: data?.WmsPrWorkName?.work_name,  
+          milestone_name: data?.WmsPrMLName?.milestone_name,
+          percent_weightage: data?.WmsPrPercent?.percent_weightage,
+          tenantId:tenantId
+        }],
+      };
+    
+      /* use customiseCreateFormData hook to make some chnages to the Employee object [0].PreduleOfRateApplication*/
+     Digit.WMSService.PHMApplications.create(PrApplications.PrApplication[0], tenantId).then((result,err)=>{
+      setIsLoading(false);
+       history.push("/upyog-ui/citizen/wms/pr-home");
+     })
+     .catch((e) => {
+     console.log("err");
+    });
+  };
+
+  
+
+  if (isLoading) {
+    return <Loader />;
+  }
+  const configs = newConfig?newConfig:newConfig;
+
+  return (
+    <div>
+    <Header>{t("WMS_NEW_PHM_FORM_HEADER")}</Header>
+    <FormComposer
+              head={t("WMS_PHM_FORM_CREATE_HEAD")}
+              label={t("WMS_COMMON_SAVE")}
+              config={configs}
+              onSubmit={onSubmit}
+              fieldStyle={{ marginRight: 0 }}
+            />
+            {showToast && (
+        <Toast
+          error={showToast.key}
+          label={t(showToast.label)}
+          onClose={() => {
+            setShowToast(null);
+          }}
+        />
+      )}
+            </div>
+  );
+};
+
+export default WmsPrCreate;
