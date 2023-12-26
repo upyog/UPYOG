8446375db513efcda99fdb9fbd3a8c94a5f7ad63
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2023-12-26 12:59:06 +0530

    UM-6631 Upyog Sandbox: PT: egov-mdms-service fetches data for PT module instead of MDMS v2

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/SelectPTUnits.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/SelectPTUnits.js
@@ -69,7 +69,7 @@ const SelectPTUnits = React.memo(({ t, config, onSelect, userType, formData }) =
     }
   };
 
-  const { data: mdmsData, isLoading } = Digit.Hooks.useCommonMDMS(
+  const { data: mdmsData, isLoading } = Digit.Hooks.useCommonMDMSV2(
     Digit.ULBService.getStateId(),
     "PropertyTax",
     ["Floor", "OccupancyType", "UsageCategory"],
