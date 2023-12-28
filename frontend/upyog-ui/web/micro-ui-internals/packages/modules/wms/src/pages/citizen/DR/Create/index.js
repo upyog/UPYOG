e923b4b842bc15f7b3c3988c88bdfe5de1645deb
Mobhatt <mohit.bhatt1@in.ey.com>
2023-12-28 13:37:08 +0530

    field change

--- frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/DR/Create/index.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/DR/Create/index.js
@@ -43,9 +43,9 @@ const WmsDrCreate = () => {
 
   return (
     <div>
-    <Header>{t("WMS_NEW_PHM_FORM_HEADER")}</Header>
+    <Header>{t("WMS_NEW_DR_FORM_HEADER")}</Header>
     <FormComposer
-              head={t("WMS_PHM_FORM_CREATE_HEAD")}
+              head={t("WMS_DR_FORM_CREATE_HEAD")}
               label={t("WMS_COMMON_SAVE")}
               config={configs}
               onSubmit={onSubmit}
