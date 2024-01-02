615638a5b0c8a5b6956fb68998a2ddcc6698ab72
Mobhatt <mohit.bhatt1@in.ey.com>
2024-01-02 12:52:06 +0530

    mba module

--- frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/MBA/Create/index.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/modules/wms/src/pages/citizen/MBA/Create/index.js
@@ -2,10 +2,10 @@ import { FormComposer, Loader,Header } from "@egovernments/digit-ui-react-compon
 import React, {  useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
-import {newConfig} from "../../../../components/config/pma-config";
+import {newConfig} from "../../../../components/config/MBA-config";
 
 import { convertEpochToDate, pdfDownloadLink } from "../../../../components/Utils";
-const WmsPmaCreate = () => {
+const WmsMBACreate = () => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
   const history = useHistory();  
@@ -14,21 +14,21 @@ const WmsPmaCreate = () => {
   const [showToast, setShowToast] = useState(null);
   const onSubmit = (data) => {
     setIsLoading(true);
-    let PmaemeApplications = 
+    let MBAemeApplications = 
       {
-        PmaemeApplication: [{
-          description_of_item: data?.WmsPmaDescriptionOfItem?.description_of_item, 
-          percent_weightage: data?.WmsPmaPercent?.percent_weightage, 
-          start_date:data?.WmsPmaStartDate?.start_date,
-          end_date:data?.WmsPmaEndDate?.end_date,
+        MBAemeApplication: [{
+          description_of_item: data?.WmsMBADescriptionOfItem?.description_of_item, 
+          percent_weightage: data?.WmsMBAPercent?.percent_weightage, 
+          start_date:data?.WmsMBAStartDate?.start_date,
+          end_date:data?.WmsMBAEndDate?.end_date,
           tenantId:tenantId
         }],
       };
     
-      /* use customiseCreateFormData hook to make some chnages to the Employee object [0].PmaeduleOfRateApplication*/
-     Digit.WMSService.PMAApplications.create(PmaemeApplications.PmaemeApplication[0], tenantId).then((result,err)=>{
+      /* use customiseCreateFormData hook to make some chnages to the Employee object [0].MBAeduleOfRateApplication*/
+     Digit.WMSService.MBAApplications.create(MBAemeApplications.MBAemeApplication[0], tenantId).then((result,err)=>{
       setIsLoading(false);
-       history.push("/upyog-ui/citizen/wms/pma-home");
+       history.push("/upyog-ui/citizen/wms/MBA-home");
      })
      .catch((e) => {
      console.log("err");
@@ -44,9 +44,9 @@ const WmsPmaCreate = () => {
 
   return (
     <div>
-    <Header>{t("WMS_NEW_PMA_FORM_HEADER")}</Header>
+    <Header>{t("WMS_NEW_MBA_FORM_HEADER")}</Header>
     <FormComposer
-              head={t("WMS_PMA_FORM_CREATE_HEAD")}
+              head={t("WMS_MBA_FORM_CREATE_HEAD")}
               label={t("WMS_COMMON_SAVE")}
               config={configs}
               onSubmit={onSubmit}
@@ -65,4 +65,4 @@ const WmsPmaCreate = () => {
   );
 };
 
-export default WmsPmaCreate;
+export default WmsMBACreate;
