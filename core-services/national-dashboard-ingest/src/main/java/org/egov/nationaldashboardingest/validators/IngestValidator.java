8abf95539ddd7ae6513ab258c986cb56b1aa85dd
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2024-01-02 11:12:24 +0000

    replace : while db persistence

--- core-services/national-dashboard-ingest/src/main/java/org/egov/nationaldashboardingest/validators/IngestValidator.java
+++ core-services/national-dashboard-ingest/src/main/java/org/egov/nationaldashboardingest/validators/IngestValidator.java
@@ -155,7 +155,7 @@ public class IngestValidator {
         validateStringNotNumeric(ingestData.getRegion());
         validateStringNotNumeric(ingestData.getState());
         if(ingestData.getWard().contains(":"))
-        	ingestData.setWard(ingestData.getWard().replace(":",""));
+        	ingestData.setWard(ingestData.getWard().replace(":"," "));
 		
         ingestData.setState(toCamelCase(ingestData.getState()));
 
@@ -514,6 +514,8 @@ public class IngestValidator {
         IngestAckData hashedData = new IngestAckData();
         List<AckEntity> ackEntityList = new ArrayList<>();
         ingestData.forEach(data -> {
+	    if(data.getWard().contains(":"))
+        	 data.setWard(data.getWard().replace(":"," "));
             StringBuilder currKeyData = new StringBuilder();
             currKeyData.append(data.getDate()).append(":").append(data.getModule()).append(":").append(data.getWard()).append(":").append(data.getUlb()).append(":").append(data.getRegion()).append(":").append(data.getState());
             log.info("Current key data: " + currKeyData);
