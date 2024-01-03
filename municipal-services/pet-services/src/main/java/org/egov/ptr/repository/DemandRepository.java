140cb73e36041c0dc94f2fdae24315bc7e9d22cc
abhijniua <123440235+abhijniua@users.noreply.github.com>
2024-01-03 10:50:39 +0530

    Changes for demand debug

--- municipal-services/pet-services/src/main/java/org/egov/ptr/repository/DemandRepository.java
+++ municipal-services/pet-services/src/main/java/org/egov/ptr/repository/DemandRepository.java
@@ -40,9 +40,11 @@ public class DemandRepository {
         System.out.println("Request object for fetchResult: "+ request);
         System.out.println("URL for fetchResult: "+ url);
         Object result = serviceRequestRepository.fetchResult(url,request);
+        System.out.println("Result from fetchResult method: "+ result);
         DemandResponse response = null;
         try{
             response = mapper.convertValue(result,DemandResponse.class);
+            System.out.println("Demand response mapper: "+ response);
         }
         catch(IllegalArgumentException e){
             throw new CustomException("PARSING ERROR","Failed to parse response of create demand");
