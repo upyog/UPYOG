c1602004341172072953276f0d77b32ece53e3ae
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2024-01-08 13:08:07 +0530

    Adding navagiation from workcbench to digit-ui

--- frontend/workbench-ui/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/SideBar/SideBar.js
+++ frontend/workbench-ui/web/micro-ui-internals/packages/modules/core/src/components/TopBarSideBar/SideBar/SideBar.js
@@ -26,6 +26,9 @@ const DIGIT_UI_CONTEXTS = ["digit-ui", "works-ui", "workbench-ui", "health-ui", 
 Used to navigate to other mission's ui if user has access
 */
 const navigateToRespectiveURL = (history = {}, url = "") => {
+  if (url === "/"){
+    url = "/digit-ui/employee";
+  }
   if (url?.indexOf(`/${window?.contextPath}`) === -1) {
     const hostUrl = window.location.origin;
     const updatedURL = DIGIT_UI_CONTEXTS?.every((e) => url?.indexOf(`/${e}`) === -1) ? hostUrl + `${url.includes("/digit-ui")?"":"/employee/"}` + url : hostUrl + url;
@@ -101,7 +104,6 @@ const Sidebar = ({ data }) => {
     return null; // Return null if no non-empty leftIcon is found
   }
   const renderSidebarItems = (items, parentKey = null, flag = true, level = 0) => {
-    items.Home.item.navigationURL="/digit-ui/employee";
     return (
       <div className={`submenu-container level-${level}`}>
         {Object.keys(items).map((key, index) => {
