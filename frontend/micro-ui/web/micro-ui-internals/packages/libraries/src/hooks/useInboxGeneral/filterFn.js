074b2f5934ed6c400b4fd439a0be91b3308cc661
Shivank Shukla <75115354+Shivank0501@users.noreply.github.com>
2024-01-09 12:53:34 +0530

    Whole Code merge of pet service to test env

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useInboxGeneral/filterFn.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useInboxGeneral/filterFn.js
@@ -105,4 +105,55 @@ export const filterFunctions = {
 
     return { searchFilters, workflowFilters };
   },
+  PTR: (filtersArg) => {
+    console.log("filer",filtersArg )
+    let { uuid } = Digit.UserService.getUser()?.info || {};
+
+    const searchFilters = {};
+    const workflowFilters = {};
+
+    const { applicationNumber, mobileNumber, limit, offset, sortBy, sortOrder, total, applicationStatus, services } = filtersArg || {};
+
+    if (filtersArg?.applicationNumber) {
+      searchFilters.applicationNumber = filtersArg?.applicationNumber;
+    }
+    
+    if (applicationStatus && applicationStatus?.[0]) {
+      workflowFilters.applicationStatus = applicationStatus.map((status) => status.code).join(",");
+    }
+    if (filtersArg?.locality?.length) {
+      searchFilters.locality = filtersArg?.locality.map((item) => item.code.split("_").pop()).join(",");
+    }
+
+    if (filtersArg?.locality?.code) {
+      searchFilters.locality = filtersArg?.locality?.code;
+    }
+
+    if (filtersArg?.uuid && filtersArg?.uuid.code === "ASSIGNED_TO_ME") {
+      workflowFilters.assignee = uuid;
+    }
+    if (mobileNumber) {
+      searchFilters.mobileNumber = mobileNumber;
+    }
+    if (applicationNumber) {
+      searchFilters.applicationNumber = applicationNumber;
+    }
+    if (sortBy) {
+      searchFilters.sortBy = sortBy;
+    }
+    if (sortOrder) {
+      searchFilters.sortOrder = sortOrder;
+    }
+    if (services) {
+      workflowFilters.businessServices = services.join();
+    }
+    if (limit) {
+      searchFilters.limit = limit;
+    }
+    if (offset) {
+      searchFilters.offset = offset;
+    }
+
+    return { searchFilters, workflowFilters };
+  },
 };
---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ptr/src/config/Create/config.js
@@ -0,0 +1,110 @@
+export const newConfig =[
+        
+    {
+        "head": "ES_TITILE_OWNER_DETAILS",
+        "body": [
+            {
+                "component": "PTROwnerDetails",
+                "withoutLabel": true,
+                "key": "owners",
+                "type": "component"
+            }
+        ]
+    },
+
+    {
+        "head": "ES_TITILE_PET_DETAILS",
+        "body": [
+            {
+                "component": "PTRPetdetails",
+                "withoutLabel": true,
+                "key": "pets",
+                "type": "component"
+            }
+        ]
+    },
+    
+
+    {
+        "head": "PTR_LOCATION_DETAILS",
+        "body": [
+           
+            {
+                "route": "pincode",
+                "component": "PTRSelectPincode",
+                "texts": {
+                    "headerCaption": "PT_PROPERTY_LOCATION_CAPTION",
+                    "header": "PT_PINCODE_LABEL",
+                    "cardText": "PT_PINCODE_TEXT",
+                    "submitBarLabel": "PT_COMMON_NEXT",
+                    "skipText": "CORE_COMMON_SKIP_CONTINUE"
+                },
+                "withoutLabel": true,
+                "key": "address",
+                "nextStep": "address",
+                "type": "component"
+            },
+            {
+                "route": "address",
+                "component": "PTRSelectAddress",
+                "withoutLabel": true,
+                "texts": {
+                    "headerCaption": "PT_PROPERTY_LOCATION_CAPTION",
+                    "header": "CS_FILE_APPLICATION_PROPERTY_LOCATION_ADDRESS_TEXT",
+                    "cardText": "CS_FILE_APPLICATION_PROPERTY_LOCATION_CITY_MOHALLA_TEXT",
+                    "submitBarLabel": "PT_COMMON_NEXT"
+                },
+                "key": "address",
+                "nextStep": "street",
+                "isMandatory": true,
+                "type": "component"
+            },
+            {
+                "type": "component",
+                "route": "street",
+                "component": "PTRSelectStreet",
+                "key": "address",
+                "withoutLabel": true,
+                "texts": {
+                    "headerCaption": "PT_PROPERTY_LOCATION_CAPTION",
+                    "header": "CS_FILE_APPLICATION_PROPERTY_LOCATION_ADDRESS_TEXT",
+                    "cardText": "PT_STREET_TEXT",
+                    "submitBarLabel": "PT_COMMON_NEXT"
+                },
+                "nextStep": "landmark"
+            },
+            {
+                "type": "component",
+                "route": "landmark",
+                "component": "PTSelectLandmark",
+                "withoutLabel": true,
+                "texts": {
+                    "headerCaption": "PT_PROPERTY_LOCATION_CAPTION",
+                    "header": "CS_FILE_APPLICATION_PROPERTY_LOCATION_PROVIDE_LANDMARK_TITLE",
+                    "cardText": "CS_FILE_APPLICATION_PROPERTY_LOCATION_PROVIDE_LANDMARK_TEXT",
+                    "submitBarLabel": "PT_COMMON_NEXT",
+                    "skipText": "CORE_COMMON_SKIP_CONTINUE"
+                },
+                "key": "address",
+                // "nextStep": "proof",
+                "hideInEmployee": true
+            },
+            
+        ]
+    },
+    {
+        "head": "ES_TITILE_DOCUMENT_DETAILS",
+        "body": [
+            {
+                "component": "PTRDocumentUpload",
+                "withoutLabel": true,
+                "key": "documents",
+                "type": "component"
+            }
+        ]
+    },
+
+       
+       
+        
+];
