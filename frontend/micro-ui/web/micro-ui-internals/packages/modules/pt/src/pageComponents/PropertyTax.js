30a286134551647f25bbfc40940b7ad4c0a48fb0
unknown <sriranjan.srivastava@pwc.com>
2024-01-22 17:27:14 +0530

    Added bulk bill changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/PropertyTax.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/PropertyTax.js
@@ -19,6 +19,124 @@ const PropertyTax = ({ t, config, onSelect, userType, formData }) => {
   function goNext() {
     onSelect();
   }
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  function generateCodeVerifier(length) {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+    let codeVerifier = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      codeVerifier += characters.charAt(randomIndex);
+    }
+    return codeVerifier;
+  }
+  function sha256(plain) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest("SHA-256", data);
+  }
+  function base64UrlEncode(buffer) {
+    const padding = "=".repeat((4 - (buffer.length % 4)) % 4);
+    const base64 = btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)));
+    return (
+      base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "") + padding
+    );
+  }
+  async function generateCodeChallenge(codeVerifier) {
+    const hashedBuffer = await sha256(codeVerifier);
+    const codeChallenge = base64UrlEncode(hashedBuffer);
+    return codeChallenge;
+  }
+
+const onConcent=(e)=>{
+  e.preventDefault()
+    /* Number of Random Bytes to Use to Generate Code Verifier (min 32, max 96 bytes) */
+    const randomByte = randomIntFromInterval(44, 96);
+    const codeVerifier = generateCodeVerifier(randomByte);
+    setItemWithExpiry('DigiLocker.codeVerifier', codeVerifier, 60);
+    /* Generate Code Challenge */
+    generateCodeChallenge(codeVerifier)
+      .then((codeChallenge) => {
+     
+        console.log("Code Verifier:", codeVerifier);
+        console.log("Code Challenge:", codeChallenge);
+          window.location.href =`https://digilocker.meripehchaan.gov.in/public/oauth2/1/authorize?response_type=code&client_id=AT3053EB6D&state=oidc_flow&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fdigit-ui%2Fcitizen%2Fpt%2Fproperty%2Fnew-application%2Finfo&code_challenge=${codeChallenge}&code_challenge_method=S256&dl_flow=signin`;
+        
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+      
+ 
+}
+
+useEffect(()=>{
+if(window.location.href.includes("code"))
+{
+  let code =window.location.href.split("=")[1].split("&")[0]
+  fetch('https://api.digitallocker.gov.in/public/oauth2/1/token', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "PUT, DELETE,POST"
+    },
+    body: new URLSearchParams({
+      'code': code,
+      'grant_type': "authorization_code",
+      'client_id': "AT3053EB6D",
+      "client_secret": "75fa589aa7c35b89e127",
+      "redirect_uri": "http://localhost:3000/digit-ui/citizen/pt/property/new-application/info",
+      "code_verifier": getItemWithExpiry('DigiLocker.codeVerifier')
+    })
+  }) .then(response =>
+    response.json().then(data => ({
+      data: data,
+
+    })).then(res => {
+      console.log("step 1",res)
+      //code1 = "Bearer " + res.data.access_token
+      sessionStorage.setItem('DigiLocker.token1', res.data.access_token)
+      setItemWithExpiry('DigiLocker.token', res.data.access_token, 60);
+    }))
+  
+}
+},[])
+// Function to set data with an expiration time in sessionStorage
+function setItemWithExpiry(key, value, expiryMinutes) {
+  const now = new Date();
+  const expiryTime = now.getTime() + (expiryMinutes * 60 * 1000); // Convert minutes to milliseconds
+
+  const item = {
+    value: value,
+    expiry: expiryTime
+  };
+
+  sessionStorage.setItem(key, JSON.stringify(item));
+}
+
+// Function to get data from sessionStorage, checking for expiration
+function getItemWithExpiry(key) {
+  const itemString = sessionStorage.getItem(key);
+
+  if (!itemString) {
+    return null;
+  }
+
+  const item = JSON.parse(itemString);
+  const now = new Date();
+
+  if (now.getTime() > item.expiry) {
+    // Item has expired, remove it
+    sessionStorage.removeItem(key);
+    return null;
+  }
+
+  return item.value;
+}
 
   return (
     <React.Fragment>
@@ -62,6 +180,9 @@ const PropertyTax = ({ t, config, onSelect, userType, formData }) => {
         </div>
         <span>
           <SubmitBar label={t("PT_COMMON_NEXT")} onSubmit={onSelect} />
+        </span>
+        <span>
+          <SubmitBar label={t("PT_DIGILOCKER_CONSENT")} onSubmit={(e) => {onConcent(e)}} />
         </span>
       </Card>
     </React.Fragment>
