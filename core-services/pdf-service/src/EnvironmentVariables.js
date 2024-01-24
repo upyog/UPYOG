151cbec19c4f56edcad4b25aaa9f1dfcf9c93220
deepikaarora88 <deepika.arora@pwc.com>
2024-01-24 12:15:13 +0000

    reverting property file change

--- core-services/pdf-service/src/EnvironmentVariables.js
+++ core-services/pdf-service/src/EnvironmentVariables.js
@@ -2,12 +2,12 @@ const envVariables = {
   MAX_NUMBER_PAGES: process.env.MAX_NUMBER_PAGES || 80,
   EGOV_HOST: process.env.EGOV_HOST || "https://upyog-test.niua.org",
   EGOV_LOCALISATION_HOST:
-    process.env.EGOV_LOCALISATION_HOST || "http://localhost:8087",
+    process.env.EGOV_LOCALISATION_HOST || "http://egov-localization:8080",
   EGOV_LOCALISATION_SEARCH:
     process.env.EGOV_LOCALISATION_SEARCH || "/localization/messages/v2/_search",
   EGOV_FILESTORE_SERVICE_HOST:
-    process.env.EGOV_FILESTORE_SERVICE_HOST || "http://localhost:8086",
-  SERVER_PORT: process.env.SERVER_PORT || 8093,
+    process.env.EGOV_FILESTORE_SERVICE_HOST || "http://egov-filestore:8080",
+  SERVER_PORT: process.env.SERVER_PORT || 8080,
 
   KAFKA_BROKER_HOST: process.env.KAFKA_BROKER_HOST || "localhost:9092",
   KAFKA_CREATE_JOB_TOPIC:
@@ -24,13 +24,13 @@ const envVariables = {
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_NAME: process.env.DB_NAME || "PdfGen",
   DB_PORT: process.env.DB_PORT || 5432,
-  EGOV_EXTERNAL_HOST: process.env.EGOV_EXTERNAL_HOST || "http://localhost" ,
+  EGOV_EXTERNAL_HOST: process.env.EGOV_EXTERNAL_HOST || "https://dev.digit.org/" ,
   SAVE_PDF_DIR: process.env.SAVE_PDF_DIR || '/mnt/pdf/',
   DEFAULT_LOCALISATION_LOCALE:
     process.env.DEFAULT_LOCALISATION_LOCALE || "en_IN",
     DEFAULT_LOCALISATION_TENANT:
     process.env.DEFAULT_LOCALISATION_TENANT || "pg",
-    DATA_CONFIG_URLS: process.env.DATA_CONFIG_URLS || "file://C:/Users/Administrator/DIGIT/upyog_configs_forked_Dev2.0/upyog-configs/configs/pdf-service/data-config/property-receipt.json",
-    FORMAT_CONFIG_URLS: process.env.FORMAT_CONFIG_URLS || "file://C:/Users/Administrator/DIGIT/upyog_configs_forked_Dev2.0/upyog-configs/configs/pdf-service/format-config/property-receipt.json"
+    DATA_CONFIG_URLS: process.env.DATA_CONFIG_URLS,
+    FORMAT_CONFIG_URLS: process.env.FORMAT_CONFIG_URLS
 };
 export default envVariables;
