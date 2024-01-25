5121569337c488fc5543039067d6dc3a18f30040
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2024-01-25 14:40:44 +0530

    Package update

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -2,12 +2,13 @@
 
 BRANCH="$(git branch --show-current)"
 
+echo "Main Branch: $BRANCH"
+
 INTERNALS="micro-ui-internals"
 
-cd $INTERNALS && echo "installing packages" && yarn install && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
-cd ..
+cp $INTERNALS/example/src/UICustomizations.js src/Customisations
 
-rm -rf node_modules
-rm -f yarn.lock
+cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && echo "installing packages"
+
 
 # yarn install
