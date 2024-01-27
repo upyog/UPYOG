34190da38d3bf8b44924bf739ecea0701562e802
Sriranjan Srivastava <sriranjansrivastava@NIUAs-MacBook-Pro.local>
2024-01-27 12:56:37 +0530

    added digilocker changes

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/digiLockerApi/createTokenAPI.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/digiLockerApi/createTokenAPI.js
@@ -2,11 +2,10 @@ import { DigiLockerService } from "../../services/elements/DigiLocker";
 import { PTService } from "../../services/elements/PT";
 import { useMutation } from "react-query";
 
-const createTokenAPI = () => {
- 
-    return useMutation((data) => {
-      console.log("createTokenAPI",data)
-      DigiLockerService.token(data)});
-};
+const createTokenAPI = (type) => {
+
+  return useMutation((data) => {
+    DigiLockerService.token(data)});
+ }
 
 export default createTokenAPI;
