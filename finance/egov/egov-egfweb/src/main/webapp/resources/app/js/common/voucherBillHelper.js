df13b00ad76ad7c10a8bd0e876113d879351e716
khalid rashid <rashidkhalid9114@gmail.com>
2024-02-05 22:44:40 +0530

    all necessary changes done for testing

--- finance/egov/egov-egfweb/src/main/webapp/resources/app/js/common/voucherBillHelper.js
+++ finance/egov/egov-egfweb/src/main/webapp/resources/app/js/common/voucherBillHelper.js
@@ -116,13 +116,11 @@ function loadScheme(fundId){
 			method : "GET",
 			url : "/services/EGF/common/getschemesbyfundid",
 			data : {
-				fundId : fundId,
+				fundId : fundId
 			},
 			async : true
 		}).done(
 				function(response) {
-					console.log('Hello Parvesh ');
-					console.log(response);
 					$('#scheme').empty();
 					$('#scheme').append($("<option value=''>Select from below</option>"));
 					$.each(response, function(index, value) {
