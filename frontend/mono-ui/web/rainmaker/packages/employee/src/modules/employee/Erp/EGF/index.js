4d3b8390dde1fa129f26e803fdf3834e26498ddd
khalid rashid <rashidkhalid9114@gmail.com>
2024-02-06 19:38:19 +0530

    changes done 4

--- frontend/mono-ui/web/rainmaker/packages/employee/src/modules/employee/Erp/EGF/index.js
+++ frontend/mono-ui/web/rainmaker/packages/employee/src/modules/employee/Erp/EGF/index.js
@@ -28,8 +28,8 @@ class EGFFinance extends Component {
     // Reading environment name (ex: dev, qa, uat, fin-uat etc) from the globalconfigs if exists else reading from the .env file
     finEnv = this.globalConfigExists() ? window.globalConfigs.getConfig("FIN_ENV") : process.env.REACT_APP_FIN_ENV;
     // Preparing finance subdomain url using the above environment name and the domain url
-    // subdomainurl = !!(finEnv) ? "-" + finEnv + "." + domainurl : "." + domainurl;
-    erp_url = loc.protocol + "//" + getTenantId().split(".")[1] + ".localhost:8080" + menuUrl;
+    subdomainurl = !!(finEnv) ? "-" + finEnv + "." + domainurl : "." + domainurl;
+    erp_url = loc.protocol + "//" + getTenantId().split(".")[1] + subdomainurl + menuUrl;
 
     return (
       <div>
