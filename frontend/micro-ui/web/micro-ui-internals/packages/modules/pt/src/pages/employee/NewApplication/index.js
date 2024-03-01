82139b23aa30b14878b58e91de9e4622cf341289
chinmoy-eGov <chinmoy.nayak@walkingtree.tech>
2024-03-01 15:03:00 +0530

    pt

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/employee/NewApplication/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pages/employee/NewApplication/index.js
@@ -1,4 +1,4 @@
-import { FormComposer, Loader,Modal ,Card , CardHeader, StatusTable,Row } from "@egovernments/digit-ui-react-components";
+import { FormComposer, Loader } from "@egovernments/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
@@ -11,15 +11,6 @@ const NewApplication = () => {
   const [canSubmit, setSubmitValve] = useState(false);
   const defaultValues = { };
   const history = useHistory();
-  const [showToast, setShowToast] = useState(null);
-  const [searchData, setSearchData] = useState({});
-  const { data: propertyData, isLoading: propertyDataLoading, error, isSuccess, billData } = Digit.Hooks.pt.usePropertySearchWithDue({
-    tenantId: searchData?.city,
-    filters: searchData?.filters,
-    auth: true /*  to enable open search set false  */,
-    configs: { enabled: Object.keys(searchData).length > 0, retry: false, retryOnMount: false, staleTime: Infinity },
-  });
-  const [formData,setFormData]=useState({})
   // delete
   // const [_formData, setFormData,_clear] = Digit.Hooks.useSessionStorage("store-data",null);
   const [mutationHappened, setMutationHappened, clear] = Digit.Hooks.useSessionStorage("EMPLOYEE_MUTATION_HAPPENED", false);
@@ -32,8 +23,7 @@ const NewApplication = () => {
 
   const onFormValueChange = (setValue, formData, formState) => {
     setSubmitValve(!Object.keys(formState.errors).length);
-    let addressError= formData?.address?.street == "" || formData?.address?.doorNo == "" || !formData?.address?.doorNo || !formData?.address?.street || Object.keys(formState.errors).length? setSubmitValve(false): setSubmitValve(true);
-    if (Object.keys(formState.errors).length === 1 && (formState.errors?.units?.message.includes("arv")|| formState.errors?.units?.message.includes("RentedMonths") ) ){
+    if (Object.keys(formState.errors).length === 1 && formState.errors?.units?.message === "arv") {
       setSubmitValve(!formData?.units.some((unit) => unit.occupancyType === "RENTED" && !unit.arv));
     }
     if (formData?.ownershipCategory?.code?.includes("MULTIPLEOWNERS") && formData?.owners?.length < 2) {
@@ -50,11 +40,6 @@ const NewApplication = () => {
   };
 
   const onSubmit = (data) => {
-    let dataNew = data?.units?.map((value)=>{
-      let additionalDetails ={"structureType" : value.structureType,"ageOfProperty":value.ageOfProperty }
-      return {...value,additionalDetails}
-    })
-    data.units = dataNew
     const formData = {
       tenantId,
       address: {
@@ -70,15 +55,7 @@ const NewApplication = () => {
       propertyType: data?.PropertyType?.code,
       noOfFloors: Number(data?.noOfFloors),
       ownershipCategory: data?.ownershipCategory?.code,
-      additionalDetails:{
-      RentedMonths: data?.units[0]?.RentedMonths,
-      NonRentedMonthsUsage: data?.units[0]?.NonRentedMonthsUsage,
-      // ageOfProperty:data?.units[0]?.ageOfProperty,
-      // structureType:data?.units[0]?.structureType,
-      electricity:data?.electricity,
-      uid:data?.uid
-      },
-      owners: data?.owners.map((owner,index) => {
+      owners: data?.owners.map((owner) => {
         let {
           name,
           mobileNumber,
@@ -104,8 +81,6 @@ const NewApplication = () => {
             fatherOrHusbandName,
             gender: owner?.gender.code,
             emailId,
-            additionalDetails:{ownerSequence:index, ownerName:owner?.name}
-            
           };
         }
 
@@ -134,14 +109,7 @@ const NewApplication = () => {
       documents: data?.documents?.documents,
       applicationStatus: "CREATE",
     };
-    let tempObject={
-      "mobileNumber":formData.owners?.[0].mobileNumber,
-      "name":formData.owners?.[0].name,
-      "doorNo": formData.address.doorNo,
-      "locality": formData.address.locality.code,
-      "isRequestForDuplicatePropertyValidation":true
-    }
-   
+
     if (!data?.ownershipCategory?.code.includes("INDIVIDUAL")) {
       formData.institution = {
         name: data.owners?.[0].institution.name,
@@ -151,60 +119,24 @@ const NewApplication = () => {
         tenantId: Digit.ULBService.getCurrentTenantId(),
       };
     }
-  setFormData(formData)
-  setSearchData({ city: Digit.ULBService.getCurrentTenantId(), filters: tempObject });
-  };
- 
-  useEffect(() => {  
-    if(propertyDataLoading && propertyData?.Properties.length >0)  
-    {  
-      //alert("property exist"),  
-      setShowToast(true) 
-    }  
-    else if(propertyDataLoading && propertyData?.Properties.length === 0) {  
-      setShowToast(false)  
-      history.replace("/digit-ui/employee/pt/response", { Property: formData }); //current wala
-    }  
-    }, [propertyData]);
-  /* use newConfig instead of commonFields for local development in case needed */
 
-  const configs = commonFields?commonFields:newConfig;
+    history.replace("/digit-ui/employee/pt/response", { Property: formData }); //current wala
+
+  };
   if (isLoading) {
     return <Loader />;
   }
-  const Heading = (props) => {
-    return <h1 className="heading-m">{props.label}</h1>;
-  };
 
-  const Close = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF">
-      <path d="M0 0h24v24H0V0z" fill="none" />
-      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
-    </svg>
-  );
+  /* use newConfig instead of commonFields for local development in case needed */
 
-  const CloseBtn = (props) => {
-    return (
-      <div className="icon-bg-secondary" onClick={props.onClick}>
-        <Close />
-      </div>
-    );
-  };
-  const closeModal =() =>{
-    setShowToast(false)
-  }
-  const setModal=()=>{
-      setShowToast(false)   
-      history.replace("/digit-ui/employee/pt/response", { Property: formData })
-    }
+  const configs = commonFields?commonFields:newConfig;
 
   return (
-    <div>   
     <FormComposer
       heading={t("ES_TITLE_NEW_PROPERTY_APPLICATION")}
       isDisabled={!canSubmit}
       label={t("ES_COMMON_APPLICATION_SUBMIT")}
-      config={configs.map((config) => {   
+      config={configs.map((config) => {
         return {
           ...config,
           body: config.body.filter((a) => !a.hideInEmployee),
@@ -215,30 +147,6 @@ const NewApplication = () => {
       defaultValues={defaultValues}
       onFormValueChange={onFormValueChange}
     />
-    <div>
-    { showToast &&   <Modal
-    headerBarMain={<Heading label={t("CR_PROPERTY_NUMBER")} />}
-    headerBarEnd={<CloseBtn onClick={closeModal} />}
-    actionCancelLabel={"Cancel"}
-    actionCancelOnSubmit={closeModal}
-    actionSaveLabel={"Proceed"}
-    actionSaveOnSubmit={setModal}
-    formId="modal-action"
-  >  <div style={{ width: "100%" }}>
-  <Card>
-      <CardHeader>Property Details</CardHeader>
-   
-          <StatusTable>
-              <Row label={t("CR_PROPERTY_NUMBER")} text={propertyData?.Properties?.[0]?.propertyId || "NA"} textStyle={{ whiteSpace: "pre" }} />
-              <Row label={t("CR_OWNER_NAME")} text={propertyData?.Properties?.[0]?.owners?.[0].name || "NA"} />
-              <Row label={t("CR_MOBILE_NUMBER")} text={propertyData?.Properties?.[0]?.owners?.[0].mobileNumber|| "NA"} /> 
-              <Row label={t("CR_ADDRESS")}    text={( propertyData?.Properties?.[0]?.address?.doorNo +", "+ propertyData?.Properties?.[0]?.address?.locality?.name +", "+ propertyData?.Properties?.[0]?.address?.city ) || "NA"}/>      
-          </StatusTable>
-  </Card>
-</div>
-    </Modal>}
-  </div>
-  </div>
   );
 };
 
