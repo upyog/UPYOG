d5bcbb972ede956ae639df507ce1cf061a83c2d6
faizan <faizan@tattvafoundation.org>
2024-03-15 16:45:32 +0530

    http

--- finance/entrypoint.sh
+++ finance/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-metadata=$(wget https://nexus-repo.digit.org/nexus/content/groups/public/org/egov/egov-ear/3.0.0-SNAPSHOT/maven-metadata.xml)
+metadata=$(wget http://nexus-repo.digit.org/nexus/content/groups/public/org/egov/egov-ear/3.0.0-SNAPSHOT/maven-metadata.xml)
 snapshotVersion=$(sed -n 's/.*<value>\([^<]*\)<\/value>.*/\1/p' ./maven-metadata.xml 2>&1 | head -n 1)
-ear=$(wget https://nexus-repo.digit.org/nexus/content/groups/public/org/egov/egov-ear/3.0.0-SNAPSHOT//egov-ear-$snapshotVersion.ear)
+ear=$(wget http://nexus-repo.digit.org/nexus/content/groups/public/org/egov/egov-ear/3.0.0-SNAPSHOT//egov-ear-$snapshotVersion.ear)
 $(mkdir /app/egov/egov-ear/target)
 $(mv egov-ear-$snapshotVersion.ear /app/egov/egov-ear/target/)
 #echo "$snapshotVersion"
