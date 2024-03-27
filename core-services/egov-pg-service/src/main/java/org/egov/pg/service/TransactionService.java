88ff0d702aa3ebfaf28125ebd6018cd6a9763fe6
Owner <dinternationalit@gmail.com>
2024-03-27 10:26:27 +0530

    Fixed Pg Service

--- core-services/egov-pg-service/src/main/java/org/egov/pg/service/TransactionService.java
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/service/TransactionService.java
@@ -29,149 +29,154 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TransactionService {
 
-    private TransactionValidator validator;
-    private GatewayService gatewayService;
-    private Producer producer;
-    private EnrichmentService enrichmentService;
-    private AppProperties appProperties;
-    private TransactionRepository transactionRepository;
-    private PaymentsService paymentsService;
+	private TransactionValidator validator;
+	private GatewayService gatewayService;
+	private Producer producer;
+	private EnrichmentService enrichmentService;
+	private AppProperties appProperties;
+	private TransactionRepository transactionRepository;
+	private PaymentsService paymentsService;
 
+	@Autowired
+	TransactionService(TransactionValidator validator, GatewayService gatewayService, Producer producer,
+			TransactionRepository transactionRepository, PaymentsService paymentsService,
+			EnrichmentService enrichmentService, AppProperties appProperties) {
+		this.validator = validator;
+		this.gatewayService = gatewayService;
+		this.producer = producer;
+		this.transactionRepository = transactionRepository;
+		this.paymentsService = paymentsService;
+		this.enrichmentService = enrichmentService;
+		this.appProperties = appProperties;
+	}
 
+	/**
+	 * Initiates a transaction by generating a gateway redirect URI for the request
+	 * <p>
+	 * 1. Validates transaction object 2. Enriches the request by assigning a TxnID
+	 * and a default status of PENDING 3. If yes, calls the gateway's implementation
+	 * to generate a redirect URI 4. Persists the transaction and a transaction dump
+	 * with the RAW requests asynchronously 5. Returns the redirect URI
+	 *
+	 * @param transactionRequest Valid transaction request for which transaction
+	 *                           needs to be initiated
+	 * @return Redirect URI to the gateway for the particular transaction
+	 */
+	public Transaction initiateTransaction(TransactionRequest transactionRequest) {
+		validator.validateCreateTxn(transactionRequest);
 
-    @Autowired
-    TransactionService(TransactionValidator validator, GatewayService gatewayService, Producer producer,
-                       TransactionRepository
-                               transactionRepository, PaymentsService paymentsService,
-                       EnrichmentService enrichmentService,
-                       AppProperties appProperties) {
-        this.validator = validator;
-        this.gatewayService = gatewayService;
-        this.producer = producer;
-        this.transactionRepository = transactionRepository;
-        this.paymentsService = paymentsService;
-        this.enrichmentService = enrichmentService;
-        this.appProperties = appProperties;
-    }
+		// Enrich transaction by generating txnid, audit details, default status
+		enrichmentService.enrichCreateTransaction(transactionRequest);
 
-    /**
-     * Initiates a transaction by generating a gateway redirect URI for the request
-     * <p>
-     * 1. Validates transaction object
-     * 2. Enriches the request by assigning a TxnID and a default status of PENDING
-     * 3. If yes, calls the gateway's implementation to generate a redirect URI
-     * 4. Persists the transaction and a transaction dump with the RAW requests asynchronously
-     * 5. Returns the redirect URI
-     *
-     * @param transactionRequest Valid transaction request for which transaction needs to be initiated
-     * @return Redirect URI to the gateway for the particular transaction
-     */
-    public Transaction initiateTransaction(TransactionRequest transactionRequest) {
-        validator.validateCreateTxn(transactionRequest);
+		RequestInfo requestInfo = transactionRequest.getRequestInfo();
+		Transaction transaction = transactionRequest.getTransaction();
 
-        // Enrich transaction by generating txnid, audit details, default status
-        enrichmentService.enrichCreateTransaction(transactionRequest);
+		TransactionDump dump = TransactionDump.builder().txnId(transaction.getTxnId())
+				.auditDetails(transaction.getAuditDetails()).build();
 
-        RequestInfo requestInfo = transactionRequest.getRequestInfo();
-        Transaction transaction = transactionRequest.getTransaction();
+		if (validator.skipGateway(transaction)) {
+			transaction.setTxnStatus(Transaction.TxnStatusEnum.SUCCESS);
+			paymentsService.registerPayment(transactionRequest);
+		} else {
+			URI uri = gatewayService.initiateTxn(transaction);
+			transaction.setRedirectUrl(uri.toString());
+			if (uri.getRawQuery() != null && transaction.getGateway().equals("RAZORPAY")) {
+				String param = uri.getRawQuery();
+				String[] orderId = param.split("orderId=");
+				if (orderId != null && orderId.length > 1)
+					transaction.setGatewayTxnId(orderId[1]);
+			}
+			dump.setTxnRequest(uri.toString());
+		}
 
-        TransactionDump dump = TransactionDump.builder()
-                .txnId(transaction.getTxnId())
-                .auditDetails(transaction.getAuditDetails())
-                .build();
+		// Persist transaction and transaction dump objects
+		producer.push(appProperties.getSaveTxnTopic(),
+				new org.egov.pg.models.TransactionRequest(requestInfo, transaction));
+		producer.push(appProperties.getSaveTxnDumpTopic(), new TransactionDumpRequest(requestInfo, dump));
 
-        if(validator.skipGateway(transaction)){
-            transaction.setTxnStatus(Transaction.TxnStatusEnum.SUCCESS);
-            paymentsService.registerPayment(transactionRequest);
-        }
-        else{
-            URI uri = gatewayService.initiateTxn(transaction);
-            transaction.setRedirectUrl(uri.toString());
-if(uri.getRawQuery()!=null&&transaction.getGateway().equals("RAZORPAY")){
-Stringparam=uri.getRawQuery();
-String[]orderId=param.split("orderId=");
-if(orderId!=null&&orderId.length>1)
-transaction.setGatewayTxnId(orderId[1]);
-}
-            dump.setTxnRequest(uri.toString());
-        }
+		if (validator.skipGateway(transaction)) {
+			transaction.setTxnStatus(Transaction.TxnStatusEnum.SUCCESS);
+			paymentsService.registerPayment(transactionRequest);
+		} else {
+			URI uri = gatewayService.initiateTxn(transaction);
+			transaction.setRedirectUrl(uri.toString());
+			if (uri.getRawQuery() != null && transaction.getGateway().equals("RAZORPAY")) {
+				String param = uri.getRawQuery();
+				String[] orderId = param.split("orderId=");
+				if (orderId != null && orderId.length > 1)
+					transaction.setGatewayTxnId(orderId[1]);
+			}
+			dump.setTxnRequest(uri.toString());
+		}
 
-        // Persist transaction and transaction dump objects
-        producer.push(appProperties.getSaveTxnTopic(), new org.egov.pg.models.TransactionRequest
-                (requestInfo, transaction));
-        producer.push(appProperties.getSaveTxnDumpTopic(), new TransactionDumpRequest(requestInfo, dump));
+		return transaction;
+	}
 
-        return transaction;
-    }
+	/**
+	 * Fetches a list of transactions matching the current criteria
+	 * <p>
+	 * Currently has a hard limit of 5 records, configurable
+	 *
+	 * @param transactionCriteria Search Conditions that should be matched
+	 * @return List of transactions matching the conditions.
+	 */
+	public List<Transaction> getTransactions(TransactionCriteria transactionCriteria) {
+		log.info(transactionCriteria.toString());
+		try {
+			return transactionRepository.fetchTransactions(transactionCriteria);
+		} catch (DataAccessException e) {
+			log.error("Unable to fetch data from the database for criteria: " + transactionCriteria.toString(), e);
+			throw new CustomException("FETCH_TXNS_FAILED", "Unable to fetch transactions from store");
+		}
+	}
 
+	/**
+	 * Updates the status of the transaction from the gateway
+	 * <p>
+	 * 1. Fetch TXN ID from the request params, if not found, exit! 2. Fetch current
+	 * transaction status from DB, if not found, exit! 3. Fetch the current
+	 * transaction status from the payment gateway 4. Verify the amount returned
+	 * from the gateway matches our records 5. If successful, generate receipt 6.
+	 * Persist the updated transaction status and raw gateway transaction response
+	 *
+	 * @param requestInfo
+	 * @param requestParams Response parameters posted by the gateway
+	 * @return Updated transaction
+	 */
+	public List<Transaction> updateTransaction(RequestInfo requestInfo, Map<String, String> requestParams) {
 
-    /**
-     * Fetches a list of transactions matching the current criteria
-     * <p>
-     * Currently has a hard limit of 5 records, configurable
-     *
-     * @param transactionCriteria Search Conditions that should be matched
-     * @return List of transactions matching the conditions.
-     */
-    public List<Transaction> getTransactions(TransactionCriteria transactionCriteria) {
-        log.info(transactionCriteria.toString());
-        try {
-            return transactionRepository.fetchTransactions(transactionCriteria);
-        } catch (DataAccessException e) {
-            log.error("Unable to fetch data from the database for criteria: " + transactionCriteria.toString(), e);
-            throw new CustomException("FETCH_TXNS_FAILED", "Unable to fetch transactions from store");
-        }
-    }
+		Transaction currentTxnStatus = validator.validateUpdateTxn(requestParams);
 
-    /**
-     * Updates the status of the transaction from the gateway
-     * <p>
-     * 1. Fetch TXN ID from the request params, if not found, exit!
-     * 2. Fetch current transaction status from DB, if not found, exit!
-     * 3. Fetch the current transaction status from the payment gateway
-     * 4. Verify the amount returned from the gateway matches our records
-     * 5. If successful, generate receipt
-     * 6. Persist the updated transaction status and raw gateway transaction response
-     *
-     * @param requestInfo
-     * @param requestParams Response parameters posted by the gateway
-     * @return Updated transaction
-     */
-    public List<Transaction> updateTransaction(RequestInfo requestInfo, Map<String, String> requestParams) {
+		log.debug(currentTxnStatus.toString());
+		log.debug(requestParams.toString());
 
-        Transaction currentTxnStatus = validator.validateUpdateTxn(requestParams);
+		Transaction newTxn = null;
 
-        log.debug(currentTxnStatus.toString());
-        log.debug(requestParams.toString());
+		if (validator.skipGateway(currentTxnStatus)) {
+			newTxn = currentTxnStatus;
 
-        Transaction newTxn = null;
+		} else {
+			newTxn = gatewayService.getLiveStatus(currentTxnStatus, requestParams);
 
-        if(validator.skipGateway(currentTxnStatus)) {
-            newTxn = currentTxnStatus;
+			// Enrich the new transaction status before persisting
+			enrichmentService.enrichUpdateTransaction(new TransactionRequest(requestInfo, currentTxnStatus), newTxn);
+		}
 
-        } else{
-            newTxn = gatewayService.getLiveStatus(currentTxnStatus, requestParams);
+		// Check if transaction is successful, amount matches etc
+		if (validator.shouldGenerateReceipt(currentTxnStatus, newTxn)) {
+			TransactionRequest request = TransactionRequest.builder().requestInfo(requestInfo).transaction(newTxn)
+					.build();
+			paymentsService.registerPayment(request);
+		}
 
-            // Enrich the new transaction status before persisting
-            enrichmentService.enrichUpdateTransaction(new TransactionRequest(requestInfo, currentTxnStatus), newTxn);
-        }
+		TransactionDump dump = TransactionDump.builder().txnId(currentTxnStatus.getTxnId())
+				.txnResponse(newTxn.getResponseJson()).auditDetails(newTxn.getAuditDetails()).build();
 
-        // Check if transaction is successful, amount matches etc
-        if (validator.shouldGenerateReceipt(currentTxnStatus, newTxn)) {
-        	TransactionRequest request = TransactionRequest.builder().requestInfo(requestInfo).transaction(newTxn).build();
-        	paymentsService.registerPayment(request);
-        }
+		producer.push(appProperties.getUpdateTxnTopic(),
+				new org.egov.pg.models.TransactionRequest(requestInfo, newTxn));
+		producer.push(appProperties.getUpdateTxnDumpTopic(), new TransactionDumpRequest(requestInfo, dump));
 
-        TransactionDump dump = TransactionDump.builder()
-                .txnId(currentTxnStatus.getTxnId())
-                .txnResponse(newTxn.getResponseJson())
-                .auditDetails(newTxn.getAuditDetails())
-                .build();
-
-        producer.push(appProperties.getUpdateTxnTopic(), new org.egov.pg.models.TransactionRequest(requestInfo, newTxn));
-        producer.push(appProperties.getUpdateTxnDumpTopic(), new TransactionDumpRequest(requestInfo, dump));
-
-        return Collections.singletonList(newTxn);
-    }
+		return Collections.singletonList(newTxn);
+	}
 
 }
