8ef0911e1280cd99e210fd4a4b69597a28e3c705
Owner <dinternationalit@gmail.com>
2024-04-03 15:15:40 +0530

    User Pom

--- business-services/billing-service/src/main/java/org/egov/demand/config/ApplicationProperties.java
+++ business-services/billing-service/src/main/java/org/egov/demand/config/ApplicationProperties.java
@@ -195,4 +195,17 @@ public class ApplicationProperties {
 
 	@Value("${statelevel.rolecode.excluded.list}")
 	private List<String> statelevelRolecodeExclusionList;
+    
+    @Value("${egov.shortener.url}")
+    private String egovShortenerUrl;
+    
+    @Value("${notification.url}")
+	private String notificationUrl;
+    
+    @Value("${notification.sms.link}")
+	private String smsNotificationLink;
+
+    @Value("${egov.shortener.host}")
+   	private String shortenerHost;
+    
 }
