3cd266c523061b9bce8f1973105fea0a28202ade
anujkit <46367271+anujkit@users.noreply.github.com>
2024-04-23 11:43:59 +0530

    FireNoc Calculator Changes
    FireNoc Calculator Changes

--- municipal-services/firenoc-calculator/src/controller/search.js
+++ municipal-services/firenoc-calculator/src/controller/search.js
@@ -34,7 +34,7 @@ const search = async (req, res, pool, next) => {
 export default search;
 
 export const searchService = async (reqestCriteria, searchResponse, pool) => {
-  const querystring = generateQuery(reqestCriteria);
+  const  querystring = generateQuery(reqestCriteria);
   let billingSlabs = [];
   billingSlabs = await pool
     .query(querystring)
@@ -50,58 +50,60 @@ const popolateSearchResponse = result => {
   let BillingSlabs = [];
   result.rows.map(rowData => {
     const billingSlab = {};
-    billingSlab.tenantId = rowData.tenantid;
+   
     billingSlab.id = rowData.id;
     billingSlab.isActive = rowData.isactive;
     billingSlab.fireNOCType = rowData.firenoctype;
     billingSlab.buildingUsageType = rowData.buildingusagetype;
     billingSlab.calculationType = rowData.calculationtype;
     billingSlab.rate = Number(rowData.rate);
-    billingSlab.uom = rowData.uom;
-    billingSlab.fromUom = rowData.fromuom;
-    billingSlab.toUom = rowData.touom;
-    billingSlab.fromDate = rowData.fromuom;
+    billingSlab.heightuom = rowData.heightuom;
+    billingSlab.heightfromUom = rowData.heightfromuom;
+    billingSlab.heighttoUom = rowData.heighttouom;
+    billingSlab.areauom = rowData.areauom;
+    billingSlab.areafromUom = rowData.areafromuom;
+    billingSlab.areatoUom = rowData.areatouom;
+    billingSlab.fromDate = rowData.fromdate;
     billingSlab.toDate = rowData.todate;
-    billingSlab.auditDetails = {};
-    billingSlab.auditDetails.createdBy = rowData.createdby;
-    billingSlab.auditDetails.createdDate = Number(rowData.createddate);
-    billingSlab.auditDetails.lastModifiedBy = rowData.lastmodifiedby;
-    billingSlab.auditDetails.lastModifiedDate = Number(
-      rowData.lastmodifieddate
-    );
+    billingSlab.maxfee = rowData.maxfee;
+    billingSlab.minfee = rowData.minfee;
+    billingSlab.feeperunitrate = rowData.feeperunitrate;
+    billingSlab.new_percentage = rowData.new_percentage;
+    billingSlab.renew_percentage = rowData.renew_percentage;
+    billingSlab.provisional_percentage = rowData.provisional_percentage;
+    billingSlab.slab_description = rowData.slab_description;
+
+    // billingSlab.id = '0ce2dd8789d3aa634c4e81599824822b';
+    // billingSlab.isActive = 1;
+    // billingSlab.fireNOCType = null;
+    // billingSlab.buildingUsageType = 'BANK.BANK';
+    // billingSlab.calculationType = 'FLAT';
+    // billingSlab.rate = 20000;
+    // billingSlab.heightuom = 'MTR';
+    // billingSlab.heightfromUom = 0;
+    // billingSlab.heighttoUom = 100;
+    // billingSlab.areauom = 'ACRE';
+    // billingSlab.areafromUom = 0.00;
+    // billingSlab.areatoUom = 0.99;
+    // billingSlab.fromDate = null;
+    // billingSlab.toDate = null;
+    // billingSlab.maxfee = null;
+    // billingSlab.minfee = null;
+    // billingSlab.feeperunitrate = 0;
+    // billingSlab.new_percentage = 100;
+    // billingSlab.renew_percentage = 50;
+    // billingSlab.provisional_percentage = 50;
+    // billingSlab.slab_description = 'Rs. 20000 lumpsum';
+   
     BillingSlabs.push(billingSlab);
   });
   return BillingSlabs;
 };
 
 const generateQuery = params => {
+let height=params.height;
+let usagetype=params.buildingUsageType; let AREA_SQYD=params.areaSQYD; let AREA_ACRE=params.areaACRE;
   let queryString =
-    "select tenantid, id, isactive , firenoctype, buildingusagetype, calculationtype, uom, fromuom, touom, fromdate, todate, rate, createdby, createddate, lastmodifiedby, lastmodifieddate from eg_firenoc_billingslab where ";
-  queryString = `${queryString} tenantid = '${params.tenantId}'`;
-  if (params.hasOwnProperty("isActive")) {
-    queryString = `${queryString} and isactive = ${params.isActive}`;
-  } else queryString = `${queryString} and  isactive = true`;
-
-  if (params.hasOwnProperty("fireNOCType")) {
-    queryString = `${queryString} and firenoctype = '${params.fireNOCType}'`;
-  }
-  if (params.hasOwnProperty("buildingUsageType")) {
-    queryString = `${queryString} and buildingusagetype = '${
-      params.buildingUsageType
-    }'`;
-  }
-  if (params.hasOwnProperty("calculationType")) {
-    queryString = `${queryString} and calculationtype = '${
-      params.calculationType
-    }'`;
-  }
-  if (params.hasOwnProperty("uom")) {
-    queryString = `${queryString} and uom = '${params.uom}'`;
-  }
-  if (params.hasOwnProperty("uomValue")) {
-    queryString = `${queryString} and fromuom < ${
-      params.uomValue
-    } and touom >= ${params.uomValue} `;
-  }
+  "SELECT * FROM eg_firenoc_state_billingslab WHERE (" + height + " BETWEEN heightfromuom AND heighttouom) AND buildingusagetype='" + usagetype + "' AND ((CASE WHEN areauom='SQYD' THEN " + AREA_SQYD + " BETWEEN areafromuom AND areatouom END) OR (CASE WHEN areauom='ACRE' THEN " + AREA_ACRE + " BETWEEN areafromuom AND areatouom END))";
   return queryString;
 };
