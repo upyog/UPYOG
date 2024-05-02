e4d192ccff1374e94170e51f1daeee6157eee185
chinmoy-eGov <115543497+chinmoy-eGov@users.noreply.github.com>
2024-05-02 12:46:23 +0530

    Update consumer.js

--- municipal-services/firenoc-services/src/kafka/consumer.js
+++ municipal-services/firenoc-services/src/kafka/consumer.js
@@ -40,145 +40,129 @@ const run = async () => {
         let events = [];
         let { RequestInfo } = value;
 
-        const sendEventNotificaiton = (tenantId) => {
-          let requestPayload = {
-            RequestInfo,
-            events
-          };
-
-          let topic = envVariables.KAFKA_TOPICS_EVENT_NOTIFICATION;
-          var isCentralInstance  = envVariables.IS_ENVIRONMENT_CENTRAL_INSTANCE;
-          if(typeof isCentralInstance =="string")
-            isCentralInstance = (isCentralInstance.toLowerCase() == "true");
-
-      let ownerName = get(
-        FireNOCs[i],
-        "fireNOCDetails.applicantDetails.owners.0.name"
-      );
-      let uuid = get(
-        FireNOCs[i],
-        "fireNOCDetails.applicantDetails.owners.0.uuid"
-      );
-      let applicationNumber = get(
-        FireNOCs[i],
-        "fireNOCDetails.applicationNumber"
-      );
-      let fireNOCNumber = get(FireNOCs[i], "fireNOCDetails.validTo");
-      let validTo = get(FireNOCs[i], "fireNOCDetails.validTo");
-      let tenantId = get(FireNOCs[i], "tenantId");
-      switch (FireNOCs[i].fireNOCDetails.status) {
-        case "INITIATED":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} has been generated. Your application no. is ${applicationNumber}.\n\nEGOVS`;
-          break;
-        case "PENDINGPAYMENT":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} has been submitted. Your application no. is ${applicationNumber}. Please pay your NoC Fees online or at your applicable fire office\n\nEGOVS`;
-          break;
-        case "DOCUMENTVERIFY":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been forwarded for document verifier.\n\nEGOVS`;
-          break;
-        case "FIELDINSPECTION":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been forwarded for field inpsection.\n\nEGOVS`;
-          break;
-        case "PENDINGAPPROVAL":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been forwarded for approver.\n\nEGOVS`;
-          break;
-        case "APPROVED":
-          var currentDate = new Date(validTo);
-          var date = currentDate.getDate();
-          var month = currentDate.getMonth(); //Be careful! January is 0 not 1
-          var year = currentDate.getFullYear();
-          if(isCentralInstance)
-            topic = getStateSpecificTopicName(tenantId, topic);
-
-          payloads.push({
-            topic: topic,
-            messages: JSON.stringify(requestPayload)
-          });
-          // httpRequest({
-          //   hostURL: envVariables.EGOV_EVENT_HOST,
-          //   endPoint: `${envVariables.EGOV_EVENT_CONTEXT_PATH}${envVariables.EGOV_EVENT_CREATE_ENPOINT}`,
-          //   requestPayload
-          // }).then(
-          //   function(response) {
-          //     console.log(response);
-          //   },
-          //   function(error) {
-          //     console.log(error);
-          //   }
-          // );
+      const sendEventNotificaiton = (tenantId) => {
+        let requestPayload = {
+          RequestInfo,
+          events
         };
 
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} is approved.And your fire NoC has been generated.Your Fire NoC No. is ${fireNOCNumber}. It is valid till ${dateString}\n\nEGOVS`;
-          break;
-        case "CITIZENACTIONREQUIRED-DV":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName}, 
-          Your application for ${firenocType} Fire NOC Certificate with application no. ${applicationNumber} is send back to you for further actions.Please check the comments and Re-submit application through mSeva App or by ULB counter.\n\nEGOVS`;
-          break;
-        case "CITIZENACTIONREQUIRED":
-          smsRequest[
-             "message"
-          ] = `Dear ${ownerName}, 
-          Your application for ${firenocType} Fire NOC Certificate with application no. ${applicationNumber} is send back to you for further actions.Please check the comments and Re-submit application through mSeva App or by ULB counter.\n\nEGOVS`;
-          break;
-        case "REJECTED":
-          smsRequest[
-            "message"
-          ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been rejected.To know more details please contact your applicable fire office\n\nEGOVS`;
-          break;
-        // case "CANCELLED":
-        //   break;
-        default:
-      }
-      payloads.push({
-        topic,
-        messages: JSON.stringify(smsRequest)
-      });
-      // console.log("smsRequest",smsRequest);
-      if (smsRequest.message) {
-        events.push({
-          tenantId: tenantId,
-          eventType: "SYSTEMGENERATED",
-          description: smsRequest.message,
-          name: "Firenoc notification",
-          source: "webapp",
-          recepient: {
-            toUsers: [uuid]
-          }
+        let topic = envVariables.KAFKA_TOPICS_EVENT_NOTIFICATION;
+        var isCentralInstance = envVariables.IS_ENVIRONMENT_CENTRAL_INSTANCE;
+        if (typeof isCentralInstance == "string")
+          isCentralInstance = (isCentralInstance.toLowerCase() == "true");
+
+        let ownerName = get(
+          FireNOCs[i],
+          "fireNOCDetails.applicantDetails.owners.0.name"
+        );
+        let uuid = get(
+          FireNOCs[i],
+          "fireNOCDetails.applicantDetails.owners.0.uuid"
+        );
+        let applicationNumber = get(
+          FireNOCs[i],
+          "fireNOCDetails.applicationNumber"
+        );
+        let fireNOCNumber = get(FireNOCs[i], "fireNOCDetails.validTo");
+        let validTo = get(FireNOCs[i], "fireNOCDetails.validTo");;
+        let tenantId = get(FireNOCs[i], "tenantId");
+        switch (FireNOCs[i].fireNOCDetails.status) {
+          case "INITIATED":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} has been generated. Your application no. is ${applicationNumber}.\n\nEGOVS`;
+            break;
+          case "PENDINGPAYMENT":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} has been submitted. Your application no. is ${applicationNumber}. Please pay your NoC Fees online or at your applicable fire office\n\nEGOVS`;
+            break;
+          case "DOCUMENTVERIFY":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been forwarded for document verifier.\n\nEGOVS`;
+            break;
+          case "FIELDINSPECTION":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been forwarded for field inpsection.\n\nEGOVS`;
+            break;
+          case "PENDINGAPPROVAL":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been forwarded for approver.\n\nEGOVS`;
+            break;
+          case "APPROVED":
+            var currentDate = new Date(validTo);
+            var date = currentDate.getDate();
+            var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+            var year = currentDate.getFullYear();
+            if(isCentralInstance)
+              topic = getStateSpecificTopicName(tenantId, topic);
+
+            payloads.push({
+              topic: topic,
+              messages: JSON.stringify(requestPayload)
+            });
+            // httpRequest({
+            //   hostURL: envVariables.EGOV_EVENT_HOST,
+            //   endPoint: `${envVariables.EGOV_EVENT_CONTEXT_PATH}${envVariables.EGOV_EVENT_CREATE_ENPOINT}`,
+            //   requestPayload
+            // }).then(
+            //   function(response) {
+            //     console.log(response);
+            //   },
+            //   function(error) {
+            //     console.log(error);
+            //   }
+            // );
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} is approved.And your fire NoC has been generated.Your Fire NoC No. is ${fireNOCNumber}. It is valid till ${dateString}\n\nEGOVS`;
+            break;
+          case "CITIZENACTIONREQUIRED-DV":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName}, 
+            Your application for ${firenocType} Fire NOC Certificate with application no. ${applicationNumber} is send back to you for further actions.Please check the comments and Re-submit application through mSeva App or by ULB counter.\n\nEGOVS`;
+            break;
+          case "CITIZENACTIONREQUIRED":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName}, 
+            Your application for ${firenocType} Fire NOC Certificate with application no. ${applicationNumber} is send back to you for further actions.Please check the comments and Re-submit application through mSeva App or by ULB counter.\n\nEGOVS`;
+            break;
+          case "REJECTED":
+            smsRequest[
+              "message"
+            ] = `Dear ${ownerName},Your application for ${firenocType} with application no. is ${applicationNumber} has been rejected.To know more details please contact your applicable fire office\n\nEGOVS`;
+            break;
+          // case "CANCELLED":
+          //   break;
+          default:
+        }
+        payloads.push({
+          topic,
+          messages: JSON.stringify(smsRequest)
         });
+        // console.log("smsRequest",smsRequest);
+        if (smsRequest.message) {
+          events.push({
+            tenantId: tenantId,
+            eventType: "SYSTEMGENERATED",
+            description: smsRequest.message,
+            name: "Firenoc notification",
+            source: "webapp",
+            recepient: {
+              toUsers: [uuid]
+            }
+          });
+        }
+        if (events.length > 0) {
+          sendEventNotificaiton();
+        }
       }
-    }
-    // console.log("events",events);
-    if (events.length > 0) {
-      sendEventNotificaiton();
-    }
-  };
-  const FireNOCPaymentStatus = async value => {
-    try {
-      //console.log("Consumer Payment data"+JSON.stringify(value));
-      const { Payment, RequestInfo } = value;
-      let tenantId = get(Payment, "tenantId");
-      const { paymentDetails } = Payment;
-      if (paymentDetails) {
-        for (var index = 0; index < paymentDetails.length; index++) {
-          let businessService = get(paymentDetails[index], "businessService");
-          if (businessService === envVariables.BUSINESS_SERVICE) {
-            let applicationNumber = get(
-              paymentDetails[index],
-              "bill.consumerCode"
+
         const sendFireNOCSMSRequest = async FireNOCs => {
           let tenantId = get(FireNOCs[0], "tenantId");
           for (let i = 0; i < FireNOCs.length; i++) {
@@ -326,6 +310,7 @@ const run = async () => {
           });
 
         };
+        
         const FireNOCPaymentStatus = async value => {
           try {
             //console.log("Consumer Payment data"+JSON.stringify(value));
