47f722bb31c8803908fe70a27abc1da032d553a7
palamgarg <palamgarg@gmail.com>
2024-05-31 10:36:45 +0530

    updated SQl

--- business-services/billing-service/src/main/resources/db/migration/main/V20170712133510__egbs_demand_base_update_ddl.sql
+++ business-services/billing-service/src/main/resources/db/migration/main/V20170712133510__egbs_demand_base_update_ddl.sql
@@ -1,3 +1,3 @@
-ALTER TABLE  egbs_demand drop CONSTRAINT IF  EXISTS uk_egbs_demand_consumercode_businessservice ;
+--ALTER TABLE  egbs_demand drop CONSTRAINT IF  EXISTS uk_egbs_demand_consumercode_businessservice ;
 
-ALTER TABLE  egbs_demand add CONSTRAINT uk_egbs_demand_consumercode_businessservice UNIQUE (consumercode,businessservice);
+--ALTER TABLE  egbs_demand add CONSTRAINT uk_egbs_demand_consumercode_businessservice UNIQUE (consumercode,businessservice);
