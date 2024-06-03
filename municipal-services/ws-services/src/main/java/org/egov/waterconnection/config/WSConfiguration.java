1cf0a97ebeaeb551d94ebccbb1ece02c8373cab9
vinothrallapalli-eGov <vinoth.r@egovernments.org>
2024-06-03 16:27:42 +0530

    Revert "Reverting mdms v2 endpoint changes in municipal services"

--- municipal-services/ws-services/src/main/java/org/egov/waterconnection/config/WSConfiguration.java
+++ municipal-services/ws-services/src/main/java/org/egov/waterconnection/config/WSConfiguration.java
@@ -219,12 +219,18 @@ public class WSConfiguration {
     private String stateLevelTenantId;
 
     //mdms
-    @Value("${egov.mdms.host}")
+//    @Value("${egov.mdms.host}")
+//    private String mdmsHost;
+//
+//    @Value("${egov.mdms.search.endpoint}")
+//    private String mdmsUrl;
+    
+	@Value("${mdms.v2.host}")
     private String mdmsHost;
 
-    @Value("${egov.mdms.search.endpoint}")
+    @Value("${mdms.v2.search.endpoint}")
     private String mdmsUrl;
-    
+
     @Value("${egov.disconnect.businessservice}")
     private String disconnectBusinessServiceName;
 
