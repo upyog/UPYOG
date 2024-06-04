31816ea3320fd44f74b0e3e2f4908d4e68816ea9
chinmoy-eGov <115543497+chinmoy-eGov@users.noreply.github.com>
2024-06-04 15:09:26 +0530

    merge 2.9 frontend code

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -37,7 +37,7 @@ import { initReceiptsComponents, ReceiptsModule } from "@egovernments/digit-ui-m
 import { initOBPSComponents } from "@upyog/digit-ui-module-obps";
 import { initNOCComponents } from "@egovernments/digit-ui-module-noc";
 import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
-import { initWSComponents } from @egovernments/digit-ui-module-dss;
+import { initWSComponents } from "@egovernments/digit-ui-module-ws";
 import { initCustomisationComponents } from "./Customisations";
 import { initCommonPTComponents } from "@egovernments/digit-ui-module-commonpt";
 import { initBillsComponents } from "@egovernments/digit-ui-module-bills";
