92feb1ec9adbbb30c093b5c14687ec55e85c2e57
erlove000 <145665604+erlove000@users.noreply.github.com>
2024-06-05 15:33:52 +0530

    Update PaymentService(receipt)

--- business-services/collection-services/src/main/java/org/egov/collection/service/PaymentService.java
+++ business-services/collection-services/src/main/java/org/egov/collection/service/PaymentService.java
@@ -103,17 +103,21 @@ public class PaymentService {
      	 // if(businessservice.equals("WS")||businessservice.equals("SW")) {
        //       setPropertyData(receiptnumber,payments);
        //   }
-// 	if((businessservice.equals("WS")||businessservice.equals("SW") )|| payments.get(0).getAddress().isEmpty()) {
-//   	 List<String> usageCategory = paymentRepository.fetchUsageCategoryByApplicationnos(paymentSearchCriteria.getReceiptNumbers(),businessservice);
-//          List<String> address = paymentRepository.fetchAddressByApplicationnos(paymentSearchCriteria.getReceiptNumbers(),businessservice);
-//          List<String> propertyIds = paymentRepository.fetchPropertyid(paymentSearchCriteria.getReceiptNumbers(), businessservice);
+ if((businessservice.equals("WS")||businessservice.equals("SW") ) && payments.get(0).getAddress()==null) {
+   	  List<String> usageCategory = paymentRepository.fetchUsageCategoryByApplicationnos(paymentSearchCriteria.getReceiptNumbers(),businessservice);
+          List<String> address = paymentRepository.fetchAddressByApplicationnos(paymentSearchCriteria.getReceiptNumbers(),businessservice);
+          List<String> propertyIds = paymentRepository.fetchPropertyid(paymentSearchCriteria.getReceiptNumbers(), businessservice);
+          System.out.println("Receipt Number: " + receiptnumber);
+          System.out.println("Payments: " + payments);   
+          setPropertyData(receiptnumber,payments);
+          payments.get(0).setUsageCategory(usageCategory.get(0));
+   	  payments.get(0).setAddress(address.get(0));
+          payments.get(0).setPropertyId(propertyIds.get(0));
 
-// //           setPropertyData(receiptnumber,payments);
-//          payments.get(0).setUsageCategory(usageCategory.get(0));
-//   		 payments.get(0).setAddress(address.get(0));
-//   		payments.get(0).setPropertyId(propertyIds.get(0));
-
-//    }
+    }
+ 	else {
+ 		  return payments;
+ 	}
 	}
         return payments;
     }
