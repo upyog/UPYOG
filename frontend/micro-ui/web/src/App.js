94fde6a990242dd515efc1f5dc9a65567ed6fcb2
chinmoy-eGov <115543497+chinmoy-eGov@users.noreply.github.com>
2024-06-05 10:15:07 +0530

    merge 2.9 frontend code

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -11,7 +11,7 @@ import {
   PTComponents,
 } from "@egovernments/digit-ui-module-pt";
 import { MCollectModule, MCollectLinks, initMCollectComponents } from "@egovernments/digit-ui-module-mcollect";
-import { initDSSComponents } from "@upyog/digit-ui-module-dss";
+import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
 import {
   PaymentModule,
   PaymentLinks,
