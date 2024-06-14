03231e5569bc57d843626800a251299e1d21d5ce
Amardeep Gumber <45850454+deepgumber9996@users.noreply.github.com>
2024-06-14 16:22:17 +0530

    Update BillGenerationValidator.java

--- municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/validator/BillGenerationValidator.java
+++ municipal-services/ws-calculator/src/main/java/org/egov/wscalculation/validator/BillGenerationValidator.java
@@ -46,11 +46,46 @@ public class BillGenerationValidator {
 				billScheduler.getBillingcycleStartdate(), billScheduler.getBillingcycleEnddate(),
 				billScheduler.getTenantId());
 
-		if (status.contains("INITIATED") || status.contains("INPROGRESS") || status.contains("COMPLETED")) {
+		if (status.contains("INITIATED") || status.contains("INPROGRESS")) {
 
 			throw new CustomException(WSCalculationConstant.WS_DUPLICATE_BILL_SCHEDULER,
 					WSCalculationConstant.WS_DUPLICATE_BILL_SCHEDULER_MSG + billScheduler.getLocality());
 		}
 	}
+
+	public boolean checkBillingCycleDates(BillGenerationReq billGenerationReq, RequestInfo requestInfo) {
+		 boolean checkBillingStatus = false;
+		Map<String, Object> billingMasterData = calculatorUtils.loadBillingFrequencyMasterData(requestInfo,
+				billGenerationReq.getBillScheduler().getTenantId());
+
+		if (billingMasterData.get("taxPeriodFrom") == null || billingMasterData.get("taxPeriodTo") == null) {
+			throw new CustomException(WSCalculationConstant.WS_NO_BILLING_PERIOD_MSG,
+					WSCalculationConstant.WS_NO_BILLING_PERIOD_MSG);
+		}
+		long taxPeriodFrom = (long) billingMasterData.get("taxPeriodFrom");
+		long taxPeriodTo = (long) billingMasterData.get("taxPeriodTo");
+
+		billGenerationReq.getBillScheduler().setBillingcycleStartdate(taxPeriodFrom);
+		billGenerationReq.getBillScheduler().setBillingcycleEnddate(taxPeriodTo);
+		checkBillingStatus = validateExistingScheduledBillStatusForBatch(billGenerationReq);	
+		return checkBillingStatus;
+	}
+
+	private boolean validateExistingScheduledBillStatusForBatch(BillGenerationReq billGenerationReq) {
+		 boolean checkBillingStatus = false;
+		BillScheduler billScheduler = billGenerationReq.getBillScheduler();
+		List<String> status = billGeneratorDao.fetchExistingBillSchedularStatusForLocality(billScheduler.getLocality(),
+				billScheduler.getBillingcycleStartdate(), billScheduler.getBillingcycleEnddate(),
+				billScheduler.getTenantId());
+
+		
+		if (status.contains("INITIATED") || status.contains("INPROGRESS")) {			
+			checkBillingStatus = true;
+	
+	}
+		return checkBillingStatus;
+	}
+
+	
 
 }
