1c9bc7765b46359bf008864cbc2b019ccae68dc5
palak-egov <86659286+palak-egov@users.noreply.github.com>
2024-06-14 14:12:10 +0530

    Update BulkIndexer.java

--- core-services/egov-indexer/src/main/java/org/egov/infra/indexer/bulkindexer/BulkIndexer.java
+++ core-services/egov-indexer/src/main/java/org/egov/infra/indexer/bulkindexer/BulkIndexer.java
@@ -44,7 +44,7 @@ public class BulkIndexer {
 			final HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 			headers.add("Authorization", indexerUtils.getESEncodedCredentials());
-			log.debug("Headers: " + headers.toString());
+			log.info("Headers: " + headers.toString());
 			final HttpEntity<String> entity = new HttpEntity<>(indexJson, headers);
 			Object response = restTemplate.postForObject(url.toString(), entity, Map.class);
 			if (url.contains("_bulk")) {
