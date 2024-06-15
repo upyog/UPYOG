64a3e4b2e8ca91d72b69a6b6b4f1d44b8bb5db82
Nikhil5922 <nikhilkumar7800@gmail.com>
2024-06-15 16:13:51 +0530

    adding citizen and employee changes chb

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useInboxGeneral/filterFn.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useInboxGeneral/filterFn.js
@@ -260,6 +260,7 @@ export const filterFunctions = {
 
     return { searchFilters, workflowFilters };
   },
+  
   TL: (filtersArg) => {
     let { uuid } = Digit.UserService.getUser()?.info || {};
 
