74709fe71c94e9941239fd563ceb5899d448d57f
Shivankshuklaa <shuklashivank28@gmail.com>
2024-06-17 16:45:14 +0530

    Reverted MDMS-V2 to MDMS in WS, OBPS and mCollect Module

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/ws/WSSearchMdmsTypes.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/ws/WSSearchMdmsTypes.js
@@ -1,24 +1,23 @@
 import { useQuery } from "react-query";
-import { getMultipleTypes, getGeneralCriteria } from "../../services/elements/MDMS";
-import { MdmsServiceV2 } from "../../services/elements/MDMSV2";
+import { getMultipleTypes, MdmsService, getGeneralCriteria } from "../../services/elements/MDMS"
 
 const WSSearchMdmsTypes = {
-  useWSMDMSBillAmendment: ({ tenantId, config = {} }) => {
-    const BillAmendmentMdmsDetails = getMultipleTypes(tenantId, "BillAmendment", ["documentObj", "DemandRevisionBasis"]);
-    return useQuery([tenantId, "WS_BILLAMENDMENT_MDMS"], () => MdmsServiceV2.getDataByCriteria(tenantId, BillAmendmentMdmsDetails, "BillAmendment"), {
-      select: ({ BillAmendment }) => {
-        return BillAmendment?.DemandRevisionBasis.map((e, index) => {
-          return { ...e, i18nKey: `DEMAND_REVISION_BASIS_${e.code}`, allowedDocuments: BillAmendment?.documentObj[index] };
-        });
+  useWSMDMSBillAmendment: ({tenantId, config={}}) => {
+    const BillAmendmentMdmsDetails = getMultipleTypes(tenantId, "BillAmendment", ["documentObj", "DemandRevisionBasis"])
+    return useQuery([tenantId, "WS_BILLAMENDMENT_MDMS"], () => MdmsService.getDataByCriteria(tenantId, BillAmendmentMdmsDetails, "BillAmendment"), {
+      select: ({BillAmendment}) => {
+        return BillAmendment?.DemandRevisionBasis.map( (e, index) => {
+          return { ...e, i18nKey: `DEMAND_REVISION_BASIS_${e.code}`, allowedDocuments: BillAmendment?.documentObj[index] }
+        })
       },
-      ...config,
+      ...config
     });
   },
   useWSServicesMasters: (tenantId, type) =>
     useQuery(
       [tenantId, type, "WS_WS_SERVICES_MASTERS"],
       () =>
-        MdmsServiceV2.getDataByCriteria(
+        MdmsService.getDataByCriteria(
           tenantId,
           {
             details: {
@@ -28,7 +27,7 @@ const WSSearchMdmsTypes = {
                   moduleName: "ws-services-masters",
                   masterDetails: [
                     {
-                      name: type ? type : "Documents",
+                      name: type ? type : "Documents"
                     },
                   ],
                 },
@@ -40,15 +39,15 @@ const WSSearchMdmsTypes = {
       {
         select: (data) => {
           const wsDocsData = type ? type : "Documents";
-          data?.["ws-services-masters"]?.[wsDocsData]?.forEach((type) => {
+          data?.["ws-services-masters"]?.[wsDocsData]?.forEach(type => {
             type.code = type.code;
-            type.i18nKey = type.code ? type.code.replaceAll(".", "_") : "";
-            type.dropdownData.forEach((value) => {
-              value.i18nKey = value.code ? value.code.replaceAll(".", "_") : "";
-            });
-          });
-          return data?.["ws-services-masters"] ? data?.["ws-services-masters"] : [];
-        },
+            type.i18nKey = type.code ? type.code.replaceAll('.', '_') : "";
+            type.dropdownData.forEach(value => {
+              value.i18nKey = value.code ? value.code.replaceAll('.', '_') : "";
+            })
+          })
+          return data?.["ws-services-masters"] ? data?.["ws-services-masters"] : []
+        }
       }
     ),
 
@@ -56,7 +55,7 @@ const WSSearchMdmsTypes = {
     useQuery(
       [tenantId, "WS_WS_SERVICES_CALCULATION"],
       () =>
-        MdmsServiceV2.getDataByCriteria(
+        MdmsService.getDataByCriteria(
           tenantId,
           {
             details: {
@@ -77,11 +76,11 @@ const WSSearchMdmsTypes = {
         ),
       {
         select: (data) => {
-          data?.["ws-services-calculation"]?.PipeSize?.forEach((type) => {
+          data?.["ws-services-calculation"]?.PipeSize?.forEach(type => {
             type.i18nKey = type.size ? `${type.size} Inches` : "";
-          });
-          return data?.["ws-services-calculation"] ? data?.["ws-services-calculation"] : [];
-        },
+          })
+          return data?.["ws-services-calculation"] ? data?.["ws-services-calculation"] : []
+        }
       }
     ),
 };
