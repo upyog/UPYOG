f53dc9b936f9d7673416b83afdbd22c3c77ce5c7
Gurpreet Sigh Khanna <45963460+gurpreetsinghkhanna@users.noreply.github.com>
2024-06-18 17:09:49 +0530

    mobileno and dlt_template as sms parameters

--- municipal-services/property-services/src/main/java/org/egov/pt/web/controllers/PropertyController.java
+++ municipal-services/property-services/src/main/java/org/egov/pt/web/controllers/PropertyController.java
@@ -31,6 +31,7 @@ import org.egov.pt.web.contracts.PropertyResponse;
 import org.egov.pt.web.contracts.RequestInfoWrapper;
 import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration.DslContextConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -140,15 +141,15 @@ public class PropertyController {
 
 
     @RequestMapping(value = "/_sendOpenSMS", method = RequestMethod.POST)
-    public ResponseEntity<Integer> sendOpenSMS(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper,@RequestParam String msgParam)
+     public ResponseEntity<Integer> sendOpenSMS(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper,@RequestParam String msgParam,@RequestParam String mobile_no,@RequestParam String dlt_entity_id,@RequestParam String dlt_template_id)
     {
     	try
     	{
     		//String msg="Dear OpenSMS, Status for your application no.OpenSMSApp for property OpenPid to OpenCreated property has been changed to OpenActivate. You can track your application on the link given below-https://mseva.lgpunjab.gov.in/employee/user/login Thank you|1301157492438182299|1407162859196626520";
 
-    	String msg=msgParam;
+    	String msg=msgParam+"|"+dlt_entity_id+"|"+dlt_template_id;
     	Map<String, String> mobileNumberToOwner = new HashMap<>();
-    	mobileNumberToOwner.put("9417630724", "gurpreet");
+    	mobileNumberToOwner.put(mobile_no, "openuser");
     	List<SMSRequest> smsRequests = notifUtil.createSMSRequest(msg, mobileNumberToOwner);
 		notifUtil.sendSMS(smsRequests);
 
