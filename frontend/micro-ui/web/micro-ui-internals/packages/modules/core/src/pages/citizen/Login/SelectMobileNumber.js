bc275804c9f17854809e7d8e11545f37840fad61
sriranjan-s <sriranjansrivastava@Sriranjans-MacBook-Pro.local>
2024-06-19 02:15:31 +0530

    Added changes for login

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/SelectMobileNumber.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/Login/SelectMobileNumber.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { CardText, FormStep, CitizenConsentForm, Loader, CheckBox } from "@upyog/digit-ui-react-components";
+import { CardText,Card, CardHeader,FormStep, CitizenConsentForm, Loader, CheckBox,ButtonSelector,TextInput ,OTPInput} from "@upyog/digit-ui-react-components";
 import { Link } from "react-router-dom";
-
+const TYPE_LOGIN = { type: "login" };
 const SelectMobileNumber = ({ t, onSelect, showRegisterLink, mobileNumber, onMobileChange, config, canSubmit }) => {
 
   const [isCheckBox, setIsCheckBox] = useState(false);
   const [isCCFEnabled, setisCCFEnabled] = useState(false);
   const [mdmsConfig, setMdmsConfig] = useState("");
   const [error, setError]=useState("");
+  const [isOtpLogin, setIsOtpLogin] = useState(false);
+  const [isOtpSent, setIsOtpSent] = useState(false);
+  const [formValues, setFormValues] = useState({
+    username: '',
+    password: '',
+    mobile: '',
+    otp: ''
+  });
   const { isLoading, data } = Digit.Hooks.useCustomMDMS(Digit.ULBService.getStateId(), "common-masters", [{ name: "CitizenConsentForm" }]);
 
   function setTermsAndPolicyDetails(e) {
@@ -62,7 +70,27 @@ const SelectMobileNumber = ({ t, onSelect, showRegisterLink, mobileNumber, onMob
         setError(t("CORE_COMMON_PROFILE_MOBILE_NUMBER_INVALID"));
       }
   };
-  if (isLoading) return <Loader />
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleChangeOTP = (otp) => {
+    console.log("hell1234")
+    setFormValues({
+      ...formValues,
+      otp,
+    });
+  };
+
+  const toggleLoginMethod = () => {
+    console.log("hell122")
+    setIsOtpLogin(!isOtpLogin);
+    setIsOtpSent(false);
+  };
+ 
   const register = async (e) => {
     const data = await Digit.DigiLockerService.register({ module: "REGISTER" });
     e.preventDefault()
@@ -71,42 +99,133 @@ const SelectMobileNumber = ({ t, onSelect, showRegisterLink, mobileNumber, onMob
     sessionStorage.setItem("code_verfier_register", data?.codeverifier)
     window.location.href = redirectUrl
   }
-  return (
-    <FormStep
-      isDisabled={checkDisbaled()}
-      onSelect={onSelect}
-      config={config}
-      t={t}
-      componentInFront="+91"
-      onChange={handleMobileChange}
-      value={mobileNumber}
-    >
-      {error && <p style={{color:"red"}}>{error}</p>}
-      {isCCFEnabled?.isCitizenConsentFormEnabled && (
-      <div>
-        <CheckBox
-          className="form-field"
-          label={checkLabels()}
-          value={isCheckBox}
-          checked={isCheckBox}
-          style={{ marginTop: "5px", marginLeft: "55px" }}
-          styles={{marginBottom: "30px"}}
-          onChange={setTermsAndPolicyDetails}
-        />
+  const sendOtp = async (data) => {
+    try {
+      const res = await Digit.UserService.sendOtp(data, "pg");
+      return [res, null];
+    } catch (err) {
+      return [null, err];
+    }
+  };
+  const handleSendOtp = async (e) => {
+    e.preventDefault()
+    console.log('OTP sent to:', formValues.mobile);
+    const data = {
+      mobileNumber: formValues.mobile,
+      tenantId: "pg",
+      userType: "Citizen",
+    };
+    const [res, err] = await sendOtp({ otp: { ...data, ...TYPE_LOGIN } });
+    setIsOtpSent(true);
+  };
 
-        <CitizenConsentForm
-          styles={{}}
-          t={t}
-          isCheckBoxChecked={setTermsAndPolicyDetails}
-          labels={isCCFEnabled?.checkBoxLabels}
-          mdmsConfig={mdmsConfig}
-          setMdmsConfig={setMdmsConfig}
-        />
-      </div>)}
-      <div className="col col-md-4  text-md-center p-0" style={{width:"40%", marginTop:"5px"}}>
-             <button className="digilocker-btn"type="submit" onClick={(e)=> register(e)}><img src="https://meripehchaan.gov.in/assets/img/icon/digi.png" class="mr-2" style={{"width":"12%"}}></img>Register with DigiLocker</button>
+  return (
+    // <FormStep
+    //   isDisabled={checkDisbaled()}
+    //   onSelect={onSelect}
+    //   config={config}
+    //   t={t}
+    //   componentInFront="+91"
+    //   onChange={handleMobileChange}
+    //   value={mobileNumber}
+    // >
+    //   {error && <p style={{color:"red"}}>{error}</p>}
+    //   {isCCFEnabled?.isCitizenConsentFormEnabled && (
+    //   <div>
+    //     <CheckBox
+    //       className="form-field"
+    //       label={checkLabels()}
+    //       value={isCheckBox}
+    //       checked={isCheckBox}
+    //       style={{ marginTop: "5px", marginLeft: "55px" }}
+    //       styles={{marginBottom: "30px"}}
+    //       onChange={setTermsAndPolicyDetails}
+    //     />
+
+    //     <CitizenConsentForm
+    //       styles={{}}
+    //       t={t}
+    //       isCheckBoxChecked={setTermsAndPolicyDetails}
+    //       labels={isCCFEnabled?.checkBoxLabels}
+    //       mdmsConfig={mdmsConfig}
+    //       setMdmsConfig={setMdmsConfig}
+    //     />
+    //   </div>)}
+    //   <div className="col col-md-4  text-md-center p-0" style={{width:"40%", marginTop:"5px"}}>
+    //          <button className="digilocker-btn"type="submit" onClick={(e)=> register(e)}><img src="https://meripehchaan.gov.in/assets/img/icon/digi.png" class="mr-2" style={{"width":"12%"}}></img>Register with DigiLocker</button>
+    //             </div>
+    // </FormStep>
+    <React.Fragment>
+      <Card>
+
+        <CardHeader variant="body2">
+          {isOtpLogin ? t("Login with OTP") : t("Login with Username & Password")}
+        </CardHeader>
+        <div>
+          <div onClick={toggleLoginMethod} style={{ cursor: 'pointer', fontSize: '24px' }}>
+            <i className={isOtpLogin ? 'fa-solid fa-toggle-on' : 'fa-solid fa-toggle-on fa-flip-horizontal'}></i>
+          </div>
+          <span style={{ marginLeft: "10px" }}>{isOtpLogin ? 'Click for Login with Username & Password' : 'Click for Login with OTP'}</span>
+        </div>
+          {isOtpLogin ? (
+            <div>
+              <label>{t("Mobile_No")}</label>
+              <TextInput
+                required
+                id="mobile"
+                name="mobile"
+                label={t("Mobile Number")}
+                variant="outlined"
+                value={formValues.mobile}
+                onChange={handleChange}
+                fullWidth
+                margin="normal"
+              />
+              {!isOtpSent ? (
+                <ButtonSelector type="button" onSubmit={handleSendOtp} style={{ marginLeft: "10px", marginBottom: "10px" }} label=  {t("Send OTP")} />
+
+              ) : (
+                <div>
+                  <label>{t("OTP")}</label>
+                  <OTPInput length={6} onChange={handleChangeOTP} value={formValues.otp} />
                 </div>
-    </FormStep>
+              )}
+            </div>
+          ) : (
+            <div>
+              <label>{t("User_Name")}</label>
+              <TextInput
+                required
+                id="username"
+                name="username"
+                label={t("Username")}
+                variant="outlined"
+                value={formValues.username}
+                onChange={handleChange}
+                fullWidth
+                margin="normal"
+              />
+              <label>{t("Password")}</label>
+              <TextInput
+                required
+                id="password"
+                name="password"
+                label={t("Password")}
+                type="password"
+                variant="outlined"
+                value={formValues.password}
+                onChange={handleChange}
+                fullWidth
+                margin="normal"
+              />
+            </div>
+          )}
+          {/* <ButtonSelector type="button" onSubmit={(e) => }style={{marginLeft: "0px" }} label= {isOtpLogin ? 'Login with OTP' : 'Login with Username & Password'} /> */}
+           
+          <ButtonSelector  type="button" size="small" onSubmit={() => window.location.href= window.location.href.split("login")[0] + "register/name"} label="Register" />
+
+      </Card>
+      </React.Fragment>
   );
 };
 
