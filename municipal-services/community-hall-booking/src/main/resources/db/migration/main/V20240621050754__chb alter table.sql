5eb95677bb1bf1fc3a03223ef0a7b1ef340389be
keshavkumar <keshavkumar@192.168.180.157>
2024-06-20 09:42:37 +0530

    added hall code column

--- municipal-services/community-hall-booking/src/main/resources/db/migration/main/V20240621050754__chb alter table.sql
+++ municipal-services/community-hall-booking/src/main/resources/db/migration/main/V20240621050754__chb alter table.sql
@@ -1,6 +1,7 @@
 ALTER TABLE COMMUNITY_HALL_BOOKING_DETAILS DROP COLUMN approval_no, DROP COLUMN  approval_date;
 
-ALTER TABLE BOOKING_SLOT_DETAILS DROP COLUMN booking_slot_dateTime;
+ALTER TABLE BOOKING_SLOT_DETAILS DROP COLUMN booking_slot_dateTime,
+DROP COLUMN hallcode;
 
 ALTER TABLE COMMUNITY_HALL_BOOKING_DETAILS 
 ADD COLUMN application_no  character varying(64),
@@ -13,7 +14,8 @@ ADD COLUMN applicant_alternate_mobile_no  character varying(12)  NOT NULL;
   
   
 ALTER TABLE BOOKING_SLOT_DETAILS  
-ADD COLUMN  hall_name character varying(100),
+ADD COLUMN  hall_name character varying(200),
+ADD COLUMN  hall_code character varying(100),
 ADD COLUMN  booking_date character varying(20) NOT NULL,
 ADD COLUMN  booking_from_time character varying(20) NOT NULL,
 ADD COLUMN  booking_to_time character varying(20) NOT NULL;
