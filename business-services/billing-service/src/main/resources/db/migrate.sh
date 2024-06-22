b9570a85e0eb0965035131ebf0c22a692ccca39e
abhithakur2 <156669712+abhithakur2@users.noreply.github.com>
2024-06-22 17:01:56 +0530

    Update migrate.sh
    update

--- business-services/billing-service/src/main/resources/db/migrate.sh
+++ business-services/billing-service/src/main/resources/db/migrate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -outOfOrder=true -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=false -ignoreMissingMigrations=true migrate
+flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -outOfOrder=true -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=false  migrate
