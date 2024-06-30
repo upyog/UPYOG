c4be837c6ee5551c957132b7b7a394ebfa1be893
sourabh7yy <sourabh7y@gmail.com>
2024-06-30 21:50:51 +0530

    action button check

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/ew/src/pages/employee/ApplicationDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ew/src/pages/employee/ApplicationDetails.js
@@ -1,26 +1,29 @@
-    import { Header, MultiLink } from "@upyog/digit-ui-react-components";
-    import _ from "lodash";
-    import React, { useEffect, useState, useCallback } from "react";
-    import { useTranslation } from "react-i18next";
-    import { useParams } from "react-router-dom";
-    import ApplicationDetailsTemplate from "../../../../templates/ApplicationDetails";
+import { Header, MultiLink } from "@upyog/digit-ui-react-components";
+import _ from "lodash";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import ApplicationDetailsTemplate from "../../../../templates/ApplicationDetails";
+// import getEwAcknowledgementData from "../../utils/getEwAcknowledgementData";
 
-    const EWApplicationDetails = () => {
-      const { t } = useTranslation();
-      const { data: storeData } = Digit.Hooks.useStore.getInitData();
-      const tenantId = Digit.ULBService.getCurrentTenantId();
-      const { tenants } = storeData || {};
-      const { id: requestId } = useParams();
-      const [showToast, setShowToast] = useState(null);
-      const [appDetailsToShow, setAppDetailsToShow] = useState({});
-      const [showOptions, setShowOptions] = useState(false);
-      const [enableAudit, setEnableAudit] = useState(false);
-      const [businessService, setBusinessService] = useState("ewst");
-      const [workflowDetails, setWorkflowDetails] = useState(null);
-    
-    
-      const { isLoading, isError, data: applicationDetails, error } = Digit.Hooks.ew.useEwApplicationDetail(t,tenantId,requestId);
+const EWApplicationDetails = () => {
+  const { t } = useTranslation();
+  const { data: storeData } = Digit.Hooks.useStore.getInitData();
+  
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+  const { tenants } = storeData || {};
+  const { id: requestId } = useParams();
+  const [showToast, setShowToast] = useState(null);
+  const [appDetailsToShow, setAppDetailsToShow] = useState({});
+  const [showOptions, setShowOptions] = useState(false);
+  const [enableAudit, setEnableAudit] = useState(false);
+  
+  let businessService = "ewst"
 
+
+
+  const { isLoading, isError, data: applicationDetails, error } = Digit.Hooks.ew.useEwApplicationDetail(t, tenantId, requestId);
+  console.log("applicationDetails",applicationDetails)
   const {
     isLoading: updatingApplication,
     isError: updateApplicationError,
@@ -29,61 +32,62 @@
     mutate,
   } = Digit.Hooks.ew.useEWApplicationAction(tenantId);
 
-  const Session = Digit.SessionStorage.get("User");
-  const uuid = Session?.info?.uuid;
-  const modified = applicationDetails?.applicationData?.applicationData?.auditDetails?.lastModifiedBy;
+  let workflowDetails = Digit.Hooks.useWorkflowDetails({
+      tenantId: applicationDetails?.applicationData?.tenantId || tenantId,
+      id: applicationDetails?.applicationData?.applicationData?.requestId,
+      moduleCode: businessService,
+      role: "EW_VENDOR",
+  });
 
-  useEffect(() => {
-    if (modified === uuid || modified === null  ) {
-      const details = Digit.Hooks.useWorkflowDetails({
-        tenantId: applicationDetails?.applicationData?.tenantId || tenantId,
-        id: applicationDetails?.applicationData?.applicationData?.requestId,
-        moduleCode: businessService,
-        role: "EW_VENDOR",
-      });
-      setWorkflowDetails(details);
-    }
-  }
-  , [modified, uuid, applicationDetails, tenantId, businessService]);
+
+  console.log("workflowDetails",workflowDetails);
 
   const { isLoading: auditDataLoading, isError: isAuditError, data: auditData } = Digit.Hooks.ew.useEWSearch(
     {
-    tenantId,
-    filters: { requestId: requestId, audit: true },
+      tenantId,
+      filters: { requestId: requestId, audit: true },
     },
   );
 
-  const closeToast = useCallback(() => {
+  const closeToast = () => {
     setShowToast(null);
-  }, []);
+  };
 
   useEffect(() => {
     if (applicationDetails) {
       setAppDetailsToShow(_.cloneDeep(applicationDetails));
+    
     }
   }, [applicationDetails]);
 
-  useEffect(() => {
-    if (workflowDetails?.data?.applicationBusinessService && !(workflowDetails?.data?.applicationBusinessService === "ewst" && businessService === "ewst")) {
-      setBusinessService(workflowDetails?.data?.applicationBusinessService);
-    }
-  }, [workflowDetails?.data]);
 
-   // const handleDownloadPdf = async () => {
-      //   const EwasteApplication = appDetailsToShow?.applicationData;
-      //   console.log("ewaste",EwasteApplication)
-      //   const tenantInfo = "pg.citya"
-      //   tenants.find((tenant) => tenant.code === EwasteApplication.tenantId);
-      //   const data = await getEwAcknowledgementData(EwasteApplication.applicationData, tenantInfo, t);
-      //   Digit.Utils.pdf.generate(data);
-      // };
 
-      // const petDetailsPDF = {
-      //   order: 1,
-      //   label: t("EW_APPLICATION"),
-      //   onClick: () => handleDownloadPdf(),
-      // };
+  // useEffect(() => {
+
+  //   if (workflowDetails?.data?.applicationBusinessService && !(workflowDetails?.data?.applicationBusinessService === "ewst" && businessService === "ewst")) {
+  //     setBusinessService(workflowDetails?.data?.applicationBusinessService);
+  //   }
+  // }, [workflowDetails.data]);
+
+
+  // const handleDownloadPdf = async () => {
+  //   const EwasteApplication = appDetailsToShow?.applicationData;
+  //   console.log("ewaste",EwasteApplication)
+  //   const tenantInfo = "pg.citya"
+  //   tenants.find((tenant) => tenant.code === EwasteApplication.tenantId);
+  //   const data = await getEwAcknowledgementData(EwasteApplication.applicationData, tenantInfo, t);
+  //   Digit.Utils.pdf.generate(data);
+  // };
+
+  // const petDetailsPDF = {
+  //   order: 1,
+  //   label: t("EW_APPLICATION"),
+  //   onClick: () => handleDownloadPdf(),
+  // };
+
   let downloadOptions = [""];
+
+
 
   const { data: reciept_data, isLoading: recieptDataLoading } = Digit.Hooks.useRecieptSearch(
     {
@@ -94,26 +98,27 @@
     },
     { enabled: appDetailsToShow?.applicationData?.applicationData?.requestId ? true : false }
   );
-
+  
   const printCertificate = async () => {
     let response = await Digit.PaymentService.generatePdf(tenantId, { EwasteApplication: [applicationDetails?.applicationData?.applicationData] }, "ewasteservicecertificate");
     const fileStore = await Digit.PaymentService.printReciept(tenantId, { fileStoreIds: response.filestoreIds[0] });
     window.open(fileStore[response?.filestoreIds[0]], "_blank");
   };
-  
 
-  if (appDetailsToShow?.applicationData?.tenantId === "pg.citya") {
-    downloadOptions.push({
-      label: t("EW_CERTIFICATE"),
-      onClick: () => printCertificate(),
-    });
-  }
+
+  if (appDetailsToShow?.applicationData?.tenantId === "pg.citya")
+  downloadOptions.push({
+    label: t("EW_CERTIFICATE"),
+    onClick: () => printCertificate(),
+  });
+
 
   return (
     <div>
       <div className={"employee-application-details"} style={{ marginBottom: "15px" }}>
+
         <Header styles={{ marginLeft: "0px", paddingTop: "10px", fontSize: "32px" }}>{t("EW_APPLICATION_DETAILS")}</Header>
-        {downloadOptions && downloadOptions.length > 0 && (
+        { downloadOptions && downloadOptions.length > 0 && (
           <MultiLink
             className="multilinkWrapper employee-mulitlink-main-div"
             onHeadClick={() => setShowOptions(!showOptions)}
@@ -121,6 +126,7 @@
             options={downloadOptions}
             downloadBtnClassName={"employee-download-btn-className"}
             optionsClassName={"employee-options-btn-className"}
+          // ref={menuRef}
           />
         )}
       </div>
