331bb008abec468d07a0c0aaf2157c90579ebd73
ABHISHEK  RANA <121286986+abhirana04@users.noreply.github.com>
2024-07-23 10:22:13 +0530

    Changes for sms in coll

--- business-services/collection-services/src/main/java/org/egov/collection/consumer/CollectionNotificationConsumer.java
+++ business-services/collection-services/src/main/java/org/egov/collection/consumer/CollectionNotificationConsumer.java
@@ -1,6 +1,11 @@
 package org.egov.collection.consumer;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,155 +37,178 @@ import static org.egov.collection.config.CollectionServiceConstants.*;
 
 @Slf4j
 @Component
-public class CollectionNotificationConsumer{
+public class CollectionNotificationConsumer {
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    @Autowired
-    private ApplicationProperties applicationProperties;
+	@Autowired
+	private ApplicationProperties applicationProperties;
 
-    @Autowired
-    private CollectionProducer producer;
+	@Autowired
+	private CollectionProducer producer;
 
-    @Autowired
-    private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
-    @KafkaListener(topics = { "${kafka.topics.payment.create.name}", "${kafka.topics.payment.receiptlink.name}" })
-    public void listen(HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
-        try{
-            PaymentRequest req = objectMapper.convertValue(record, PaymentRequest.class);
-            sendNotification(req);
-        }catch(Exception e){
-            log.error("Exception while reading from the queue: ", e);
-        }
-    }
+	@KafkaListener(topics = { "${kafka.topics.payment.create.name}", "${kafka.topics.payment.receiptlink.name}" })
+	public void listen(HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+		try {
+			PaymentRequest req = objectMapper.convertValue(record, PaymentRequest.class);
+			sendNotification(req);
+		} catch (Exception e) {
+			log.error("Exception while reading from the queue: ", e);
+		}
+	}
 
-    private void sendNotification(PaymentRequest paymentRequest){
-        Payment payment = paymentRequest.getPayment();
-        for (PaymentDetail paymentDetail : payment.getPaymentDetails()) {
-            String mobNo = payment.getMobileNumber();
-            String paymentStatus = (payment.getPaymentStatus().toString() == null ? "NEW" : payment.getPaymentStatus().toString());
-            Bill bill = paymentDetail.getBill();
-            String message = buildSmsBody(bill, paymentDetail, paymentRequest.getRequestInfo(), paymentStatus);
-            if (!StringUtils.isEmpty(message)) {
-                HashMap<String, Object> request = new HashMap<>();
-                request.put("mobileNumber", mobNo);
-                request.put("message", message);
-                producer.producer(applicationProperties.getSmsTopic(), request);
-            } else {
-                log.error("Message not configured! No notification will be sent.");
-            }
-        }
+	private void sendNotification(PaymentRequest paymentRequest) {
+		Payment payment = paymentRequest.getPayment();
+		for (PaymentDetail paymentDetail : payment.getPaymentDetails()) {
+			String mobNo = payment.getMobileNumber();
+			String paymentStatus = (payment.getPaymentStatus().toString() == null ? "NEW"
+					: payment.getPaymentStatus().toString());
+			Bill bill = paymentDetail.getBill();
+			String message = buildSmsBody(bill, paymentDetail, paymentRequest.getRequestInfo(), paymentStatus);
+			if (!StringUtils.isEmpty(message)) {
+				HashMap<String, Object> request = new HashMap<>();
+				request.put("mobileNumber", mobNo);
+				request.put("message", message);
+				producer.producer(applicationProperties.getSmsTopic(), request);
+			} else {
+				log.error("Message not configured! No notification will be sent.");
+			}
+		}
 
-    }
+	}
 
-    private String buildSmsBody(Bill bill, PaymentDetail paymentDetail, RequestInfo requestInfo, String paymentStatus){
-        String message = null;
-        String content = null;
-        switch(paymentStatus.toUpperCase()){
-            case "NEW":
-                content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(), COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_OPEN_CODE);
-                break;
-            case "DEPOSITED":
-                content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(), COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_DEPOSITED_CODE);
-                break;
-            case "CANCELLED":
-                content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(), COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_CANCELLED_CODE);
-                break;
-            case "DISHONOURED":
-                content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(), COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_DISHONOURED_CODE);
-                break;
-            default:
-                break;
-        }
-        if(!StringUtils.isEmpty(content)){
-            StringBuilder link = new StringBuilder();
-            link.append(applicationProperties.getUiHost() + "/citizen").append("/otpLogin?mobileNo=").append(bill.getMobileNumber()).append("&redirectTo=")
-                    .append(applicationProperties.getUiRedirectUrl()).append("&params=").append(paymentDetail.getTenantId() + "," + paymentDetail.getReceiptNumber());
+	private String buildSmsBody(Bill bill, PaymentDetail paymentDetail, RequestInfo requestInfo, String paymentStatus) {
+		String message = null;
+		String content = null;
+		switch (paymentStatus.toUpperCase()) {
+		case "NEW":
+			content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(),
+					COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_OPEN_CODE);
+			break;
+		case "DEPOSITED":
+			content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(),
+					COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_DEPOSITED_CODE);
+			break;
+		case "CANCELLED":
+			content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(),
+					COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_CANCELLED_CODE);
+			break;
+		case "DISHONOURED":
+			content = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(),
+					COLLECTION_LOCALIZATION_MODULE, WF_MT_STATUS_DISHONOURED_CODE);
+			break;
+		default:
+			break;
+		}
+		if (!StringUtils.isEmpty(content)) {
+			if (!paymentStatus.equalsIgnoreCase("NEW")) {
+				StringBuilder link = new StringBuilder();
+				link.append(applicationProperties.getUiHost() + "/citizen").append("/otpLogin?mobileNo=")
+						.append(bill.getMobileNumber()).append("&redirectTo=")
+						.append(applicationProperties.getUiRedirectUrl()).append("&params=")
+						.append(paymentDetail.getTenantId() + "," + paymentDetail.getReceiptNumber());
 
-            String receiptLink = getShortenedUrl(link.toString());
+				String receiptLink = getShortenedUrl(link.toString());
 
-            content = content.replaceAll("{rcpt_link}", receiptLink);
+				content = content.replaceAll("{rcpt_link}", receiptLink);
 
-            String moduleName = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(),
-                    BUSINESSSERVICE_LOCALIZATION_MODULE, formatCodes(paymentDetail.getBusinessService()));
+				String moduleName = fetchContentFromLocalization(requestInfo, paymentDetail.getTenantId(),
+						BUSINESSSERVICE_LOCALIZATION_MODULE, formatCodes(paymentDetail.getBusinessService()));
 
-            if(StringUtils.isEmpty(moduleName))
-                moduleName = "Adhoc Tax";
+				if (StringUtils.isEmpty(moduleName))
+					moduleName = "Adhoc Tax";
+				content = content.replaceAll("<mod_name>", moduleName);
+			}
+			if (content.contains("<owner_name>"))
+				content = content.replaceAll("<owner_name>", bill.getPayerName());
+			else
+				content = content.replaceAll("<tax_name>", bill.getBusinessService());
+			if (content.contains("<amount_paid>"))
+				content = content.replaceAll("<amount_paid>", paymentDetail.getTotalAmountPaid().toString());
+			Collections.sort(bill.getBillDetails(), (b1, b2) -> b2.getFromPeriod().compareTo(b1.getFromPeriod()));
+			String formattedDate = LocalDateTime
+					.ofInstant(Instant.ofEpochMilli(bill.getBillDetails().get(0).getFromPeriod()),
+							ZoneId.systemDefault())
+					.format(DateTimeFormatter.ofPattern("yyyy"));
+			String formattedtoDate = LocalDateTime
+					.ofInstant(Instant.ofEpochMilli(bill.getBillDetails().get(0).getToPeriod()), ZoneId.systemDefault())
+					.format(DateTimeFormatter.ofPattern("yy"));
+			if (content.contains("<fin_year>"))
 
-            content = content.replaceAll("{owner_name}", bill.getPayerName());
+				content = content.replaceAll("<fin_year>", formattedDate + "-" + formattedtoDate);
+			content = content.replaceAll("<rcpt_no>", paymentDetail.getReceiptNumber());
 
-            if(content.contains("{amount_paid}"))
-                content = content.replaceAll("{amount_paid}", paymentDetail.getTotalAmountPaid().toString());
+			content = content.replaceAll("<unique_id>", bill.getConsumerCode());
+			message = content;
+		}
+		return message;
+	}
 
-            content = content.replaceAll("{rcpt_no}", paymentDetail.getReceiptNumber());
-            content = content.replaceAll("{mod_name}", moduleName);
-            content = content.replaceAll("{unique_id}", bill.getConsumerCode());
-            message = content;
-        }
-        return message;
-    }
+	private String fetchContentFromLocalization(RequestInfo requestInfo, String tenantId, String module, String code) {
+		String message = null;
+		List<String> codes = new ArrayList<>();
+		List<String> messages = new ArrayList<>();
+		Object result = null;
+		String locale = "";
+		if (requestInfo.getMsgId().contains("|"))
+			locale = requestInfo.getMsgId().split("[\\|]")[1];
+		if (StringUtils.isEmpty(locale))
+			locale = applicationProperties.getFallBackLocale();
+		StringBuilder uri = new StringBuilder();
+		uri.append(applicationProperties.getLocalizationHost()).append(applicationProperties.getLocalizationEndpoint());
+		uri.append("?tenantId=").append(tenantId.split("\\.")[0]).append("&locale=").append(locale).append("&module=")
+				.append(module);
 
-    private String fetchContentFromLocalization(RequestInfo requestInfo, String tenantId, String module, String code){
-        String message = null;
-        List<String> codes = new ArrayList<>();
-        List<String> messages = new ArrayList<>();
-        Object result = null;
-        String locale = "";
-        if(requestInfo.getMsgId().contains("|"))
-            locale = requestInfo.getMsgId().split("[\\|]")[1];
-        if(StringUtils.isEmpty(locale))
-            locale = applicationProperties.getFallBackLocale();
-        StringBuilder uri = new StringBuilder();
-        uri.append(applicationProperties.getLocalizationHost()).append(applicationProperties.getLocalizationEndpoint());
-        uri.append("?tenantId=").append(tenantId.split("\\.")[0]).append("&locale=").append(locale).append("&module=").append(module);
+		Map<String, Object> request = new HashMap<>();
+		request.put("RequestInfo", requestInfo);
+		try {
+			result = restTemplate.postForObject(uri.toString(), request, Map.class);
+			codes = JsonPath.read(result, LOCALIZATION_CODES_JSONPATH);
+			messages = JsonPath.read(result, LOCALIZATION_MSGS_JSONPATH);
+		} catch (Exception e) {
+			log.error("Exception while fetching from localization: " + e);
+		}
+		if (CollectionUtils.isEmpty(messages)) {
+			throw new CustomException("LOCALIZATION_NOT_FOUND", "Localization not found for the code: " + code);
+		}
+		for (int index = 0; index < codes.size(); index++) {
+			if (codes.get(index).equals(code)) {
+				message = messages.get(index);
+			}
+		}
+		return message;
+	}
 
-        Map<String, Object> request = new HashMap<>();
-        request.put("RequestInfo", requestInfo);
-        try {
-            result = restTemplate.postForObject(uri.toString(), request, Map.class);
-            codes = JsonPath.read(result, LOCALIZATION_CODES_JSONPATH);
-            messages = JsonPath.read(result, LOCALIZATION_MSGS_JSONPATH);
-        } catch (Exception e) {
-            log.error("Exception while fetching from localization: " + e);
-        }
-        if(CollectionUtils.isEmpty(messages)){
-            throw new CustomException("LOCALIZATION_NOT_FOUND", "Localization not found for the code: " + code);
-        }
-        for(int index = 0; index < codes.size(); index++){
-            if(codes.get(index).equals(code)){
-                message = messages.get(index);
-            }
-        }
-        return message;
-    }
+	private String formatCodes(String code) {
+		String regexForSpecialCharacters = "[-+$&,:;=?@#|'<>.^*()%!]";
+		code = code.replaceAll(regexForSpecialCharacters, "");
+		code = code.replaceAll(" ", "_");
 
-    private String formatCodes(String code){
-        String regexForSpecialCharacters = "[-+$&,:;=?@#|'<>.^*()%!]";
-        code = code.replaceAll(regexForSpecialCharacters, "");
-        code = code.replaceAll(" ", "_");
+		return BUSINESSSERVICELOCALIZATION_CODE_PREFIX + code.toUpperCase();
+	}
 
-        return BUSINESSSERVICELOCALIZATION_CODE_PREFIX + code.toUpperCase();
-    }
+	/**
+	 * Method to shortent the url returns the same url if shortening fails
+	 * 
+	 * @param url
+	 */
+	public String getShortenedUrl(String url) {
 
-    /**
-     * Method to shortent the url
-     * returns the same url if shortening fails
-     * @param url
-     */
-    public String getShortenedUrl(String url){
+		HashMap<String, String> body = new HashMap<>();
+		body.put("url", url);
+		StringBuilder builder = new StringBuilder(applicationProperties.getUrlShortnerHost());
+		builder.append(applicationProperties.getUrlShortnerEndpoint());
+		String res = restTemplate.postForObject(builder.toString(), body, String.class);
 
-        HashMap<String,String> body = new HashMap<>();
-        body.put("url",url);
-        StringBuilder builder = new StringBuilder(applicationProperties.getUrlShortnerHost());
-        builder.append(applicationProperties.getUrlShortnerEndpoint());
-        String res = restTemplate.postForObject(builder.toString(), body, String.class);
-
-        if(StringUtils.isEmpty(res)){
-            log.error("URL_SHORTENING_ERROR","Unable to shorten url: "+url); ;
-            return url;
-        }
-        else return res;
-    }
+		if (StringUtils.isEmpty(res)) {
+			log.error("URL_SHORTENING_ERROR", "Unable to shorten url: " + url);
+			;
+			return url;
+		} else
+			return res;
+	}
 }
