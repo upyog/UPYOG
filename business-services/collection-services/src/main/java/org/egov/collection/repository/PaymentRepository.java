345fac7622bc59ae509d46be46192df00e66407f
ABHISHEK  RANA <121286986+abhirana04@users.noreply.github.com>
2024-07-24 16:59:22 +0530

    Revert "chnages in query"
    This reverts commit 24c9c119d343647faae0f9c3960a21024fc357e5.

--- business-services/collection-services/src/main/java/org/egov/collection/repository/PaymentRepository.java
+++ business-services/collection-services/src/main/java/org/egov/collection/repository/PaymentRepository.java
@@ -647,11 +647,11 @@ status = new ArrayList<String>();
 		Map<String, Object> preparedStatementValues = new HashMap<>();
 		String queryString;
 		if (businesssrvice.contains("WS")) {
-			 queryString = "select concat(a3.doorno,',',a3.plotno,',',a3.buildingname,',',a3.street,',',a3.landmark,',',a3.district ,',',a3.region,',',a3.city )  FROM eg_ws_connection a1 INNER JOIN eg_pt_property a2 ON a1.property_id = a2.propertyid  inner join eg_pt_address as a3 on a2.id=a3.propertyid where   a1.status='Active' and a1.connectionno   ='"+consumercode+"';";
+			 queryString = "select concat(a3.doorno,',',a3.plotno,',',a3.buildingname,',',a3.street',',',a3.landmark,',',a3.district ,',',a3.region,',',a3.city )  FROM eg_ws_connection a1 INNER JOIN eg_pt_property a2 ON a1.property_id = a2.propertyid  inner join eg_pt_address as a3 on a2.id=a3.propertyid where   a1.status='Active' and a1.connectionno   ='"+consumercode+"';";
 		log.info("Query for fetchAddressByApplicationno: " +queryString);
 		}
 		else {
-			 queryString = "select concat(a3.doorno,',',a3.plotno,',',a3.buildingname,',',a3.street,',',a3.landmark,',',a3.district ,',',a3.region,',',a3.city ) FROM eg_sw_connection a1 INNER JOIN eg_pt_property a2 ON a1.property_id = a2.propertyid  inner join eg_pt_address as a3 on a2.id=a3.propertyid where   a1.status='Active' and a1.connectionno   ='"+consumercode+"';";
+			 queryString = "select concat(a3.doorno,',',a3.plotno,',',a3.buildingname,',',a3.street',',',a3.landmark,',',a3.district ,',',a3.region,',',a3.city ) FROM eg_sw_connection a1 INNER JOIN eg_pt_property a2 ON a1.property_id = a2.propertyid  inner join eg_pt_address as a3 on a2.id=a3.propertyid where   a1.status='Active' and a1.connectionno   ='"+consumercode+"';";
 
 				log.info("Query for fetchAddressByApplicationno: " +queryString);
 		}
