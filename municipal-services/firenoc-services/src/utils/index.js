c6001777398c6c083a0d39fa7236a032c9cefcc5
anujkit <chauhan.chauhan5@gmail.com>
2024-07-29 17:08:04 +0530

    Resubmit issue

--- municipal-services/firenoc-services/src/utils/index.js
+++ municipal-services/firenoc-services/src/utils/index.js
@@ -77,14 +77,13 @@ export const createWorkFlow = async body => {
       businessService: envVariables.BUSINESS_SERVICE,
       businessId: fireNOC.fireNOCDetails.applicationNumber,
       action: fireNOC.fireNOCDetails.action,
-      comment: get(fireNOC.fireNOCDetails.additionalDetail, "comment", null),
-      assignes: (fireNOC.fireNOCDetails.additionalDetail.assignee && fireNOC.fireNOCDetails.additionalDetail.assignee != null
-                 && fireNOC.fireNOCDetails.additionalDetail.assignee.length>0)
-      ? fireNOC.fireNOCDetails.additionalDetail.assignee.map(ids =>{ return {
-        uuid:ids
-      };})
-      : null,
-      documents: get(fireNOC.fireNOCDetails.additionalDetail, "wfDocuments", null),
+      comment: get(fireNOC.fireNOCDetails, "comment", null),
+      assignes: [(fireNOC.fireNOCDetails.assignee 
+        && fireNOC.fireNOCDetails.assignee[0] != null 
+        && fireNOC.fireNOCDetails.assignee[0] !='')
+        ? { uuid: fireNOC.fireNOCDetails.assignee[0] }
+        : null],
+      documents: get(fireNOC.fireNOCDetails, "wfDocuments", null),
       sla: 0,
       previousStatus: null,
       moduleName: envVariables.BUSINESS_SERVICE
