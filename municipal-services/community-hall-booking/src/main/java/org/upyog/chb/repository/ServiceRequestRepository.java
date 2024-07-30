550964b2801b4d0f77c818691e16cc597f78a969
keshav <keshav@localhost>
2024-07-30 12:31:39 +0530

    Added logs for notification service

--- municipal-services/community-hall-booking/src/main/java/org/upyog/chb/repository/ServiceRequestRepository.java
+++ municipal-services/community-hall-booking/src/main/java/org/upyog/chb/repository/ServiceRequestRepository.java
@@ -51,12 +51,17 @@ public class ServiceRequestRepository {
     public String getShorteningURL(StringBuilder uri, Object request) {
 		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 		String response = null;
-		StringBuilder str = new StringBuilder(this.getClass().getCanonicalName()).append(".fetchResult:")
-				.append(System.lineSeparator());
-		str.append("URI: ").append(uri.toString()).append(System.lineSeparator());
+		/*
+		 * StringBuilder strq = new
+		 * StringBuilder(this.getClass().getCanonicalName()).append(".fetchResult:")
+		 * .append(System.lineSeparator());
+		 * str.append("URI: ").append(uri.toString()).append(System.lineSeparator());
+		 */
 		try {
-			log.debug(str.toString());
+			//log.info("Url shortener url : " +  str.toString());
+			log.info("request info : "+ request + " uri : " + uri);
 			response = restTemplate.postForObject(uri.toString(), request, String.class);
+			log.info("response info : "+ response);
 		} catch (HttpClientErrorException e) {
 			log.error("External Service threw an Exception: ", e);
 			throw new ServiceCallException(e.getResponseBodyAsString());
