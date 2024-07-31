032d2a63cc0e8dd49fea2dbf25c79ccd86278519
deepikaarora88 <deepika.arora@pwc.com>
2024-07-31 07:59:22 +0000

    Finance issue fix

--- finance/egov/egov-egfweb/src/main/java/org/egov/egf/web/actions/report/GeneralLedgerReportAction.java
+++ finance/egov/egov-egfweb/src/main/java/org/egov/egf/web/actions/report/GeneralLedgerReportAction.java
@@ -227,7 +227,7 @@ public class GeneralLedgerReportAction extends BaseFormAction {
 				fund = (Fund) persistenceService.find("from Fund where id = ?", 0);
 			} else
 				fund = (Fund) persistenceService.find("from Fund where id = ?",
-						Long.valueOf(generalLedgerReportBean.getFund_id()));
+						Integer.parseInt(generalLedgerReportBean.getFund_id()));
 		}
 		if (fund == null && glCode!=null) {
 			heading = "General Ledger Report for " + glCode.getGlcode() + ":" + glCode.getName() + " from "
