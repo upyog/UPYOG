5e0cf62810e840fbb957270b11e37d4f4c7b4748
Nikhil5922 <nikhilkumar7800@gmail.com>
2024-08-05 23:31:13 +0530

    Fixed searchHall checkbox issue and resolved the problem where filestore ID was not updating after downloading receipt or permission letter.

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbSearch.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbSearch.js
@@ -6,19 +6,18 @@ const useChbSearch = ({ tenantId, filters, auth,searchedFrom="" }, config = {}) 
   const args = tenantId ? { tenantId, filters, auth } : { filters, auth };
 
   const defaultSelect = (data) => {
-    console.log("chbhook", data)
     if(data.hallsBookingApplication.length > 0)  data.hallsBookingApplication[0].applicationNo = data.hallsBookingApplication[0].applicationNo || [];
       
     return data;
   };
 
-  const { isLoading, error, data, isSuccess } = useQuery(["chbSearchList", tenantId, filters, auth, config], () => Digit.CHBServices.search(args), {
+  const { isLoading, error, data, isSuccess,refetch } = useQuery(["chbSearchList", tenantId, filters, auth, config], () => Digit.CHBServices.search(args), {
     
     select: defaultSelect,
     ...config,
   });
 
-  return { isLoading, error, data, isSuccess, revalidate: () => client.invalidateQueries(["chbSearchList", tenantId, filters, auth]) };
+  return { isLoading, error, data, isSuccess,refetch, revalidate: () => client.invalidateQueries(["chbSearchList", tenantId, filters, auth]) };
 };
 
 export default useChbSearch;
