335db2433762a5d0ebcc2cca419f82d78e93d04d
vinothrallapalli-eGov <vinoth.r@egovernments.org>
2024-08-13 14:00:05 +0530

    Update vehicle number format

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pages/employee/configs/VehicleConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pages/employee/configs/VehicleConfig.js
@@ -16,7 +16,7 @@ const VehicleConfig = (t, disabled = false) => {
             name: "registrationNumber",
             ValidationRequired: true,
             validation: {
-              pattern: `[A-Z]{2}[0-9]{2}[A-Z]{0,2}[0-9]{4}`,
+              pattern: `[A-Z]{2}\\s{1}[0-9]{2}\\s{0,1}[A-Z]{1,2}\\s{1}[0-9]{4}`,
               title: t("ES_FSM_VEHICLE_FORMAT_TIP"),
             },
             error: t("FSM_REGISTRY_INVALID_REGISTRATION_NUMBER"),
