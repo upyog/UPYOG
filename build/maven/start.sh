3f565f5c8882190233fd061e9bbbf2f773c21591
flatlineratheist <131427314+flatlineratheist@users.noreply.github.com>
2024-08-13 15:11:57 +0530

    Update start.sh

--- build/maven/start.sh
+++ build/maven/start.sh
@@ -10,5 +10,7 @@ if [ -n "${JAVA_ENABLE_DEBUG}" ] && [ "${JAVA_ENABLE_DEBUG}" != "false" ]; then
     java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-5005}"
 fi
 
+java_opts_preview="--enable-preview"
+
 # Execute the Java application with the provided arguments
 exec java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS} -jar /opt/egov/*.jar
