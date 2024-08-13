38531f1c1d825bf46552a7dc1f98641cdd739e76
flatlineratheist <131427314+flatlineratheist@users.noreply.github.com>
2024-08-13 14:56:34 +0530

    Update start.sh

--- build/maven/start.sh
+++ build/maven/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-if [[ -z "${JAVA_OPTS}" ]];then
+# Check if JAVA_OPTS is empty and set default values if it is
+if [ -z "${JAVA_OPTS}" ]; then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
 
-if [ x"${JAVA_ENABLE_DEBUG}" != x ] && [ "${JAVA_ENABLE_DEBUG}" != "false" ]; then
+# Check if JAVA_ENABLE_DEBUG is set and not false
+if [ -n "${JAVA_ENABLE_DEBUG}" ] && [ "${JAVA_ENABLE_DEBUG}" != "false" ]; then
     java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-5005}"
 fi
 
-exec java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS}  -jar /opt/egov/*.jar
+# Execute the Java application with the provided arguments
+exec java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS} -jar /opt/egov/*.jar
