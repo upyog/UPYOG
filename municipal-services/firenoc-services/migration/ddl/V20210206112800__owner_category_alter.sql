7f9f453efc2e646f789d32e1ed82a247f01461b4
anujkit <46367271+anujkit@users.noreply.github.com>
2024-08-13 15:19:05 +0530

    Update V20210206112800__owner_category_alter.sql

--- municipal-services/firenoc-services/migration/ddl/V20210206112800__owner_category_alter.sql
+++ municipal-services/firenoc-services/migration/ddl/V20210206112800__owner_category_alter.sql
@@ -1,1 +1,3 @@
-ALTER TABLE eg_fn_owner ADD applicantcategory character varying(64);
+--ALTER TABLE eg_fn_owner ADD applicantcategory character varying(64);
+
+ALTER TABLE eg_fn_owner ADD  applicationAssignee character varying(64);
