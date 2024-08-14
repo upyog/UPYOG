6a4668df3889a5f185240218ff650aef442b5f77
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-08-14 17:22:09 +0530

    Update SchemeApiController.java

--- business-services/bmc-service-v1/src/main/java/digit/web/controllers/SchemeApiController.java
+++ business-services/bmc-service-v1/src/main/java/digit/web/controllers/SchemeApiController.java
@@ -51,7 +51,7 @@ public class SchemeApiController {
     }
 
     @PostMapping("/scheme/_search")
-    @CrossOrigin(origins = "http://localhost:3000")
+//    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<SchemeSearchResponse> v1RegistrationSearchPost(
             @ApiParam(value = "Details for Schemes", required = true) 
             @Valid @RequestBody SchemeSearchRequest schemeSearchRequest) {
