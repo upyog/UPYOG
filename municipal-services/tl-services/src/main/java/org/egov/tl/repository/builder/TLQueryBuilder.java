8472684146bb9497b57121aa89c0524133a73ed0
Puli-shilpa <puli.shilpa@pwc.com>
2024-08-19 11:19:24 +0530

    made name case insensitive in tl search

--- municipal-services/tl-services/src/main/java/org/egov/tl/repository/builder/TLQueryBuilder.java
+++ municipal-services/tl-services/src/main/java/org/egov/tl/repository/builder/TLQueryBuilder.java
@@ -213,6 +213,12 @@ public class TLQueryBuilder {
                 builder.append("  LOWER(tl.tradename) = LOWER(?) ");
                 preparedStmtList.add(criteria.getTradeName());
             }
+            
+            if(criteria.getOwnerName() != null) {
+                addClauseIfRequired(preparedStmtList, builder);
+                builder.append("  LOWER(tl.owners.additionalDetails ->>'ownerName') LIKE ?");
+                preparedStmtList.add('%' +criteria.getOwnerName().toLowerCase()+ '%');
+            }
 
             if (criteria.getIssuedFrom() != null) {
                 addClauseIfRequired(preparedStmtList, builder);
