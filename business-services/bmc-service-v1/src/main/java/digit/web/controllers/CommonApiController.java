cd6f88a5e1acff9b6da79cc552b039a30aa8a117
Navajit Mishra <163862517+navajitmishra@users.noreply.github.com>
2024-08-23 13:02:43 +0530

    Update CommonApiController.java

--- business-services/bmc-service-v1/src/main/java/digit/web/controllers/CommonApiController.java
+++ business-services/bmc-service-v1/src/main/java/digit/web/controllers/CommonApiController.java
@@ -45,7 +45,6 @@ public class CommonApiController {
     }
 
     @PostMapping("/common/_get")
-    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<CommonResponse>registrationSearchPost(
             @ApiParam(value = "Details for Schemes", required = true) 
             @Valid @RequestBody CommonRequest commonSearchRequest) {
