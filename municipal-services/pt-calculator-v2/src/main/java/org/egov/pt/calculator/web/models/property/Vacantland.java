a861664d54e23503093317a7bcf4f5c101fe1c56
suryapattanayak <bpattanayak@deloitte.com>
2024-08-26 10:38:20 +0530

    vacant land details

---
+++ municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/web/models/property/Vacantland.java
@@ -0,0 +1,16 @@
+package org.egov.pt.calculator.web.models.property;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Vacantland {
+	
+	private BigDecimal vacantlandamount;
+	
+	private String vacantlandtype;
+
+}
