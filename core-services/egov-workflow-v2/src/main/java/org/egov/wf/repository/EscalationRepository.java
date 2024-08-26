2168f85db147bb43431097e0a986972dfe13d8fd
deepikaarora88 <deepika.arora@pwc.com>
2024-08-26 08:41:11 +0000

    build fix

--- core-services/egov-workflow-v2/src/main/java/org/egov/wf/repository/EscalationRepository.java
+++ core-services/egov-workflow-v2/src/main/java/org/egov/wf/repository/EscalationRepository.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.stereotype.Component;
 
-import jdk.internal.org.jline.utils.Log;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
