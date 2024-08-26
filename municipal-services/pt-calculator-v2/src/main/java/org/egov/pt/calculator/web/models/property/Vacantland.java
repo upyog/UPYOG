d2ba339594684257b0413257a6c11f004e597fa2
suryapattanayak <bpattanayak@deloitte.com>
2024-08-26 10:40:15 +0530

    Update Vacantland.java

--- municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/web/models/property/Vacantland.java
+++ municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/web/models/property/Vacantland.java
@@ -2,11 +2,15 @@ package org.egov.pt.calculator.web.models.property;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vacantland {
 	
 	private BigDecimal vacantlandamount;
