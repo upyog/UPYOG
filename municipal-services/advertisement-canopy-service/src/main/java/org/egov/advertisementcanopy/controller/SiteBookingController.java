e2d8e54426198f3fcaea8002f352f96464b3ea23
prabhatsuyaldeloitte <psuyal@deloitte.com>
2024-09-03 13:37:06 +0530

    Update SiteBookingController.java

--- municipal-services/advertisement-canopy-service/src/main/java/org/egov/advertisementcanopy/controller/SiteBookingController.java
+++ municipal-services/advertisement-canopy-service/src/main/java/org/egov/advertisementcanopy/controller/SiteBookingController.java
@@ -7,6 +7,7 @@ import org.egov.advertisementcanopy.service.SiteBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +20,19 @@ public class SiteBookingController {
 	@Autowired
 	private SiteBookingService service;
 
+	@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
 	@PostMapping("/_create")
 	public ResponseEntity<?> createBooking(@RequestBody SiteBookingRequest siteBookingRequest) {
 		return new ResponseEntity<SiteBookingResponse>(service.createBooking(siteBookingRequest), HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
 	@PostMapping("/_search")
 	public ResponseEntity<?> searchBooking(@RequestBody SiteBookingSearchRequest siteBookingSearchRequest) {
 		return new ResponseEntity<SiteBookingResponse>(service.searchBooking(siteBookingSearchRequest), HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
 	@PostMapping("/_update")
 	public ResponseEntity<?> updateBooking(@RequestBody SiteBookingRequest siteBookingRequest) {
 		return new ResponseEntity<SiteBookingResponse>(service.updateBooking(siteBookingRequest), HttpStatus.OK);
