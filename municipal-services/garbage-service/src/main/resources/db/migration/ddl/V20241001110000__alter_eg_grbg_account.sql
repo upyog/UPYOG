fa85d580daa9cb4961cb4fe91195ae61ec93a210
prabhatsuyaldeloitte <psuyal@deloitte.com>
2024-10-08 12:26:54 +0530

    updates

--- municipal-services/garbage-service/src/main/resources/db/migration/ddl/V20241001110000__alter_eg_grbg_account.sql
+++ municipal-services/garbage-service/src/main/resources/db/migration/ddl/V20241001110000__alter_eg_grbg_account.sql
@@ -1,4 +1,2 @@
 
-ALTER TABLE eg_grbg_account ADD COLUMN parent_account VARCHAR(255);
-ALTER TABLE eg_grbg_account ADD COLUMN is_active BOOLEAN;
-ALTER TABLE eg_grbg_account ADD COLUMN sub_account_count INT8 DEFAULT 0;
+ALTER TABLE eg_grbg_account ADD COLUMN is_parent_account BOOLEAN;
