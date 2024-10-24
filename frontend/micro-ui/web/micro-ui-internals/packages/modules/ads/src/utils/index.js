06ec56459ea619f6581558a4d674c97bc6ccb5cb
Nikhil5922 <nikhilsingh5922@gmail.com>
2024-10-24 13:03:23 +0530

    added create api payload

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/ads/src/utils/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ads/src/utils/index.js
@@ -26,3 +26,95 @@ export const pdfDownloadLink = (documents = {}, fileStoreId = "", format = "") =
     });
   return fileURL;
 };
+
+/**
+ * Payload create api data for the advertisement booking application.
+ * 
+ * setAddressDetails(data)`: Formats and sets address details.
+ * setApplicantDetails(data)`: Formats and sets applicant information.
+ * setDocumentDetails(data)`: Retains document details.
+ * ADSDataConvert(data)`: Combines all details into a structured bookingApplication object for submission.
+ */
+
+export const setaddressDetails = (data) => {
+  let { address } = data;
+
+  let addressdetails = {
+    pincode: address?.pincode,
+    city: address?.city?.city?.name,
+    cityCode:address?.city?.city?.code,
+    locality:address?.locality?.i18nKey,
+    localityCode: address?.locality?.code,
+    streetName: address?.streetName,
+    addressLine1:address?.addressline1,
+    addressLine2:address?.addressline2,
+    houseNo: address?.houseNo,
+    landmark: address?.landmark,
+  };
+
+  data.address = addressdetails;
+  return data;
+
+};
+
+export const setApplicantDetails = (data) => {
+    let { applicant } = data;
+  
+    let propApplicant = {
+      applicantName:applicant?.applicantName,
+      applicantMobileNo:applicant?.mobileNumber,
+      applicantAlternateMobileNo:applicant?.alternateNumber,
+      applicantEmailId:applicant?.emailId,
+    };
+  
+    data.applicant = propApplicant;
+    return data;
+  };
+
+  export const setDocumentDetails = (data) => {
+    let { documents } = data;
+  
+    let doc = {
+      ...documents,
+       
+      
+    };
+  
+    data.documents = doc;
+    return data;
+  };
+
+
+
+export const ADSDataConvert = (data) => {
+ 
+  data = setDocumentDetails(data);
+  data = setApplicantDetails(data);
+  data=setaddressDetails(data);
+const formdata={
+  bookingApplication: {
+    tenantId: data.tenantId,
+    applicantDetail:{
+      ...data.applicant
+    },
+    address:data.address,
+    ...data.documents,
+    bookingStatus:"BOOKING_CREATED",
+    bookingSlotDetails:data?.slotlist,
+    cartDetails:[ {
+                "addType": "Unipolar",
+                "faceArea": "Unipole 12 X 8",
+                "location": "Green Park",
+                "nightLight": false,
+                "bookingDate": "2024-11-30",
+                "bookingFromTime": "06:00",
+                "bookingToTime": "05:59",
+                "status": "BOOKING_CREATED"
+                },],
+
+    workflow:null
+  }
+}
+
+  return formdata;
+};
