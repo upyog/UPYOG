a821e0f78808387ba5e8664724cdf9f7428cb0f8
abhijniua <123440235+abhijniua@users.noreply.github.com>
2024-10-24 14:57:10 +0530

    Correcting Sv Request in backend

--- municipal-services/street-vending/src/main/java/org/upyog/sv/web/models/StreetVendingRequest.java
+++ municipal-services/street-vending/src/main/java/org/upyog/sv/web/models/StreetVendingRequest.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 import org.egov.common.contract.request.RequestInfo;
 import org.springframework.validation.annotation.Validated;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,9 +25,11 @@ import lombok.Setter;
 public class StreetVendingRequest {
 
 	@Valid
+	@JsonProperty("RequestInfo")
 	private RequestInfo requestInfo;
 
 	@Valid
+	@JsonProperty("streetVendingDetail")
 	private StreetVendingDetail streetVendingDetail;
 
 }
