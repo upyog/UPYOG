2d4a62da7e030c165a95edf96c629e438af645c1
Deepa <deepa.o.sd@pwc.com>
2024-10-31 13:05:26 +0000

    Added validation for area and builtuparea in pt

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/PTLandArea.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pt/src/pageComponents/PTLandArea.js
@@ -11,7 +11,10 @@ const Area = ({ t, config, onSelect, value, userType, formData, setError: setFor
 
 
   function setPropertyfloorarea(e) {
-    setfloorarea(e.target.value);
+    const regex=/^(|[1-9][0-9]{0,8}|)$/;
+    if(regex.test(e.target.value)|| e.target.value==" "){
+      setfloorarea(e.target.value);
+    }
   }
 
   const goNext = () => {
