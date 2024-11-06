f88ef339edc56d367dfc76d14b5c199e9e3f512c
mukund-egov <mukund.tripathi@egovernments.org>
2024-11-06 14:29:08 +0530

    modified tqm card

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/components/TqmCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/components/TqmCard.js
@@ -1,4 +1,4 @@
-import { ArrowRightInbox, ShippingTruck, EmployeeModuleCard, Loader } from "@egovernments/digit-ui-react-components";
+import { ArrowRightInbox, ShippingTruck, EmployeeModuleCard, Loader } from "@upyog/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
