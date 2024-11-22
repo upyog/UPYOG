892f0559c35ded29ec980b6b9e2365d6ebdc3f35
Swayanshu Mohapatra <swayanshu.m.mohapatra@pwc.com>
2024-11-22 16:31:58 +0530

    Email Phase 2 excel sheet

--- core-services/national-dashboard-ingest/src/main/java/org/egov/nationaldashboardingest/web/controllers/EmailController.java
+++ core-services/national-dashboard-ingest/src/main/java/org/egov/nationaldashboardingest/web/controllers/EmailController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.nationaldashboardingest.service.StateListDB;
 import org.egov.nationaldashboardingest.utils.EmailUtil;
+import org.egov.nationaldashboardingest.utils.ExcelUtil;
 import org.egov.nationaldashboardingest.web.models.RequestInfoWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.egov.nationaldashboardingest.service.StateFetchService;
 
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.*;
 
 @Slf4j
@@ -31,27 +33,18 @@ public class EmailController {
     @Autowired
     private StateListDB stateListDB;
 
+    @Autowired
+    private ExcelUtil excelUtil;
+
 
     @RequestMapping(value = "/_sendEmail", method = RequestMethod.POST)
-    public ResponseEntity<String> sendEmailtoMissingState(@RequestBody RequestInfoWrapper requestInfoWrapper) {
+    public void sendEmailtoMissingState(@RequestBody RequestInfoWrapper requestInfoWrapper, HttpServletResponse response) throws IOException {
         RequestInfo requestInfo = requestInfoWrapper.getRequestInfo();  // Get RequestInfo from the wrapper
         log.info("Received RequestInfo: {}", requestInfo);
-        Map<String, Map<String,String>> missingStates = new HashMap<>();
+        Map<String, Map<String,Object>> missingStates = new HashMap<>();
 
-        Map<String,Map<String , String>> responseList = stateFetch.stateList(requestInfo);
-        Set<String> dbState = stateListDB.findifrecordexists();
+        Map<String,Map<String , Object>> responseList = stateFetch.stateList(requestInfo);
 
-        for (Map.Entry<String,Map<String, String>> entry : responseList.entrySet()) {
-            String stateCode = entry.getKey();
-            Map<String, String> officerInfo = entry.getValue();
-            if (!dbState.contains(stateCode)) {
-                missingStates.put(stateCode,officerInfo);
-            }
-        }
-        emailUtil.sendEmail(requestInfo, missingStates);
-
-        return ResponseEntity.ok("Emails sent successfully");
+        excelUtil.generateExcelFiles(response,responseList);
     }
-
-
 }
