2efc26ae1a02b775fae807bfc84bd0baecaeb107
Nikhil5922 <nikhilsingh5922@gmail.com>
2024-12-03 12:47:41 +0530

    Added dynamic countdown timer to ChbApplication component. Implemented timeRemaining state and useEffect hook to decrement the timer every second. Time is displayed in "MM:SS" format with interval cleanup.

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/chb/src/pages/citizen/CHBMyApplications/chb-application.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/chb/src/pages/citizen/CHBMyApplications/chb-application.js
@@ -1,4 +1,4 @@
-import { Card, KeyNote, SubmitBar,Toast } from "@upyog/digit-ui-react-components";
+import { Card, KeyNote, SubmitBar,Toast,CardSubHeader } from "@upyog/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
 
 import { useTranslation } from "react-i18next";
@@ -21,7 +21,29 @@ const ChbApplication = ({ application, tenantId, buttonLabel }) => {
     },
     enabled: false, // Disable automatic refetch
   });
-
+  const [timeRemaining, setTimeRemaining] = useState(application?.timerValue);
+  // Initialize time remaining on mount or when application changes
+  useEffect(() => {
+    setTimeRemaining(application?.timerValue || 0);
+  }, [application?.timerValue]);
+  
+  // Timer logic
+  useEffect(() => {
+    if (timeRemaining <= 0) return;
+  
+    const interval = setInterval(() => {
+      setTimeRemaining((prevTime) => Math.max(prevTime - 1, 0));
+    }, 1000);
+  
+    return () => clearInterval(interval); // Cleanup interval
+  }, [timeRemaining]);
+  
+  // Format seconds into "minutes:seconds" format
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
   const getBookingDateRange = (bookingSlotDetails) => {
     if (!bookingSlotDetails || bookingSlotDetails.length === 0) {
       return t("CS_NA");
@@ -65,7 +87,17 @@ const ChbApplication = ({ application, tenantId, buttonLabel }) => {
   }, [showToast]);
   return (
     <Card>
-      <KeyNote keyValue={t("CHB_BOOKING_NO")} note={application?.bookingNo} />
+       <div style={{ display: "flex", justifyContent: "space-between" }}>
+       <KeyNote keyValue={t("CHB_BOOKING_NO")} note={application?.bookingNo} />
+            { timeRemaining>0 && (<CardSubHeader 
+              style={{ 
+                textAlign: 'right', 
+                fontSize: "24px"
+              }}
+            >
+              {t("CS_TIME_REMAINING")}: <span className="astericColor">{formatTime(timeRemaining)}</span>
+            </CardSubHeader>)}
+        </div>
       <KeyNote keyValue={t("CHB_APPLICANT_NAME")} note={application?.applicantDetail?.applicantName} />
       <KeyNote keyValue={t("CHB_COMMUNITY_HALL_NAME")} note={t(`${application?.communityHallCode}`)} />
       <KeyNote keyValue={t("CHB_BOOKING_DATE")} note={getBookingDateRange(application?.bookingSlotDetails)} />
