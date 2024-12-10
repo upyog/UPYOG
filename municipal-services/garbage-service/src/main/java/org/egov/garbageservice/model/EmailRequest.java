da1116521cd118857fbb385d2d33d678d28346c1
sudidey <142474363+sudidey@users.noreply.github.com>
2024-12-10 21:04:06 +0530

    notifications added

---
+++ municipal-services/garbage-service/src/main/java/org/egov/garbageservice/model/EmailRequest.java
@@ -0,0 +1,18 @@
+package org.egov.garbageservice.model;
+
+import org.egov.common.contract.request.RequestInfo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class EmailRequest {
+	private RequestInfo requestInfo;
+
+	private Email email;
+}
---
+++ municipal-services/garbage-service/src/main/java/org/egov/garbageservice/service/NotificationService.java
@@ -0,0 +1,79 @@
+package org.egov.garbageservice.service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.garbageservice.contract.bill.Bill;
+import org.egov.garbageservice.enums.SMSCategory;
+import org.egov.garbageservice.model.Email;
+import org.egov.garbageservice.model.EmailRequest;
+import org.egov.garbageservice.model.GarbageAccount;
+import org.egov.garbageservice.model.SMSRequest;
+import org.egov.garbageservice.util.RequestInfoWrapper;
+import org.egov.tracer.kafka.CustomKafkaTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class NotificationService {
+
+	private static final String EMAIL_BODY_GENERATE_BILL = "Email Sent Successfully";
+
+	private static final String SMS_BODY_GENERATE_BILL = "Message Sent Successfully";
+
+	private static final String EMAIL_SUBJECT_GENERATE_BILL = "Goa-IDC OPEN: Congratulations! Your Application Has Been Approved";
+
+	@Autowired
+	private CustomKafkaTemplate<String, Object> kafkaTemplate;
+
+	@Value("${kafka.topics.email.service.topic.name}")
+	private String emailTopic;
+
+	@Value("${kafka.topics.sms.service.topic.name}")
+	private String smsTopic;
+
+	public void sendSms(String message, String mobileNumber) {
+
+		SMSRequest smsRequest = SMSRequest.builder().message(message).mobileNumber(mobileNumber)
+				.category(SMSCategory.NOTIFICATION).build();
+
+		kafkaTemplate.send(smsTopic, smsRequest);
+	}
+
+	private void sendEmail(String emailBody, List<String> emailIds, RequestInfo requestInfo,
+			List<String> attachmentDocRefIds, String emailSubject) {
+		Email email = new Email();
+		email.setEmailTo(new HashSet<>(emailIds));
+		email.setBody(emailBody);
+		email.setSubject(emailSubject);
+
+		if (!CollectionUtils.isEmpty(attachmentDocRefIds)) {
+			email.setHTML(true);
+			email.setFileStoreIds(attachmentDocRefIds);
+		} else {
+			email.setHTML(false);
+		}
+
+		EmailRequest emailRequest = EmailRequest.builder().requestInfo(requestInfo).email(email).build();
+		kafkaTemplate.send(emailTopic, emailRequest);
+	}
+
+	public void triggerNotificationsGenerateBill(GarbageAccount garbageAccount, Bill bill,
+			RequestInfoWrapper requestInfoWrapper) {
+		String emailBody = EMAIL_BODY_GENERATE_BILL;
+		String smsBody = SMS_BODY_GENERATE_BILL;
+		String emailSubject = EMAIL_SUBJECT_GENERATE_BILL;
+		sendEmail(emailBody, Collections.singletonList(garbageAccount.getEmailId()),
+				requestInfoWrapper.getRequestInfo(), null, emailSubject);
+		sendSms(smsBody, garbageAccount.getMobileNumber());
+
+	}
+
+}
