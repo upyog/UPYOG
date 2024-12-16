1225e1074409c8473e1c9faf693d93803cb63bff
palamgarg <palamgarg@gmail.com>
2024-12-16 15:51:08 +0530

    Added

---
+++ core-services/egov-user/src/main/java/org/egov/user/security/oauth2/custom/authproviders/CustomThirdPartyAuthenticationProvider.java
@@ -0,0 +1,7 @@
+package org.egov.user.security.oauth2.custom.authproviders;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+
+public interface CustomThirdPartyAuthenticationProvider extends AuthenticationProvider{
+
+}
