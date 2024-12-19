554f362667d0a02ce699ebc639ceb1f31efc62ed
abhijniua <123440235+abhijniua@users.noreply.github.com>
2024-12-19 15:01:58 +0530

    Adding index in sv mobile number

--- municipal-services/street-vending/src/main/resources/db/migration/main/V20241117142800__sv_indexer.sql
+++ municipal-services/street-vending/src/main/resources/db/migration/main/V20241117142800__sv_indexer.sql
@@ -2,3 +2,5 @@ CREATE INDEX idx_sv_application_status ON eg_sv_street_vending_detail (applicati
 CREATE INDEX idx_sv_application_no ON eg_sv_street_vending_detail (application_no);
 CREATE INDEX idx_sv_vending_activity ON eg_sv_street_vending_detail (vending_activity);
 CREATE INDEX idx_sv_vending_zone ON eg_sv_street_vending_detail (vending_zone);
+CREATE INDEX idx_vendor_mobile_no ON eg_sv_vendor_detail (mobile_no);
+
