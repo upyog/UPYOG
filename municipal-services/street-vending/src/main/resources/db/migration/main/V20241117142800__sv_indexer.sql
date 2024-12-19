c49b6bc00996e63d1a0555fb7d6dece35fd78a0b
abhijniua <123440235+abhijniua@users.noreply.github.com>
2024-12-19 12:54:22 +0530

    Adding indexers in adv, ew, ptr, sv

---
+++ municipal-services/street-vending/src/main/resources/db/migration/main/V20241117142800__sv_indexer.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_sv_application_status ON eg_sv_street_vending_detail (application_status);
+CREATE INDEX idx_sv_application_no ON eg_sv_street_vending_detail (application_no);
+CREATE INDEX idx_sv_vending_activity ON eg_sv_street_vending_detail (vending_activity);
+CREATE INDEX idx_sv_vending_zone ON eg_sv_street_vending_detail (vending_zone);
