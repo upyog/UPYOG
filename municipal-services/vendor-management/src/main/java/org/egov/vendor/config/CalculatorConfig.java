46d726f5c8ea12f7b0eb8d7167aac733a5d3b3ce
BimalKumarNIUA <bimalkumar@niua.org>
2024-12-24 16:24:36 +0530

    Vendor Management base Code

--- municipal-services/vendor-management/src/main/java/org/egov/vendor/config/CalculatorConfig.java
+++ municipal-services/vendor-management/src/main/java/org/egov/vendor/config/CalculatorConfig.java
@@ -8,17 +8,5 @@ import org.springframework.context.annotation.Configuration;
 @Data
 public class CalculatorConfig {
 
-	@Value("${scheduler.batch.size}")
-	private int batchSize;
-
-	// MDMS
-	@Value("${egov.mdms.host}")
-	private String mdmsHost;
-
-	@Value("${egov.mdms.search.endpoint}")
-	private String mdmsSearchEndpoint;
-
-	@Value("${default.tenant.id}")
-	private String defaultTenantId;
 
 }
