e8179dac7c38b8cbd7712ba5ae137265507a5ade
sourmaity <sourmaity@deloitte.com>
2025-01-02 12:18:38 +0530

    Update PayNowRequest.java

--- municipal-services/garbage-service/src/main/java/org/egov/garbageservice/model/PayNowRequest.java
+++ municipal-services/garbage-service/src/main/java/org/egov/garbageservice/model/PayNowRequest.java
@@ -19,6 +19,8 @@ public class PayNowRequest {
 
 	private List<String> garbageApplicationNumbers;
 
+	private List<String> garbageUuid;
+
 	private List<String> billStatus;
 
 	private String month;
