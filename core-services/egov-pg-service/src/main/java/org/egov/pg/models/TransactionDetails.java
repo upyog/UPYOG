08ef0c47709a16b830c7b5c9905521e2f4d03ccf
sourmaity <sourmaity@deloitte.com>
2025-01-02 16:22:16 +0530

    changes

---
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/models/TransactionDetails.java
@@ -0,0 +1,36 @@
+package org.egov.pg.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class TransactionDetails {
+
+	private String uuid;
+
+	private String txnId;
+
+	private String txnAmount;
+
+	private String billId;
+
+	private String consumerCode;
+
+	@JsonProperty("additionalDetails")
+	private Object additionalDetails;
+
+	@JsonProperty("auditDetails")
+	private AuditDetails auditDetails;
+
+}
