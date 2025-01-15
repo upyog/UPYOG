0259c324b1685dc861d95d87b4fc40902048e70b
Nikhil5922 <nikhilkumar7800@gmail.com>
2025-01-15 17:37:02 +0530

    Add draftId prop to TimerValues component and validate date range in ADSearch

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/ads/src/pageComponents/ADSRequiredDoc.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/ads/src/pageComponents/ADSRequiredDoc.js
@@ -1,13 +1,14 @@
 import { Card, CardHeader, CardSubHeader, CardText, Loader, SubmitBar } from "@nudmcdgnpm/digit-ui-react-components";
 import React, { useEffect } from "react";
 import { stringReplaceAll } from "../utils";
+import { TimerValues } from "../components/TimerValues";
 
 /*
   ADSRequiredDoc  displays the info page for  required documents for an advertisement to book
    and it also shows upload restrictions and document requirements.
 */
 
-const ADSRequiredDoc = ({ t, config, onSelect, userType, formData }) => {
+const ADSRequiredDoc = ({ t, config, onSelect, userType, formData,value=formData.adslist }) => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const stateId = Digit.ULBService.getStateId();
   sessionStorage.removeItem("docReqScreenByBack");
@@ -22,7 +23,15 @@ const ADSRequiredDoc = ({ t, config, onSelect, userType, formData }) => {
   return (
     <React.Fragment>
       <Card>
+      <div style={{display:"flex", justifyContent: "space-between", width: "100%" }}>
       <CardHeader>{t("MODULE_ADS")}</CardHeader>
+      <CardSubHeader>
+        <TimerValues 
+        timerValues={value?.existingDataSet?.timervalue?.timervalue} 
+        SlotSearchData={value?.cartDetails} draftId={value?.existingDataSet?.draftId}
+      />
+      </CardSubHeader>
+      </div>
         <div>
          <CardText className={"primaryColor"}>{t("ADS_DOC_REQ_SCREEN_SUB_HEADER")}</CardText>
           <CardText className={"primaryColor"}>{t("ADS_DOC_REQ_SCREEN_TEXT")}</CardText>
