febd84b41c2e8768a6b8de4fed09e78d54468ec1
manviraheja1 <119833905+manviraheja1@users.noreply.github.com>
2025-01-21 12:12:08 +0530

    Updating for architect role issue

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/consumer/BPAConsumer.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/consumer/BPAConsumer.java
@@ -25,7 +25,7 @@ public class BPAConsumer {
 	@Autowired
 	private BPANotificationService notificationService;
 	
-	@KafkaListener(topics = { "${persister.update.buildingplan.topic}", "${persister.save.buildingplan.topic}",
+	@KafkaListener(topics = { "${persister.update.buildingplan.topic}",
 			"${persister.update.buildingplan.workflow.topic}" })
 	public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
 		ObjectMapper mapper = new ObjectMapper();
