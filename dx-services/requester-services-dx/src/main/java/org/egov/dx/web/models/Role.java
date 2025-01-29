5de025e818a7c9de228420136289afa85a176855
manviraheja1 <manvi.raheja@pwc.com>
2025-01-29 18:15:41 +0530

    digilocker changes

---
+++ dx-services/requester-services-dx/src/main/java/org/egov/dx/web/models/Role.java
@@ -0,0 +1,28 @@
+package org.egov.dx.web.models;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(of = {"code", "tenantId"})
+public class Role {
+    private static final String CITIZEN = "CITIZEN";
+    private String name;
+    private String code;
+    private String description;
+    private Long createdBy;
+    private Date createdDate;
+    private Long lastModifiedBy;
+    private Date lastModifiedDate;
+    private String tenantId;
+
+    public static Role getCitizenRole() {
+        return Role.builder().code(CITIZEN).build();
+    }
+}
