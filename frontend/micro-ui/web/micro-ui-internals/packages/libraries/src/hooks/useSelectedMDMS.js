b488332bc2c98672d2955f6afbd0a7ef75e54143
rashidkhalid1212 <rashidkhalid9114@gmail.com>
2025-01-29 17:17:04 +0530

    added comments

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useSelectedMDMS.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useSelectedMDMS.js
@@ -1,3 +1,12 @@
+/**
+ * Determines which MDMS service reference should be used.
+ * 
+ * This function checks the flag `mdmsV2Enabled` to decide whether to use the 
+ * new version of the MDMS service (`MdmsServiceV2`) or the old one (`MdmsService`). 
+ * It then assigns the appropriate service reference to `mdmsRef`.
+ * 
+ */
+
 import { MdmsService } from "../services/elements/MDMS";
 import { MdmsServiceV2 } from "../services/elements/MDMSV2";
 
