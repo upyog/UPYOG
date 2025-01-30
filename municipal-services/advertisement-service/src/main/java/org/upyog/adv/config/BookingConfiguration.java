14d48b4ffb92705759736c5bad4b437bbbcf987e
rashidkhalid1212 <rashidkhalid9114@gmail.com>
2025-01-30 11:00:31 +0530

    resolved import package error

--- municipal-services/advertisement-service/src/main/java/org/upyog/adv/config/BookingConfiguration.java
+++ municipal-services/advertisement-service/src/main/java/org/upyog/adv/config/BookingConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
-import jdk.internal.org.jline.utils.Log;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
