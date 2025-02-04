16d4a5dba84b10e544cb438d84f084d1f55da224
anubshrma <anubsharma.ext@deloitte.com>
2025-02-04 18:52:05 +0530

    fixes for time stamp

--- core-services/egov-pg-service/src/main/java/org/egov/pg/Main.java
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/Main.java
@@ -1,7 +1,12 @@
 package org.egov.pg;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.tracer.config.TracerConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,10 +16,18 @@ import org.springframework.stereotype.Component;
 @Component
 @Import({TracerConfiguration.class, MultiStateInstanceUtil.class})
 public class Main {
+	
+
+	@Value("${app.timezone}")
+	private String timeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
 
     }
-
+    
+	@PostConstruct
+	public void initialize() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+	}
 }
