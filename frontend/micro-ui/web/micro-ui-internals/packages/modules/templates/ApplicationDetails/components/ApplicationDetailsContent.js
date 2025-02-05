b4747759dec0cb22e235c7e6cefe46242403393f
Srirranjan Srivastava <sriranjan.srivastava@pwc.com>
2025-02-05 08:48:19 +0530

    published modules

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/components/ApplicationDetailsContent.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/components/ApplicationDetailsContent.js
@@ -133,15 +133,15 @@ console.log("appl", applicationDetails)
           }
         },
       };
-      const previousCheckpoint = timeline?.[index - 1];
+      const previousCheckpoint = timeline && timeline[index - 1] &&timeline?.[index - 1];
       const caption = {
         date: checkpoint?.auditDetails?.lastModified,
         name: checkpoint?.assignes?.[0]?.name,
         mobileNumber: applicationData?.processInstance?.assignes?.[0]?.uuid === checkpoint?.assignes?.[0]?.uuid && applicationData?.processInstance?.assignes?.[0]?.mobileNumber 
                      ? applicationData?.processInstance?.assignes?.[0]?.mobileNumber 
                      : checkpoint?.assignes?.[0]?.mobileNumber,
-        comment: t(checkpoint?.comment),
-        wfComment: previousCheckpoint ? previousCheckpoint.wfComment : [],
+        comment: t(checkpoint && checkpoint?.comment),
+        wfComment: previousCheckpoint ? previousCheckpoint && previousCheckpoint.wfComment : [],
         thumbnailsToShow: checkpoint?.thumbnailsToShow,
       };
       
