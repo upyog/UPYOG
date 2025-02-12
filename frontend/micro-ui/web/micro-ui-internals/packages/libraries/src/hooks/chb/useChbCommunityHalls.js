66478b6254f155ec3e89bf636ba64a717f3404c6
sourabh7yy <sourabh7y@gmail.com>
2025-02-12 13:00:17 +0530

    useChbCommunityHalls to simplify return logic by removing unnecessary switch statement

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbCommunityHalls.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbCommunityHalls.js
@@ -4,13 +4,6 @@ const useChbCommunityHalls = (tenantId, moduleCode, type, config = {}) => {
   const useCommunityHalls = () => {
     return useQuery("COMMUNITY_HALLS", () => Digit.Hooks.useSelectedMDMS().getMasterData(tenantId, moduleCode, "CommunityHalls", "CHB_COMMUNITY_HALLS_", "i18nKey"), config);
   };
-  
-
-  switch (type) {
-    case "CommunityHalls":
-      return useCommunityHalls();
-    default:
-      return null;
-  }
-};
+  return useCommunityHalls();
+ };
 export default useChbCommunityHalls;
