3025cab68c3e12a7cc4cdd81522607ba8eacd5e2
keshav-NIUA <keshav@niua.org>
2025-02-21 10:08:48 +0530

    removed compilation error

--- municipal-services/cnd-service/src/main/java/org/upyog/cdwm/config/CNDConfiguration.java
+++ municipal-services/cnd-service/src/main/java/org/upyog/cdwm/config/CNDConfiguration.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 
 @Component
 @Getter
