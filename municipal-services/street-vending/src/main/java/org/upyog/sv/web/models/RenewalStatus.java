5d6a6287062c938c5245b90b655c107024624255
abhijniua <123440235+abhijniua@users.noreply.github.com>
2025-02-25 10:16:24 +0530

    Resolving search issue of renewal status in sv

--- municipal-services/street-vending/src/main/java/org/upyog/sv/web/models/RenewalStatus.java
+++ municipal-services/street-vending/src/main/java/org/upyog/sv/web/models/RenewalStatus.java
@@ -1,21 +1,10 @@
 package org.upyog.sv.web.models;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum RenewalStatus {
-    ELIGIBLE_TO_RENEW("EligibleToRenew"),// when a application is eligible for renewal as set in scheduler
-    RENEW_IN_PROGRESS("RenewInProgress"),// when first draft call is made or straight to payment is clicked
-    RENEW_APPLICATION_CREATED("RenewApplicationCreated"),// when renewed application is created and saved
-    RENEWED("Renewed");// when the application is renewed 
 
-    private final String value;
+	ELIGIBLE_TO_RENEW, // when a application is eligible for renewal as set in scheduler
+	RENEW_IN_PROGRESS, // when first draft call is made or straight to payment is clicked
+	RENEW_APPLICATION_CREATED, // when renewed application is created and saved
+	RENEWED;// when the application is renewed
 
-    RenewalStatus(String value) {
-        this.value = value;
-    }
-
-    @JsonValue
-    public String getValue() {
-        return value;
-    }
 }
