e71c19d3f1003a4f5052c03d62858b20482cd54f
rashidkhalid1212 <rashidkhalid9114@gmail.com>
2025-02-26 21:25:17 +0530

    verification search issue resolved for login without username functionality

--- business-services/verification-service/src/main/java/org/upyog/service/CommonServiceImpl.java
+++ business-services/verification-service/src/main/java/org/upyog/service/CommonServiceImpl.java
@@ -6,17 +6,19 @@ import org.egov.common.contract.request.RequestInfo;
 import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.upyog.config.ModuleConfig;
+import org.upyog.constants.VerificationSearchConstants;
 import org.upyog.mapper.CommonDetailsMapper;
 import org.upyog.mapper.CommonDetailsMapperFactory;
 import org.upyog.repository.ServiceRequestRepository;
 import org.upyog.web.models.CommonDetails;
+import org.upyog.service.UserService;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import digit.models.coremodels.RequestInfoWrapper;
+import digit.models.coremodels.UserDetailResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -33,6 +35,9 @@ public class CommonServiceImpl implements CommonService {
 
 	@Autowired
 	private ServiceRequestRepository serviceRequestRepository;
+	
+	@Autowired
+	private UserService userService;
 
 	@Autowired
 	public CommonServiceImpl(ModuleConfig moduleConfig, CommonDetailsMapperFactory mapperFactory) {
@@ -41,6 +46,45 @@ public class CommonServiceImpl implements CommonService {
 		this.moduleEndpoints = moduleConfig.getEndpoint();
 		this.moduleUniqueIdParams = moduleConfig.getUniqueIdParam();
 		this.mapperFactory = mapperFactory;
+	}
+	
+	
+	/**
+	 * Retrieves the system user’s RequestInfo based on a predefined system username.
+	 * 
+	 * @param userService The UserService instance used to fetch user details.
+	 * @return A RequestInfo object containing the system user’s details.
+	 * @throws IllegalStateException if the system user is not found.
+	 */
+	private RequestInfo getSystemRequestInfo(UserService userService) {
+	    UserDetailResponse userDetailResponse = userService.searchByUserName(
+	        VerificationSearchConstants.SYSTEM_CITIZEN_USERNAME, 
+	        VerificationSearchConstants.VS_TENANTID
+	    );
+
+	    if (userDetailResponse == null || userDetailResponse.getUser().isEmpty()) {
+	        throw new IllegalStateException("SYSTEM user not found for tenant '" + VerificationSearchConstants.VS_TENANTID + "'.");
+	    }
+
+	    RequestInfo systemRequestInfo = RequestInfo.builder()
+	        .userInfo(userDetailResponse.getUser().get(0))
+	        .build();
+
+	    log.info("RequestInfo of system User: " + systemRequestInfo);
+	    return systemRequestInfo;
+	}
+	
+	/**
+	 * Determines the effective RequestInfo to be used.
+	 * If the given requestInfo contains user information, it is returned.
+	 * Otherwise, the systemRequestInfo is used as a fallback.
+	 * 
+	 * @param requestInfo The original RequestInfo provided in the request.
+	 * @param systemRequestInfo The fallback RequestInfo containing system user details.
+	 * @return The effective RequestInfo to be used for further processing.
+	 */
+	private RequestInfo getEffectiveRequestInfo(RequestInfo requestInfo, RequestInfo systemRequestInfo) {
+	    return requestInfo.getUserInfo() != null ? requestInfo : systemRequestInfo;
 	}
 
 	@Override
@@ -66,7 +110,9 @@ public class CommonServiceImpl implements CommonService {
 		StringBuilder urlBuilder = new StringBuilder();
 		urlBuilder.append(host).append(endpoint).append("?").append(uniqueIdParam).append("=").append(applicationNumber)
 		.append("&tenantId=").append(tenantId);
-		RequestInfoWrapper requestInfoWrapper= RequestInfoWrapper.builder().requestInfo(requestInfo).build();
+		RequestInfoWrapper requestInfoWrapper= RequestInfoWrapper.builder().requestInfo(getEffectiveRequestInfo(requestInfo, getSystemRequestInfo(userService))).build();
+
+		log.info("requestInfoWrapper data : " + requestInfoWrapper);
 		Object result = null;
 		try {
 			log.info("urlBuilder : " + urlBuilder);
