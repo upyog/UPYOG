44868798ac41e56daa0d7259f6f7bba05dffbc77
manviraheja1 <119833905+manviraheja1@users.noreply.github.com>
2025-03-18 17:41:27 +0530

    Update CalculationUtils.java

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/utils/CalculationUtils.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/utils/CalculationUtils.java
@@ -87,14 +87,14 @@ public class CalculationUtils {
 		String billingBusinessService;
 		switch (feeType) {
 		case BPACalculatorConstants.MDMS_CALCULATIONTYPE_APL_FEETYPE:
-			if (businessService.equalsIgnoreCase(BPACalculatorConstants.MDMS_BPA)) {
+			if (businessService.equalsIgnoreCase(BPACalculatorConstants.MDMS_BPA) || businessService.equalsIgnoreCase(BPACalculatorConstants.BUSINESSSERVICE_PREAPPROVEDPLAN)) {
 				billingBusinessService = config.getApplFeeBusinessService();
 			} else {
 				billingBusinessService = config.getOCApplBusinessservice();
 			}
 			break;
 		case BPACalculatorConstants.MDMS_CALCULATIONTYPE_SANC_FEETYPE:
-			if (businessService.equalsIgnoreCase(BPACalculatorConstants.MDMS_BPA)) {
+			if (businessService.equalsIgnoreCase(BPACalculatorConstants.MDMS_BPA) || businessService.equalsIgnoreCase(BPACalculatorConstants.BUSINESSSERVICE_PREAPPROVEDPLAN)) {
 				billingBusinessService = config.getSanclFeeBusinessService();
 			} else {
 				billingBusinessService = config.getOCSancBusinessservice();
