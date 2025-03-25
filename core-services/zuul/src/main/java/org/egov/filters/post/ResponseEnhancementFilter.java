70e5dd4ee429d2421acbafe1408f2abaa2314e9e
bdhal1989 <bdhal@deloitte.com>
2025-03-25 13:02:36 +0530

    update Nonce

--- core-services/zuul/src/main/java/org/egov/filters/post/ResponseEnhancementFilter.java
+++ core-services/zuul/src/main/java/org/egov/filters/post/ResponseEnhancementFilter.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import static org.egov.constants.RequestContextConstants.CORRELATION_ID_KEY;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 /**
  * Sets the correlation id to the response header.
@@ -55,8 +57,43 @@ public class ResponseEnhancementFilter extends ZuulFilter {
         ctx.addZuulResponseHeader(X_CONTENT_TYPE_OPTIONS,"nosniff");
         ctx.addZuulResponseHeader(X_FRAME_OPTIONS,"DENY");
         ctx.addZuulResponseHeader(X_XSS_PROTECTION,"1; mode=block");
-        ctx.addZuulResponseHeader(CONTENT_SECURITY_POLICY, "default-src 'self'");
+        
+      //  ctx.addZuulResponseHeader(CONTENT_SECURITY_POLICY, "default-src 'self'");
+       final String fontgoogleApiUrl= "https://fonts.googleapis.com; ";
+       final String fontstaticUrl= " https://fonts.gstatic.com data:; ";
+       final String teraformUrl= "https://mnptapp-terraform.s3.ap-south-1.amazonaws.com; ";
+       
         ctx.addZuulResponseHeader(PERMISSIONS_POLICY, "geolocation=(self)");
+        final SecureRandom SECURE_RANDOM = new SecureRandom();
+        byte[] nonceBytes = new byte[16];
+        SECURE_RANDOM.nextBytes(nonceBytes);
+        String nonce = Base64.getEncoder().encodeToString(nonceBytes);
+ 
+        // Set CSP header dynamically with nonce
+       /* ctx.addZuulResponseHeader(CONTENT_SECURITY_POLICY,
+                "default-src 'self'; " +
+                "script-src 'self' ' nonce-" + nonce + "'; " +
+                "style-src 'self' ' nonce-" + nonce + "'"+ fontgoogleApiUrl+
+                "font-src 'self'"+ fontstaticUrl+
+                "img-src 'self' "+ teraformUrl+
+                "frame-ancestors 'none';");*/
+        
+        ctx.addZuulResponseHeader(CONTENT_SECURITY_POLICY,
+                "default-src 'self'; " +
+                "script-src 'self' 'nonce-" + nonce + "'; " +
+                "style-src 'self' 'nonce-" + nonce + "' "+ fontgoogleApiUrl+
+                "font-src 'self'"+ fontstaticUrl+
+                "img-src 'self' "+ teraformUrl+
+                "frame-ancestors 'none';");
+      
+    	
+      //  default-src 'self'; script-src 'self' 'nonce-sVaHuG7g5ay+Q1QWsrxmtg=='; style-src 'self' ' nonce-sVaHuG7g5ay+Q1QWsrxmtg=='https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' https://mnptapp-terraform.s3.ap-south-1.amazonaws.com; frame-ancestors 'none';
+        
+      //  default-src 'self'; script-src 'self' 'nonce-UauTkwu4Vz9x9Eq0UDKaDw==' https://s3.ap-south-1.amazonaws.com; style-src 'self' 'nonce-UauTkwu4Vz9x9Eq0UDKaDw==' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' https://mnptapp-terraform.s3.ap-south-1.amazonaws.com; frame-ancestors 'none';
+        ctx.addZuulRequestHeader ("cspNonce", nonce);
+        ctx.getRequest().setAttribute("cspNonce", nonce);
+        
+        
         return null;
     }
 
