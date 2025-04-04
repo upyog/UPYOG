e5768f8a7372ca7109e20e0eacc609c578094e53
nehaniua <neha@niua.org>
2025-04-04 14:36:04 +0530

    Added waste type detail and document detail method

--- municipal-services/cnd-service/src/main/java/org/upyog/cdwm/repository/querybuilder/CNDServiceQueryBuilder.java
+++ municipal-services/cnd-service/src/main/java/org/upyog/cdwm/repository/querybuilder/CNDServiceQueryBuilder.java
@@ -17,27 +17,39 @@ public class CNDServiceQueryBuilder {
 
     @Autowired
     private CNDConfiguration cndServiceConfiguration;
-    
-    private static final String APPLICATION_DETAILS_SEARCH_QUERY = 
-            "SELECT ucad.application_id, application_number, application_type, vehicle_type, type_of_construction, deposit_centre_details, " +
-            "applicant_detail_id, requested_pickup_date, application_status, additional_details, house_area, " +
-            "construction_from_date, construction_to_date, property_type, total_waste_quantity, no_of_trips, ucad.vehicle_id, " +
-            "vendor_id, pickup_date, completed_on, ucad.created_by, ucad.last_modified_by, ucad.created_time, " +
-            "ucad.last_modified_time, ucad.tenant_id, ucad.applicant_detail_id, ucad.address_detail_id, " +
-            "ucad.applicant_mobile_number, " +
-            "ucwd.application_id AS waste_application_id, ucwd.entered_by_user_type, ucwd.waste_type_id, ucwd.waste_type, ucwd.quantity, ucwd.metrics, " + 
-            "udoc.document_detail_id, udoc.document_type, udoc.uploaded_by_user_type, udoc.file_store_id, " +
-            "uddc.disposal_id, uddc.vehicle_id AS disposal_vehicle_id, uddc.vehicle_depot_no, uddc.net_weight, " +
-            "uddc.gross_weight, uddc.dumping_station_name, uddc.disposal_date, uddc.disposal_type, " +
-            "uddc.name_of_disposal_site, uddc.created_by AS disposal_created_by, uddc.last_modified_by AS disposal_last_modified_by, " +
-            "uddc.created_time AS disposal_created_time, uddc.last_modified_time AS disposal_last_modified_time " +
-            "FROM public.ug_cnd_application_details ucad " +
-            "LEFT JOIN public.ug_cnd_waste_detail ucwd ON ucad.application_id = ucwd.application_id " +
-            "LEFT JOIN public.ug_cnd_document_detail udoc ON ucad.application_id = udoc.application_id " +
-            "LEFT JOIN public.ug_cnd_disposal_deposit_centre_detail uddc ON ucad.application_id = uddc.application_id";
-    
 
     
+    private static final String CND_APPLICATION_DETAILS_QUERY =
+    	    "SELECT ucad.application_id, application_number, application_type, vehicle_type, type_of_construction, deposit_centre_details, " +
+    	    "applicant_detail_id, requested_pickup_date, application_status, additional_details, house_area, " +
+    	    "construction_from_date, construction_to_date, property_type, total_waste_quantity, no_of_trips, ucad.vehicle_id, " +
+    	    "vendor_id, pickup_date, completed_on, ucad.created_by, ucad.last_modified_by, ucad.created_time, " +
+    	    "ucad.last_modified_time, ucad.tenant_id, ucad.applicant_detail_id, ucad.address_detail_id, " +
+    	    "ucad.applicant_mobile_number " +
+    	    "FROM ug_cnd_application_details ucad";
+
+    
+    private static final String WASTE_DETAIL_SELECT_QUERY =
+    	    "SELECT wd.application_id as wasteDetailApplicationId, " +
+    	    "waste_type_id as wasteTypeId, " +
+    	    "entered_by_user_type as enteredByUserType, " +
+    	    "waste_type as wasteType, " +
+    	    "quantity, " +
+    	    "metrics " +
+    	    "FROM ug_cnd_waste_detail wd " +
+    	    "JOIN ug_cnd_application_details ucad ON ucad.application_id = wd.application_id ";
+
+
+    private static final String DOCUMENT_DETAIL_SELECT_QUERY =
+    	    "SELECT document_detail_id as documentDetailId, " +
+    	    "dd.application_id as documentDetailApplicationId, " +
+    	    "document_type as documentType, " +
+    	    "uploaded_by_user_type as uploadedByUserType, " +
+    	    "file_store_id as fileStoreId " + 
+    	    "FROM ug_cnd_document_detail dd " +
+    	    "JOIN ug_cnd_application_details ucad ON ucad.application_id = dd.application_id " ;
+  
+			
     // Pagination wrapper query
     private static final String PAGINATION_WRAPPER = 
         "SELECT * FROM (SELECT *, ROW_NUMBER() OVER (ORDER BY created_time DESC) AS offset_ FROM ({}) result) " +
@@ -55,7 +67,13 @@ public class CNDServiceQueryBuilder {
      */
     public String getCNDApplicationQuery(CNDServiceSearchCriteria criteria, List<Object> preparedStmtList) {
         
-        StringBuilder query = new StringBuilder(criteria.isCountCall() ? APPLICATIONS_COUNT_QUERY : APPLICATION_DETAILS_SEARCH_QUERY);
+    	StringBuilder query;
+    	if (!criteria.isCountCall()) {
+    	    query = new StringBuilder(CND_APPLICATION_DETAILS_QUERY);
+    	} else {
+    	    query = new StringBuilder(APPLICATIONS_COUNT_QUERY);
+    	}
+
         
         if (!ObjectUtils.isEmpty(criteria.getTenantId())) {
             addClauseIfRequired(query, preparedStmtList);
@@ -93,6 +111,92 @@ public class CNDServiceQueryBuilder {
         // Apply pagination for non-count queries
         return addPaginationWrapper(query.toString(), preparedStmtList, criteria);
     }
+    
+    /**
+     * Builds the SQL query to fetch waste type details based on the provided search criteria.
+     * Supports filtering by application number (comma-separated for multiple), tenant ID, 
+     * application status, and applicant mobile number.
+     *
+     * The corresponding values are added to the provided {@code preparedStmtList} to be used in a
+     * parameterized query execution.
+     *
+     * @param criteria the search criteria for filtering waste type records
+     * @param preparedStmtList the list to which query parameters will be added
+     * @return the final SQL query string for fetching waste type details
+     */
+    
+    public String getCNDWasteQuery(CNDServiceSearchCriteria criteria, List<Object> preparedStmtList) {
+        StringBuilder query = new StringBuilder(WASTE_DETAIL_SELECT_QUERY + " WHERE 1=1 ");
+
+        if (!ObjectUtils.isEmpty(criteria.getApplicationNumber())) {
+            String[] appNumbers = criteria.getApplicationNumber().split(",");
+            String placeholders = String.join(",", Collections.nCopies(appNumbers.length, "?"));
+            query.append(" AND ucad.application_number IN (" + placeholders + ") ");
+            Collections.addAll(preparedStmtList, appNumbers);
+        }
+        
+        if (!ObjectUtils.isEmpty(criteria.getTenantId())) {
+            query.append(" AND ucad.tenant_id LIKE ? ");
+            preparedStmtList.add("%" + criteria.getTenantId() + "%");
+        }
+
+        if (!ObjectUtils.isEmpty(criteria.getStatus())) {
+            query.append(" AND ucad.application_status = ? ");
+            preparedStmtList.add(criteria.getStatus());
+        }
+
+        if (!ObjectUtils.isEmpty(criteria.getMobileNumber())) {
+            query.append(" AND ucad.applicant_mobile_number = ? ");
+            preparedStmtList.add(criteria.getMobileNumber());
+        }
+
+
+        return query.toString();
+    }
+
+    
+    /**
+     * Builds the SQL query to fetch document details based on the provided search criteria.
+     * Supports filtering by application number (comma-separated for multiple), tenant ID, 
+     * application status, and applicant mobile number.
+     *
+     * The corresponding values are added to the provided {@code preparedStmtList} to be used in a
+     * parameterized query execution.
+     *
+     * @param criteria the search criteria for filtering document records
+     * @param preparedStmtList the list to which query parameters will be added
+     * @return the final SQL query string for fetching document details
+     */
+    
+    
+    public String getCNDDocQuery(CNDServiceSearchCriteria criteria, List<Object> preparedStmtList) {
+        StringBuilder query = new StringBuilder(DOCUMENT_DETAIL_SELECT_QUERY + " WHERE 1=1 ");
+
+        if (!ObjectUtils.isEmpty(criteria.getApplicationNumber())) {
+            String[] appNumbers = criteria.getApplicationNumber().split(",");
+            String placeholders = String.join(",", Collections.nCopies(appNumbers.length, "?"));
+            query.append(" AND ucad.application_number IN (" + placeholders + ") ");
+            Collections.addAll(preparedStmtList, appNumbers);
+        }
+        
+        if (!ObjectUtils.isEmpty(criteria.getTenantId())) {
+            query.append(" AND ucad.tenant_id LIKE ? ");
+            preparedStmtList.add("%" + criteria.getTenantId() + "%");
+        }
+
+        if (!ObjectUtils.isEmpty(criteria.getStatus())) {
+            query.append(" AND ucad.application_status = ? ");
+            preparedStmtList.add(criteria.getStatus());
+        }
+
+        if (!ObjectUtils.isEmpty(criteria.getMobileNumber())) {
+            query.append(" AND ucad.applicant_mobile_number = ? ");
+            preparedStmtList.add(criteria.getMobileNumber());
+        }
+
+        return query.toString();
+    }
+
 
     /**
      * Adds WHERE or AND clause based on existing query conditions
