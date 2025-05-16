09a35fef29d7f91821fe40557dfde9ca9c3a6785
Sharma <anubsharma.ext@deloitte.com>
2025-05-16 17:46:11 +0530

    dev for indipendent count api

--- municipal-services/garbage-service/src/main/java/org/egov/garbageservice/model/TotalCountRequest.java
+++ municipal-services/garbage-service/src/main/java/org/egov/garbageservice/model/TotalCountRequest.java
@@ -1,9 +1,19 @@
 package org.egov.garbageservice.model;
 
 import org.egov.common.contract.request.RequestInfo;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TotalCountRequest {
 	
 	@JsonProperty("RequestInfo")
