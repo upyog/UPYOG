b2f9bf4950a60c237fa071b64fb9db06985c62c0
suryapattanayak <bpattanayak@deloitte.com>
2025-05-26 14:54:08 +0530

    Update RedisConfig.java
    changes for redis connection

--- core-services/user-otp/src/main/java/org/egov/web/config/RedisConfig.java
+++ core-services/user-otp/src/main/java/org/egov/web/config/RedisConfig.java
@@ -3,9 +3,12 @@ package org.egov.web.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisShardInfo;
 
 @Configuration
@@ -15,14 +18,34 @@ public class RedisConfig {
     private String host;
 	
 	@Bean
-	public JedisConnectionFactory connectionFactory() {
-		return new JedisConnectionFactory(new JedisShardInfo(host));
-	}
+    public JedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName("localhost");
+        redisConfig.setPort(6379);
+        // redisConfig.setPassword("yourpassword"); // if needed
+
+        JedisClientConfiguration clientConfig = JedisClientConfiguration.builder()
+                .usePooling()
+                .poolConfig(jedisPoolConfig())
+                .build();
+
+        return new JedisConnectionFactory(redisConfig, clientConfig);
+    }
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(1);
+        return poolConfig;
+    }
+     
 
 	@Bean
     public RedisTemplate<String, Long> redisTemplate() {
         RedisTemplate<String, Long> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory());
+        template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
 
