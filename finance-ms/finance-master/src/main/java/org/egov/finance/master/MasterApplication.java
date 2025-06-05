c542574bee025379c37fc069d2e607536626f7c0
suryapattanayak <bpattanayak@deloitte.com>
2025-06-05 10:16:55 +0530

    audit config changes

--- finance-ms/finance-master/src/main/java/org/egov/finance/master/MasterApplication.java
+++ finance-ms/finance-master/src/main/java/org/egov/finance/master/MasterApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @SpringBootApplication
 public class MasterApplication {
 
--- finance-ms/finance-master/src/main/java/org/egov/finance/master/util/AuditConfig.java
+++ finance-ms/finance-master/src/main/java/org/egov/finance/master/util/AuditConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AuditConfig {
 
     @Bean
