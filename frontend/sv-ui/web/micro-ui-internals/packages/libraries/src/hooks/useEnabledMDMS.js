26a77010e1504836c0097c330c3b9eb888e83b87
ShivankShuklaa <Shivank@niua.org>
2025-06-11 13:20:07 +0530

    added comments

--- frontend/sv-ui/web/micro-ui-internals/packages/libraries/src/hooks/useEnabledMDMS.js
+++ frontend/sv-ui/web/micro-ui-internals/packages/libraries/src/hooks/useEnabledMDMS.js
@@ -23,7 +23,7 @@ import { useQuery } from "react-query";
  */
 
 const useEnabledMDMS = (tenantId, moduleName, masterDetails = [], config = {}) => {
-    return useQuery([tenantId, moduleName, masterDetails], () => Digit.Hooks.useSelectedMDMS().getMultipleTypesWithFilter(tenantId, moduleName, masterDetails), config);
+    return useQuery([tenantId, moduleName, masterDetails], () => Digit.Hooks.useSelectedMDMS(moduleName).getMultipleTypesWithFilter(tenantId, moduleName, masterDetails), config);
 };
 
 
