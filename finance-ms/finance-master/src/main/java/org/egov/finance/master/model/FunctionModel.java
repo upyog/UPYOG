1486f1b2ca17961e9435d7fa8d9cfc1ae97f4f20
bdhal1989 <bdhal@deloitte.com>
2025-06-16 16:07:24 +0530

    Updaetd Builder

--- finance-ms/finance-master/src/main/java/org/egov/finance/master/model/FunctionModel.java
+++ finance-ms/finance-master/src/main/java/org/egov/finance/master/model/FunctionModel.java
@@ -10,11 +10,15 @@ import java.util.Date;
 import org.egov.finance.master.customannotation.SafeHtml;
 import org.hibernate.validator.constraints.Length;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FunctionModel {
 	
     private Long id;
