7810fda89f449b19b88df3a64cffe55e871d0f7c
ShivankShuklaa <Shivank@niua.org>
2025-06-16 11:08:40 +0530

    tl issue temporary fix

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/index.js
@@ -350,11 +350,8 @@ import useCreateDemand from "./sv/useCreateDemand";
 import useEmpvendorCommonSearch from "./vendor/useEmpvendorCommonSearch";
 import useFilterEmployee from "./pgrAi/useFilterEmployee";
 import useApplicationDetails from "./pgrAi/useApplicationDetails";
-<<<<<<< Updated upstream
 import useWorkerSearch from "./fsm/useWorkerSearch";
-=======
 import useTreePruningCreateAPI from "./wt/useTreePruningCreateAPI";
->>>>>>> Stashed changes
 
 const wt={
   useTankerCreateAPI,
