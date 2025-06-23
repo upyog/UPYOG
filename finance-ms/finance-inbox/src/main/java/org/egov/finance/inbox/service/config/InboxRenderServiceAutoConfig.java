3cf66ea29902989b9df8265795bd3afcd1309d77
bdhal1989 <bdhal@deloitte.com>
2025-06-23 18:15:36 +0530

    update for inbox

--- finance-ms/finance-inbox/src/main/java/org/egov/finance/inbox/service/config/InboxRenderServiceAutoConfig.java
+++ finance-ms/finance-inbox/src/main/java/org/egov/finance/inbox/service/config/InboxRenderServiceAutoConfig.java
@@ -1,10 +1,6 @@
-/**
- * Created on Jun 23, 2025.
- * 
- * @author bdhal
- */
 package org.egov.finance.inbox.service.config;
 
+import java.util.List;
 
 import org.egov.finance.inbox.entity.Budget;
 import org.egov.finance.inbox.entity.BudgetDetail;
@@ -12,46 +8,48 @@ import org.egov.finance.inbox.entity.BudgetReAppropriationMisc;
 import org.egov.finance.inbox.entity.CVoucherHeader;
 import org.egov.finance.inbox.entity.EgBillregister;
 import org.egov.finance.inbox.service.DefaultInboxRenderServiceImpl;
-import org.egov.finance.inbox.service.InboxRenderService;
-import org.egov.finance.inbox.workflow.entity.*;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.context.annotation.Bean;
+import org.egov.finance.inbox.workflow.entity.StateAware;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Role;
 
-import java.util.List;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Configuration
-public class InboxRenderServiceAutoConfig {
+public class InboxRenderServiceAutoConfig {@PersistenceContext
+    private EntityManager entityManager;
 
-    @Bean
-    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-    public static org.springframework.beans.factory.config.BeanFactoryPostProcessor dynamicInboxRenderRegistration() {
-        return beanFactory -> {
-            DefaultListableBeanFactory registry = (DefaultListableBeanFactory) beanFactory;
-            
-            //Lster additon 
-           // Challan.class,
-           // ContraJournalVoucher.class,
-           // DishonorCheque.class, ReceiptHeader.class,ReceiptVoucher.class,
-//          //EmployeeGrievance.class,
-            //   Paymentheader.class,
-           
-            List<Class<? extends StateAware>> types = List.of(
+    private final ApplicationContext applicationContext;
+
+    public InboxRenderServiceAutoConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @PostConstruct
+    public void registerInboxRenderServices() {
+        List<Class<? extends StateAware>> types = List.of(
                 Budget.class,
                 BudgetDetail.class,
                 BudgetReAppropriationMisc.class,
                 CVoucherHeader.class,
                 EgBillregister.class
-            );
+                // Add more as needed
+        );
 
-            for (Class<? extends StateAware> clazz : types) {
-                String beanName = clazz.getSimpleName() + "InboxRenderService";
-                InboxRenderService<?> service = new DefaultInboxRenderServiceImpl<>(clazz);
-                registry.registerSingleton(beanName, service);
-            }
-        };
+        BeanDefinitionRegistry registry = (BeanDefinitionRegistry) applicationContext.getAutowireCapableBeanFactory();
+
+        for (Class<? extends StateAware> clazz : types) {
+            String beanName = clazz.getSimpleName() + "InboxRenderService";
+
+            GenericBeanDefinition definition = new GenericBeanDefinition();
+            definition.setBeanClass(DefaultInboxRenderServiceImpl.class);
+            definition.getConstructorArgumentValues().addIndexedArgumentValue(0, clazz);
+            definition.getConstructorArgumentValues().addIndexedArgumentValue(1, entityManager);
+            definition.setAutowireCandidate(true);
+            registry.registerBeanDefinition(beanName, definition);
+        }
     }
-
 }
