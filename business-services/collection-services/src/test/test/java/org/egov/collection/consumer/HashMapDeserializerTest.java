38549d0dea355ce3f6a3009e96ffa237551fd5c6
UPYOG <112922579+niua-upyog@users.noreply.github.com>
2025-07-15 11:16:11 +0530

    Update HashMapDeserializerTest.java
    Signed-off-by: UPYOG <112922579+niua-upyog@users.noreply.github.com>

--- business-services/collection-services/src/test/test/java/org/egov/collection/consumer/HashMapDeserializerTest.java
+++ business-services/collection-services/src/test/test/java/org/egov/collection/consumer/HashMapDeserializerTest.java
@@ -1,25 +1,53 @@
+/*
+ * eGov suite of products aim to improve the internal efficiency,transparency,
+ * accountability and the service delivery of the government  organizations.
+ *
+ *  Copyright (C) 2016  eGovernments Foundation
+ *
+ *  The updated version of eGov suite of products as by eGovernments Foundation
+ *  is available at http://www.egovernments.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see http://www.gnu.org/licenses/ or
+ *  http://www.gnu.org/licenses/gpl.html .
+ *
+ *  In addition to the terms of the GPL license to be adhered to in using this
+ *  program, the following additional terms are to be complied with:
+ *
+ *      1) All versions of this program, verbatim or modified must carry this
+ *         Legal Notice.
+ *
+ *      2) Any misrepresentation of the origin of the material is prohibited. It
+ *         is required that all modified versions of this material be marked in
+ *         reasonable ways as different from the original version.
+ *
+ *      3) This license does not grant any rights to any user of the program
+ *         with regards to rights under trademark law for use of the trade names
+ *         or trademarks of eGovernments Foundation.
+ *
+ *  In case of any queries, you can reach eGovernments Foundation at contact@egovernments.org.
+ */
+
 package org.egov.collection.consumer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
-import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper;
+import java.util.HashMap;
 
-class HashMapDeserializerTest {
-    /**
-     * Method under test: default or parameterless constructor of {@link HashMapDeserializer}
-     */
-    @Test
-    void testConstructor() {
-        Jackson2JavaTypeMapper typeMapper = (new HashMapDeserializer()).getTypeMapper();
-        assertTrue(typeMapper instanceof DefaultJackson2JavaTypeMapper);
-        assertEquals("__TypeId__", ((DefaultJackson2JavaTypeMapper) typeMapper).getClassIdFieldName());
-        assertEquals(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID, typeMapper.getTypePrecedence());
-        assertEquals("__KeyTypeId__", ((DefaultJackson2JavaTypeMapper) typeMapper).getKeyClassIdFieldName());
-        assertTrue(((DefaultJackson2JavaTypeMapper) typeMapper).getIdClassMapping().isEmpty());
-        assertEquals("__ContentTypeId__", ((DefaultJackson2JavaTypeMapper) typeMapper).getContentClassIdFieldName());
+public class HashMapDeserializer extends JsonDeserializer<HashMap> {
+
+    public HashMapDeserializer() {
+        super(HashMap.class);
     }
-}
 
+}
