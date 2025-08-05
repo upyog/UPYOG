e7464182386021994d7663782012730359b6bb4e
Sharma <anubsharma.ext@deloitte.com>
2025-08-05 17:03:39 +0530

    removing logs from pg service

--- core-services/egov-pg-service/src/main/java/org/egov/pg/repository/TransactionRepository.java
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/repository/TransactionRepository.java
@@ -27,7 +27,6 @@ public class TransactionRepository {
     public List<Transaction> fetchTransactions(TransactionCriteria transactionCriteria) {
         List<Object> params = new ArrayList<>();
         String query = TransactionQueryBuilder.getPaymentSearchQueryByCreatedTimeRange(transactionCriteria, params);
-        log.info("Query: " + query);
         return jdbcTemplate.query(query, params.toArray(), rowMapper);
     }
     
