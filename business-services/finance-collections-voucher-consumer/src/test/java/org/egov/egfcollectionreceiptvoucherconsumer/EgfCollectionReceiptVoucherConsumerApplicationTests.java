c30616014e573eb4298f685dabbc046fd4251ff1
sourabh7yy <sourabh7y@gmail.com>
2025-08-06 14:26:05 +0530

    Enhance README with detailed service overview, features, and local testing instructions; add SpringExtension for improved test context

--- business-services/finance-collections-voucher-consumer/src/test/java/org/egov/egfcollectionreceiptvoucherconsumer/EgfCollectionReceiptVoucherConsumerApplicationTests.java
+++ business-services/finance-collections-voucher-consumer/src/test/java/org/egov/egfcollectionreceiptvoucherconsumer/EgfCollectionReceiptVoucherConsumerApplicationTests.java
@@ -2,8 +2,11 @@ package org.egov.egfcollectionreceiptvoucherconsumer;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class EgfCollectionReceiptVoucherConsumerApplicationTests {
 
