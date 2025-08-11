8a7dde02a19e70529b4f7255063e9540f749a10a
Nikhil5922 <nikhilkumar7800@gmail.com>
2025-08-11 16:30:21 +0530

    simplify document filtering logic in ADS and CHB search hooks

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbDocumentSearch.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbDocumentSearch.js
@@ -6,11 +6,10 @@ const useChbDocumentSearch = ({ application }, config = {}, Code, index) => {
   const tenant = Digit.ULBService.getStateId();
   const bookingId = application?.bookingId;
   let newDocs = [];
-  config?.value?.documents ? config?.value?.documents?.documents.filter(doc => doc?.documentType === Code /* || doc?.documentType?.includes(Code.split(".")[1]) */).map((ob)=>{
+  const documents = config?.value?.documents?.documents || config?.value;
+  documents?.filter(doc => doc?.documentType === Code).forEach((ob) => {
     newDocs.push(ob);
-  }) : config?.value.filter(doc => doc?.documentType === Code/* || doc?.documentType?.includes(Code.split(".")[1]) */).map((ob)=>{
-    newDocs.push(ob);
-  })
+  });
   const filesArray = newDocs.map((value) => value?.fileStoreId);
   const { isLoading, error, data } = useQuery([`chbDocuments-${bookingId}`, filesArray], () => Digit.UploadServices.Filefetch(filesArray, tenant));
   return { isLoading, error, data: { pdfFiles: data?.data }, revalidate: () => client.invalidateQueries([`chbDocuments-${bookingId}`, filesArray]) };
