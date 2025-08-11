3edbaafe7d87ca870c6223a851557a41cdafcedd
ABHISHEK  RANA <121286986+abhirana04@users.noreply.github.com>
2025-08-11 22:13:30 +0530

    Update DemandService.java

--- municipal-services/sw-calculator/src/main/java/org/egov/swcalculation/service/DemandService.java
+++ municipal-services/sw-calculator/src/main/java/org/egov/swcalculation/service/DemandService.java
@@ -1301,7 +1301,7 @@ public class DemandService {
 						calculationCriteriaList.clear();
 						connectionNosCount=0;
 						if(threadSleepCount == 3) {
-							Thread.sleep(15000);
+						    Thread.sleep(1000); // Sleep for 1 second
 							threadSleepCount=0;
 						}
 						threadSleepCount++;
