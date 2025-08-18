37ee10eecc22d629d7893281f6bf56ce628f845a
Nikhil5922 <nikhilkumar7800@gmail.com>
2025-08-18 23:32:18 +0530

    Fix ADS & CHB document hooks by removing redundant code and updating MDMS calls from v1 to v2.

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbDocumentsMDMS.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/chb/useChbDocumentsMDMS.js
@@ -22,21 +22,10 @@ import { useQuery } from "react-query";
 const useChbDocumentsMDMS = (tenantId, moduleCode, type, config = {}) => {
 
   const useChbDocumentsRequiredScreen = () => {
-    return useQuery("CHB_DOCUMENT_REQ_SCREEN", () => Digit.Hooks.useSelectedMDMS().getMasterData(tenantId, moduleCode, "Documents"), config);
+    return useQuery("CHB_DOCUMENT_REQ_SCREEN", () => Digit.Hooks.useSelectedMDMS(moduleCode).getMasterData(tenantId, moduleCode, type), config);
   };
 
-  const _default = () => {
-    return useQuery([tenantId, moduleCode, type], () => Digit.Hooks.useSelectedMDMS().getMultipleTypes(tenantId, moduleCode, type), config);
-  };
-
-  switch (type) {
-    
-    case "Documents":
-      return useChbDocumentsRequiredScreen();
-    
-    default:
-      return _default();
-  }
+  return useChbDocumentsRequiredScreen();
 };
 
 export default useChbDocumentsMDMS;
