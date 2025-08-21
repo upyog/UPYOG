29a6b280fd166ded0085f6df25297bf072584168
manviraheja1 <manvi.raheja@pwc.com>
2025-08-21 14:22:04 +0530

    "LTS land service"

--- municipal-services/land-services/src/main/java/org/egov/land/web/models/UserInfo.java
+++ municipal-services/land-services/src/main/java/org/egov/land/web/models/UserInfo.java
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
  */
 @ApiModel(description = "This is acting ID token of the authenticated user on the server. Any value provided by the clients will be ignored and actual user based on authtoken will be used on the server.")
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:54:07.373Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:54:07.373Z[GMT]")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
