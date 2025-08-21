98066231274b31a831f376de12a73df2cf95273c
manviraheja1 <manvi.raheja@pwc.com>
2025-08-21 11:57:29 +0530

    "LTS BPA service and calculator"

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/web/models/landinfo/Workflow.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/web/models/landinfo/Workflow.java
@@ -19,7 +19,7 @@ import io.swagger.annotations.ApiModelProperty;
  */
 @ApiModel(description = "BPA application object to capture the details of land, land owners, and address of the land.")
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:54:07.373Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:54:07.373Z[GMT]")
 public class Workflow   {
   @JsonProperty("action")
   private String action = null;
