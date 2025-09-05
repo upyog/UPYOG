ea064b05ecdfb7e554f98f554a6fc671eb9a1079
roshan2611 <>
2025-09-05 12:16:09 +0530

    Added calculation condition for BPA_LOW sanction fee

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/services/CalculationService.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/services/CalculationService.java
@@ -179,7 +179,9 @@ public class CalculationService {
 		estimates.add(estimate);
 		}
 		
-		else if (calulationCriteria.getFeeType().equalsIgnoreCase(BPACalculatorConstants.MDMS_CALCULATIONTYPE_SANC_FEETYPE) && calulationCriteria.getBpa().getBusinessService().equalsIgnoreCase("BPA"))
+		else if (calulationCriteria.getFeeType().equalsIgnoreCase(BPACalculatorConstants.MDMS_CALCULATIONTYPE_SANC_FEETYPE) 
+				&& ( calulationCriteria.getBpa().getBusinessService().equalsIgnoreCase(BPACalculatorConstants.MDMS_BPA) 
+						|| calulationCriteria.getBpa().getBusinessService().equalsIgnoreCase(BPACalculatorConstants.MDMS_BPA_LOW) ) )
 		{	
 			@SuppressWarnings("unchecked")
 			Map<String,Map<String,String>> node=(Map<String, Map<String,String>>)calulationCriteria.getBpa().getAdditionalDetails();
