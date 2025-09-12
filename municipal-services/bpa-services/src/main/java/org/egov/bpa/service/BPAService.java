1ea61198406c2c8b7649661393736f44dab02895
roshan2611 <>
2025-09-12 19:59:52 +0530

    Update BPA create and update APIs

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/service/BPAService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/service/BPAService.java
@@ -128,13 +128,13 @@ public class BPAService {
 		String applicationType = values.get(BPAConstants.APPLICATIONTYPE);
 		this.validateCreateOC(applicationType, values, requestInfo, bpaRequest);
 		bpaValidator.validateCreate(bpaRequest, mdmsData, values);
-		if (!applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN_OC)) {
+		if (!applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN_OC) && bpaRequest.getBPA().getLandInfo() != null) {
 			landService.addLandInfoToBPA(bpaRequest);
 		}
 		enrichmentService.enrichBPACreateRequest(bpaRequest, mdmsData, values);
 		wfIntegrator.callWorkFlow(bpaRequest);
 		//nocService.createNocRequest(bpaRequest, mdmsData);
-		this.addCalculation(applicationType, bpaRequest);
+//		this.addCalculation(applicationType, bpaRequest);
 		repository.save(bpaRequest);
 		return bpaRequest.getBPA();
 	}
@@ -390,7 +390,7 @@ public class BPAService {
 		log.debug("applicationType is " + applicationType);
 		BusinessService businessService = workflowService.getBusinessService(bpa, bpaRequest.getRequestInfo(),
 				bpa.getApplicationNo());
-
+		
 		List<BPA> searchResult = getBPAWithBPAId(bpaRequest);
 		if (CollectionUtils.isEmpty(searchResult) || searchResult.size() > 1) {
 			throw new CustomException(BPAErrorConstants.UPDATE_ERROR, "Failed to Update the Application, Found None or multiple applications!");
@@ -417,6 +417,17 @@ public class BPAService {
 		this.handleRejectSendBackActions(applicationType, bpaRequest, businessService, searchResult, mdmsData, edcrResponse);
                 String state = workflowService.getCurrentState(bpa.getStatus(), businessService);
                 String businessSrvc = businessService.getBusinessService();
+
+                /*
+                 * Before Citizen approval we need to create Application fee demand
+                 */
+                // Generate the Application Demand
+                if ((businessSrvc.equalsIgnoreCase(BPAConstants.BPA_OC_MODULE_CODE)
+                        || businessSrvc.equalsIgnoreCase(BPAConstants.BPA_BUSINESSSERVICE)
+                        || businessSrvc.equalsIgnoreCase(BPAConstants.BPA_LOW_MODULE_CODE))
+                        && state.equalsIgnoreCase(BPAConstants.STATUS_CITIZENAPPROVAL)) {
+                    calculationService.addCalculation(bpaRequest, BPAConstants.APPLICATION_FEE_KEY);
+                }
                 
                 /*
                  * Before approving the application we need to check sanction fee is applicable
@@ -490,8 +501,11 @@ public class BPAService {
 				actionValidator.validateUpdateRequest(bpaRequest, businessService);
 				bpaValidator.validateUpdate(bpaRequest, searchResult, mdmsData,
 				workflowService.getCurrentState(bpa.getStatus(), businessService), edcrResponse);
-				if (!applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN_OC)) {
-					landService.updateLandInfo(bpaRequest);
+				if (!applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN_OC) && bpa.getLandInfo() != null) {
+					if(bpa.getLandId() == null)
+						landService.addLandInfoToBPA(bpaRequest);
+					else
+						landService.updateLandInfo(bpaRequest);
 				}
 				bpaValidator.validateCheckList(mdmsData, bpaRequest,
 				workflowService.getCurrentState(bpa.getStatus(), businessService));
