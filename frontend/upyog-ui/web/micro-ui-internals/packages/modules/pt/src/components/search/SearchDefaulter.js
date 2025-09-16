fcea3f42d695480e1721e909707c3d6aa95c7238
Srirranjan Srivastava <sriranjan.srivastava@pwc.com>
2025-09-16 08:13:42 +0530

    Added PT localisation fix

--- frontend/upyog-ui/web/micro-ui-internals/packages/modules/pt/src/components/search/SearchDefaulter.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/modules/pt/src/components/search/SearchDefaulter.js
@@ -133,7 +133,7 @@ const { isLoading, error, data, isSuccess } = useQuery(["propertySearchList", te
       },
       {
         Header: t("PT_PROPERTY_TYPE"),
-        Cell: ({ row }) => GetCell(row?.original?.propertyType?`${row?.original?.propertyType}`:t("PT_NA")),
+        Cell: ({ row }) => GetCell(t(row?.original?.propertyType?`${row?.original?.propertyType}`:t("PT_NA"))),
         disableSortBy: true,
       }
     ],
@@ -166,7 +166,7 @@ const { isLoading, error, data, isSuccess } = useQuery(["propertySearchList", te
             <div>
               {Digit.Utils.didEmployeeHasRole("PT_CEMP") ? (
                 <span className="link"> 
-                  <a  style={{textDecoration:'none'}} onClick={() => downloadNotice(row.original)}>{t("ES_PT_COLLECT_TAX")}</a>
+                  <a  style={{textDecoration:'none'}} onClick={() => downloadNotice(row.original)}>{t("ES_PT_GENERATE_TAX")}</a>
                 </span>
               ) : null}
             </div>
