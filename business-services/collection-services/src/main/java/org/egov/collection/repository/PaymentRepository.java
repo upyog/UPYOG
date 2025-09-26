008b19d3bb97f346dc8de60a8fb3aa9441e835f0
priyanshu-egov <priyanshu.vaish@egovernments.org>
2025-09-26 11:25:23 +0530

    add the tenantId check in the get bill plain search

--- business-services/collection-services/src/main/java/org/egov/collection/repository/PaymentRepository.java
+++ business-services/collection-services/src/main/java/org/egov/collection/repository/PaymentRepository.java
@@ -182,8 +182,11 @@ public class PaymentRepository {
     	Map<String, Bill> mapOfIdAndBills = new HashMap<>();
         Map<String, Object> preparedStatementValues = new HashMap<>();
         preparedStatementValues.put("id", ids);
-        preparedStatementValues.put("tenantId", tenantId);
         StringBuilder query = new StringBuilder(paymentQueryBuilder.getBillQuery());
+        if (tenantId!=null) {
+            preparedStatementValues.put("tenantId", tenantId);
+            query.append(" AND	b.tenantid= :tenantId ;");
+        }
         query.append(" AND	b.tenantid= :tenantId ;");
         List<Bill> bills = namedParameterJdbcTemplate.query(query.toString(), preparedStatementValues, billRowMapper);
         bills.forEach(bill -> {
