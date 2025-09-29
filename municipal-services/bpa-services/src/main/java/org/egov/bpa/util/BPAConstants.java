e0537a54807f93e1a4832148e5e2bf4b2bfc7aaa
roshan2611 <56333820+roshan2611@users.noreply.github.com>
2025-09-29 16:07:26 +0530

    Update maximum building height for BPA_LOW

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/util/BPAConstants.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/util/BPAConstants.java
@@ -400,7 +400,7 @@ public class BPAConstants {
 
 	public static final String PAYMENT_LINK_PLACEHOLDER = "{PAYMENT_LINK}";
 	
-	public static final Double MAAX_BUILDING_HEIGHT = 21.0;
+	public static final Double MAAX_BUILDING_HEIGHT = 15.01;
 
 
 }
