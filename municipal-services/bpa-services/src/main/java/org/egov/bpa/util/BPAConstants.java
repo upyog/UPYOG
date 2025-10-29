e17f75a9de783271c2425c29502ea030e7f7a0f2
Roshan Chaudhary <roshanchoudhary60@gmail.com>
2025-10-30 01:21:06 +0530

    Update to get Maximum building height from mdms

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/util/BPAConstants.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/util/BPAConstants.java
@@ -401,6 +401,8 @@ public class BPAConstants {
 	public static final String PAYMENT_LINK_PLACEHOLDER = "{PAYMENT_LINK}";
 	
 	public static final Double MAAX_BUILDING_HEIGHT = 15.01;
+	
+	public static final String BUILDING_HEIGHT = "BuildingHeight";
 
 
 }
