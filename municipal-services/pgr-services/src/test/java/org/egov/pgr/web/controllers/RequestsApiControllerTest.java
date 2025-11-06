3c386cadf7dab6cceaf768f39a111aefe2843fcd
prasunpwc <prasun.naiya@pwc.com>
2025-11-06 20:56:28 +0530

    pgr-service build issue fix

--- municipal-services/pgr-services/src/test/java/org/egov/pgr/web/controllers/RequestsApiControllerTest.java
+++ municipal-services/pgr-services/src/test/java/org/egov/pgr/web/controllers/RequestsApiControllerTest.java
@@ -1,15 +1,14 @@
 package org.egov.pgr.web.controllers;
 
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.egov.pgr.TestConfiguration;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 * API tests for RequestsApiController
 */
 @Disabled
-@RunWith(SpringRunner.class)
+@SpringBootTest
 @WebMvcTest(RequestsApiController.class)
 @Import(TestConfiguration.class)
 public class RequestsApiControllerTest {
