67e44d349c5cc23803617160b78298cf8e9cf63d
jashan7167 <jashansingh9890@gmail.com>
2025-11-25 11:55:49 +0530

    added mobilenumber search for challan

--- municipal-services/inbox/src/main/java/org/egov/inbox/service/ChallanInboxFilterService.java
+++ municipal-services/inbox/src/main/java/org/egov/inbox/service/ChallanInboxFilterService.java
@@ -1,7 +1,10 @@
 package org.egov.inbox.service;
 
 import com.jayway.jsonpath.JsonPath;
+import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.common.contract.request.Role;
+import org.egov.inbox.repository.ServiceRequestRepository;
 import org.egov.inbox.web.model.InboxSearchCriteria;
 import org.egov.inbox.web.model.workflow.ProcessInstanceSearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +15,23 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-import static org.egov.inbox.util.NdcConstants.*;
+import static org.egov.inbox.util.BpaConstants.*;
+import static org.egov.inbox.util.BpaConstants.CITIZEN;
 import static org.egov.inbox.util.TLConstants.STATUS_PARAM;
 
+
+@Slf4j
 @Service
 public class ChallanInboxFilterService {
+
+    @Value("${egov.user.host}")
+    private String userHost;
+
+    @Value("${egov.user.search.path}")
+    private String userSearchEndpoint;
 
     @Value("${egov.searcher.host}")
     private String searcherHost;
@@ -35,6 +48,8 @@ public class ChallanInboxFilterService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private ServiceRequestRepository serviceRequestRepository;
     /**
      * Fetch application numbers from searcher
      */
@@ -45,16 +60,40 @@ public class ChallanInboxFilterService {
         HashMap<String, Object> moduleSearchCriteria = criteria.getModuleSearchCriteria();
         ProcessInstanceSearchCriteria processCriteria = criteria.getProcessSearchCriteria();
 
+
+        Boolean isSearchResultEmpty = false;
+        Boolean isMobileNumberPresent = false;
+        List<String> userUUIDs = new ArrayList<>();
+        List<String> citizenRoles = Collections.emptyList();
+        if (moduleSearchCriteria.containsKey(MOBILE_NUMBER_PARAM)) {
+            isMobileNumberPresent = true;
+        }
+        if (isMobileNumberPresent) {
+            String tenantId = criteria.getTenantId();
+            String mobileNumber = String.valueOf(moduleSearchCriteria.get(MOBILE_NUMBER_PARAM));
+            Map<String, List<String>> userDetails = fetchUserUUID(mobileNumber, requestInfo, tenantId);
+            userUUIDs = userDetails.get(USER_UUID);
+            citizenRoles = userDetails.get(USER_ROLES);
+            Boolean isUserPresentForGivenMobileNumber = CollectionUtils.isEmpty(userUUIDs) ? false : true;
+            isSearchResultEmpty = !isMobileNumberPresent || !isUserPresentForGivenMobileNumber;
+            if (isSearchResultEmpty) {
+                return new ArrayList<>();
+            }
+        } else {
+            List<String> roles = requestInfo.getUserInfo().getRoles().stream().map(Role::getCode).collect(Collectors.toList());
+            if(roles.contains(CITIZEN)) {
+                userUUIDs.add(requestInfo.getUserInfo().getUuid());
+                citizenRoles = roles;
+            }
+        }
         Map<String, Object> searcherRequest = new HashMap<>();
         Map<String, Object> searchCriteria = getSearchCriteria(criteria, statusIdNameMap,
-                moduleSearchCriteria, processCriteria);
-
+                moduleSearchCriteria, processCriteria,userUUIDs,citizenRoles);
         // Pagination
         searchCriteria.put(OFFSET_PARAM, criteria.getOffset());
 
-        if (!moduleSearchCriteria.containsKey(MOBILE_NUMBER_PARAM)) {
-            searchCriteria.put(NO_OF_RECORDS_PARAM, criteria.getLimit());
-        }
+        searchCriteria.put(NO_OF_RECORDS_PARAM, criteria.getLimit());
+
 
         moduleSearchCriteria.put(LIMIT_PARAM, criteria.getLimit());
 
@@ -102,7 +141,7 @@ public class ChallanInboxFilterService {
     private Map<String, Object> getSearchCriteria(InboxSearchCriteria criteria,
                                                   HashMap<String, String> statusIdNameMap,
                                                   HashMap<String, Object> moduleSearchCriteria,
-                                                  ProcessInstanceSearchCriteria processCriteria) {
+                                                  ProcessInstanceSearchCriteria processCriteria, List<String> userUUIDs, List<String> userRoles) {
 
         Map<String, Object> searchCriteria = new HashMap<>();
 
@@ -160,9 +199,9 @@ public class ChallanInboxFilterService {
             searchCriteria.put(LOCALITY_PARAM, moduleSearchCriteria.get(LOCALITY_PARAM));
         }
 
-        // Citizen UUID
-        if (moduleSearchCriteria != null && moduleSearchCriteria.containsKey("citizenUuid")) {
-            searchCriteria.put("citizenUuid", moduleSearchCriteria.get("citizenUuid"));
+        // MobileNumber
+        if (moduleSearchCriteria != null && moduleSearchCriteria.containsKey(MOBILE_NUMBER_PARAM)) {
+            searchCriteria.put("citizenUuid", userUUIDs);
         }
 
         // Assignee from workflow
@@ -199,9 +238,35 @@ public class ChallanInboxFilterService {
             HashMap<String, Object> moduleSearchCriteria = criteria.getModuleSearchCriteria();
             ProcessInstanceSearchCriteria processCriteria = criteria.getProcessSearchCriteria();
 
+            Boolean isSearchResultEmpty = false;
+            Boolean isMobileNumberPresent = false;
+            List<String> userUUIDs = new ArrayList<>();
+            List<String> citizenRoles = Collections.emptyList();
+            if (moduleSearchCriteria.containsKey(MOBILE_NUMBER_PARAM)) {
+                isMobileNumberPresent = true;
+            }
+            if (isMobileNumberPresent) {
+                String tenantId = criteria.getTenantId();
+                String mobileNumber = String.valueOf(moduleSearchCriteria.get(MOBILE_NUMBER_PARAM));
+                Map<String, List<String>> userDetails = fetchUserUUID(mobileNumber, requestInfo, tenantId);
+                userUUIDs = userDetails.get(USER_UUID);
+                citizenRoles = userDetails.get(USER_ROLES);
+                Boolean isUserPresentForGivenMobileNumber = CollectionUtils.isEmpty(userUUIDs) ? false : true;
+                isSearchResultEmpty = !isMobileNumberPresent || !isUserPresentForGivenMobileNumber;
+                if (isSearchResultEmpty) {
+                    return 0;
+                }
+            } else {
+                List<String> roles = requestInfo.getUserInfo().getRoles().stream().map(Role::getCode).collect(Collectors.toList());
+                if(roles.contains(CITIZEN)) {
+                    userUUIDs.add(requestInfo.getUserInfo().getUuid());
+                    citizenRoles = roles;
+                }
+            }
             Map<String, Object> searcherRequest = new HashMap<>();
             Map<String, Object> searchCriteria = getSearchCriteria(criteria, statusIdNameMap,
-                    moduleSearchCriteria, processCriteria);
+                    moduleSearchCriteria, processCriteria,userUUIDs, citizenRoles);
+
 
             searcherRequest.put(REQUESTINFO_PARAM, requestInfo);
             searcherRequest.put(SEARCH_CRITERIA_PARAM, searchCriteria);
@@ -220,5 +285,30 @@ public class ChallanInboxFilterService {
             List<String> apps = fetchApplicationNumbersFromSearcher(criteria, statusIdNameMap, requestInfo);
             return apps.size();
         }
+    }
+
+    private Map<String, List<String>> fetchUserUUID(String mobileNumber, RequestInfo requestInfo, String tenantId) {
+        Map<String, List<String>> userDetails = new ConcurrentHashMap<>();
+        StringBuilder uri = new StringBuilder();
+        uri.append(userHost).append(userSearchEndpoint);
+        Map<String, Object> userSearchRequest = new HashMap<>();
+        userSearchRequest.put("RequestInfo", requestInfo);
+        userSearchRequest.put("tenantId", tenantId);
+        userSearchRequest.put("userType", CITIZEN);
+        userSearchRequest.put("mobileNumber", mobileNumber);
+        try {
+            Object user = serviceRequestRepository.fetchResult(uri, userSearchRequest);
+            if (null != user) {
+                // log.info(user.toString());
+                userDetails.put(USER_UUID, JsonPath.read(user, "$.user.*.uuid"));
+                userDetails.put(USER_ROLES, new ArrayList<>(new HashSet<>(JsonPath.read(user, "$.user.*.roles.*.code"))));
+            } else {
+                log.error("Service returned null while fetching user for mobile number - " + mobileNumber);
+            }
+        } catch (Exception e) {
+            log.error("Exception while fetching user for mobile number - " + mobileNumber);
+            log.error("Exception trace: ", e);
+        }
+        return userDetails;
     }
 }
