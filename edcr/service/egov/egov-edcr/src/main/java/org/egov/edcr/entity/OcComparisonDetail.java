400be71a56a7f088aaf727e89b8ca11026001c31
deepikaarora88 <deepika.arora@pwc.com>
2025-12-12 19:33:48 +0530

    merge conflicts

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/entity/OcComparisonDetail.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/entity/OcComparisonDetail.java
@@ -170,9 +170,4 @@ public class OcComparisonDetail extends AbstractAuditable {
     public void setPermitDcr(EdcrApplicationDetail permitDcr) {
         this.permitDcr = permitDcr;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> master-LTS
-
 }
