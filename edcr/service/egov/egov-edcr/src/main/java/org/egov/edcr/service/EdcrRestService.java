94aa6c8499cda6cde444451c7497932a64ee95a9
deepikaarora88 <deepika.arora@pwc.com>
2025-12-12 19:54:37 +0530

    merge conflicts

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrRestService.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/service/EdcrRestService.java
@@ -893,7 +893,7 @@ public class EdcrRestService {
             return new ErrorDetail(BPA_07, REQ_BODY_REQUIRED);
         else if (edcrRequest.getRequestInfo().getUserInfo() == null
                 || (edcrRequest.getRequestInfo().getUserInfo() != null
-                        && (isBlank(edcrRequest.getRequestInfo().getUserInfo().getUuid()) || isBlank(edcrRequest.getRequestInfo().getUserInfo().getId()) )))
+                        && isBlank(edcrRequest.getRequestInfo().getUserInfo().getId()) ))
             return new ErrorDetail(BPA_07, USER_ID_IS_MANDATORY);
 
         if (isBlank(edcrRequest.getTransactionNumber()))
