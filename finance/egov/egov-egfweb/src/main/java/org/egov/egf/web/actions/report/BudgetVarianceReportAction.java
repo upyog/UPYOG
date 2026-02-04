3568c4e29cf4b2a7c7e5b4a510958b6c589193b4
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2026-02-04 22:43:13 +0530

    Fix formatting in BudgetVarianceReportAction

--- finance/egov/egov-egfweb/src/main/java/org/egov/egf/web/actions/report/BudgetVarianceReportAction.java
+++ finance/egov/egov-egfweb/src/main/java/org/egov/egf/web/actions/report/BudgetVarianceReportAction.java
@@ -453,11 +453,11 @@ public class BudgetVarianceReportAction extends BaseFormAction {
             setQueryParams();
             final Map.Entry<String, Map<String, Object>> vimsQueryMapEntry = formMiscQuery("vmis", "gl", "vh").entrySet().iterator().next();
             final List<Object[]> resultForVoucher = budgetDetailService.fetchActualsForFYWithParams(fromDate, 
-                    + Constants.DDMMYYYYFORMAT2.format(asOnDate), new StringBuffer(vimsQueryMapEntry.getKey()), vimsQueryMapEntry.getValue());
+                    Constants.DDMMYYYYFORMAT2.format(asOnDate), new StringBuffer(vimsQueryMapEntry.getKey()), vimsQueryMapEntry.getValue());
             extractData(resultForVoucher);
             final Map.Entry<String, Map<String, Object>> bmisQueryMapEntry = formMiscQuery("bmis", "bdetail", "bmis").entrySet().iterator().next();
             final List<Object[]> resultForBill = budgetDetailService.fetchActualsForBillWithVouchersParams(fromDate,
-                    + Constants.DDMMYYYYFORMAT2.format(asOnDate) , new StringBuffer(bmisQueryMapEntry.getKey()), bmisQueryMapEntry.getValue());
+                    Constants.DDMMYYYYFORMAT2.format(asOnDate) , new StringBuffer(bmisQueryMapEntry.getKey()), bmisQueryMapEntry.getValue());
             extractData(resultForBill);
         } else {
             addActionError("no data found");
