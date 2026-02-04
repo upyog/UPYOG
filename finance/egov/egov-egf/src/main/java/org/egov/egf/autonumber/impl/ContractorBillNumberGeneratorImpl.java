0d5fe71ecc50cab6ba3fc91c7473746cccac9768
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2026-02-04 22:12:14 +0530

    Refactor contractor bill number format

--- finance/egov/egov-egf/src/main/java/org/egov/egf/autonumber/impl/ContractorBillNumberGeneratorImpl.java
+++ finance/egov/egov-egf/src/main/java/org/egov/egf/autonumber/impl/ContractorBillNumberGeneratorImpl.java
@@ -86,8 +86,7 @@ public class ContractorBillNumberGeneratorImpl implements ContractorBillNumberGe
             throw new ValidationException(FinancialConstants.EMPTY_STRING, "Financial Year is not defined for the voucher date" );
         sequenceName = "seq_contractor_billnumber_" + financialYear.getFinYearRange();
         Serializable nextSequence = genericSequenceNumberGenerator.getNextSequence(sequenceName);
-        contractorBillNumber = String.format("%s/%s/%04d/%s", br.getEgBillregistermis().getDepartmentcode(), "CJV", nextSequence,
-                financialYear.getFinYearRange());
+        contractorBillNumber = String.format("%s/%s/%s/%04d",financialYear.getFinYearRange(), br.getEgBillregistermis().getDepartmentName(), br.getEgBillregistermis().getFunction().getCode(),nextSequence);
         return contractorBillNumber;
     }
 }
