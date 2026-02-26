596c8baab448f0c0940b2ef6b4c02ae815fcca96
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2026-02-26 16:51:28 +0530

    misc receipts changes

---
+++ finance/egov/egov-collection/src/main/java/org/egov/collection/bean/WardNumber.java
@@ -0,0 +1,27 @@
+package org.egov.collection.bean;
+
+public class WardNumber {
+	
+    private String wardNoCode;
+	
+	private String wardNoName;
+
+	public String getWardNoCode() {
+		return wardNoCode;
+	}
+
+	public void setWardNoCode(String wardNoCode) {
+		this.wardNoCode = wardNoCode;
+	}
+
+	public String getWardNoName() {
+		return wardNoName;
+	}
+
+	public void setWardNoName(String wardNoName) {
+		this.wardNoName = wardNoName;
+	}
+	
+	
+
+}
