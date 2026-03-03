d3c8129a4440f6dfd74645946af451b1c02827db
Deepa <deepa.o.sd@pwc.com>
2026-03-03 10:46:44 +0000

    url changes on core services application properties

--- core-services/pdf-service/src/EnvironmentVariables.js
+++ core-services/pdf-service/src/EnvironmentVariables.js
@@ -1,12 +1,12 @@
 const envVariables = {
   MAX_NUMBER_PAGES: process.env.MAX_NUMBER_PAGES || 80,
-  EGOV_HOST: process.env.EGOV_HOST || "https://upyog-test.niua.org",
+  EGOV_HOST: process.env.EGOV_HOST || "https://upyog-test.niua.org/",
   EGOV_LOCALISATION_HOST:
-    process.env.EGOV_LOCALISATION_HOST || "http://egov-localization:8080",
+    process.env.EGOV_LOCALISATION_HOST || "http://egov-localization:8080/",
   EGOV_LOCALISATION_SEARCH:
-    process.env.EGOV_LOCALISATION_SEARCH || "/localization/messages/v2/_search",
+    process.env.EGOV_LOCALISATION_SEARCH || "localization/messages/v2/_search",
   EGOV_FILESTORE_SERVICE_HOST:
-    process.env.EGOV_FILESTORE_SERVICE_HOST || "http://egov-filestore:8080",
+    process.env.EGOV_FILESTORE_SERVICE_HOST || "http://egov-filestore:8080/",
   SERVER_PORT: process.env.SERVER_PORT || 8080,
 
   KAFKA_BROKER_HOST: process.env.KAFKA_BROKER_HOST || "localhost:9092",
