444a042c60cc1b566ab74b2edb9dd2c750c60dec
prasunpwc <prasun.naiya@pwc.com>
2026-03-12 16:47:40 +0530

    Add RestClient bean

---
+++ core-services/egov-user/src/main/java/org/egov/user/config/RestClientConfig.java
@@ -0,0 +1,15 @@
+package org.egov.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+	
+	@Bean
+	public RestClient restClient() {
+	    return RestClient.builder().build();
+	}
+
+}
