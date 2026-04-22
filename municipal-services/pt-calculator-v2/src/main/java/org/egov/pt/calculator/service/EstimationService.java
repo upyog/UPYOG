62441a37f35205bafd6cc07ed90988911dc9f6c4
manjotkaurPMIDC <manjotkaur4@kpmg.com>
2026-04-22 16:05:55 +0530

    Update EstimationService.java

--- municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/EstimationService.java
+++ municipal-services/pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/EstimationService.java
@@ -2194,13 +2194,12 @@ if(collectedAmtForOldDemand.compareTo(BigDecimal.ZERO) > 0)
 			double totalTax = tax_payable_roundoff;
 
 			taxHeadEstimates.add(buildTaxHead("PT_TAX", PT_TAX));
-			//taxHeadEstimates.add(buildTaxHead("PT_OWNER_EXEMPTION", exemption));
 			taxHeadEstimates.add(buildTaxHead("PT_OWNER_EXEMPTION",  -Math.abs(exemption)));
-			taxHeadEstimates.add(buildTaxHead("PT_UNIT_USAGE_EXEMPTION", unit_usage_exemption));
+			taxHeadEstimates.add(buildTaxHead("PT_UNIT_USAGE_EXEMPTION", -Math.abs(unit_usage_exemption)));
 			taxHeadEstimates.add(buildTaxHead("PT_FIRE_CESS", FireCess));
 			taxHeadEstimates.add(buildTaxHead("PT_TIME_PENALTY", penality));
 			taxHeadEstimates.add(buildTaxHead("PT_CANCER_CESS", 0.0));
-			taxHeadEstimates.add(buildTaxHead("PT_TIME_REBATE", additional_rebate));
+			taxHeadEstimates.add(buildTaxHead("PT_TIME_REBATE", -Math.abs(additional_rebate)));
 			taxHeadEstimates.add(buildTaxHead("PT_TIME_INTEREST", totalInterest));
 			taxHeadEstimates.add(buildTaxHead("PT_ROUNDOFF", round_off));
 
