843034f90abcf5fdf0b8d7aa3f257d489c3493d8
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2026-04-29 18:31:15 +0530

    Receipt Report Bean

---
+++ finance/egov/egov-collection/src/main/java/org/egov/collection/bean/ReceiptReportBean.java
@@ -0,0 +1,180 @@
+
+
+package org.egov.collection.bean;
+
+import java.math.BigDecimal;
+
+public class ReceiptReportBean {
+	private String slNo;
+	private String paramDate;
+	private String receiptNo;
+	private String collectedBy;
+	private String payeeName;
+	private String payeeAddress;
+	private String serviceType;
+	private String modeOfPayment;
+	private String particulars;
+	private BigDecimal totalReceiptAmount;
+	private BigDecimal principalAmt;
+	private BigDecimal gstAmount;
+	private String dateOfDeposite;
+	private String remitanceNo;
+	private String bankAccountNo;
+	private BigDecimal depositAmount;
+	private String gstNo;
+	private String status;
+	private String department;
+	private String serviceCategory;
+	private String chequeddno;
+	private String chequedddate;
+	private String bank;
+	private String remittanceReceiptNo;
+	public String getSlNo() {
+		return slNo;
+	}
+	public void setSlNo(String slNo) {
+		this.slNo = slNo;
+	}
+	public String getParamDate() {
+		return paramDate;
+	}
+	public void setParamDate(String paramDate) {
+		this.paramDate = paramDate;
+	}
+	public String getReceiptNo() {
+		return receiptNo;
+	}
+	public void setReceiptNo(String receiptNo) {
+		this.receiptNo = receiptNo;
+	}
+	public String getCollectedBy() {
+		return collectedBy;
+	}
+	public void setCollectedBy(String collectedBy) {
+		this.collectedBy = collectedBy;
+	}
+	public String getPayeeName() {
+		return payeeName;
+	}
+	public void setPayeeName(String payeeName) {
+		this.payeeName = payeeName;
+	}
+	public String getPayeeAddress() {
+		return payeeAddress;
+	}
+	public void setPayeeAddress(String payeeAddress) {
+		this.payeeAddress = payeeAddress;
+	}
+	public String getServiceType() {
+		return serviceType;
+	}
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+	public String getModeOfPayment() {
+		return modeOfPayment;
+	}
+	public void setModeOfPayment(String modeOfPayment) {
+		this.modeOfPayment = modeOfPayment;
+	}
+	public String getParticulars() {
+		return particulars;
+	}
+	public void setParticulars(String particulars) {
+		this.particulars = particulars;
+	}
+	public BigDecimal getTotalReceiptAmount() {
+		return totalReceiptAmount;
+	}
+	public void setTotalReceiptAmount(BigDecimal totalReceiptAmount) {
+		this.totalReceiptAmount = totalReceiptAmount;
+	}
+	public BigDecimal getPrincipalAmt() {
+		return principalAmt;
+	}
+	public void setPrincipalAmt(BigDecimal principalAmt) {
+		this.principalAmt = principalAmt;
+	}
+	public BigDecimal getGstAmount() {
+		return gstAmount;
+	}
+	public void setGstAmount(BigDecimal gstAmount) {
+		this.gstAmount = gstAmount;
+	}
+	public String getDateOfDeposite() {
+		return dateOfDeposite;
+	}
+	public void setDateOfDeposite(String dateOfDeposite) {
+		this.dateOfDeposite = dateOfDeposite;
+	}
+	public String getRemitanceNo() {
+		return remitanceNo;
+	}
+	public void setRemitanceNo(String remitanceNo) {
+		this.remitanceNo = remitanceNo;
+	}
+	public String getBankAccountNo() {
+		return bankAccountNo;
+	}
+	public void setBankAccountNo(String bankAccountNo) {
+		this.bankAccountNo = bankAccountNo;
+	}
+	public BigDecimal getDepositAmount() {
+		return depositAmount;
+	}
+	public void setDepositAmount(BigDecimal depositAmount) {
+		this.depositAmount = depositAmount;
+	}
+	public String getGstNo() {
+		return gstNo;
+	}
+	public void setGstNo(String gstNo) {
+		this.gstNo = gstNo;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getServiceCategory() {
+		return serviceCategory;
+	}
+	public void setServiceCategory(String serviceCategory) {
+		this.serviceCategory = serviceCategory;
+	}
+	public String getChequeddno() {
+		return chequeddno;
+	}
+	public void setChequeddno(String chequeddno) {
+		this.chequeddno = chequeddno;
+	}
+	public String getChequedddate() {
+		return chequedddate;
+	}
+	public void setChequedddate(String chequedddate) {
+		this.chequedddate = chequedddate;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getRemittanceReceiptNo() {
+		return remittanceReceiptNo;
+	}
+	public void setRemittanceReceiptNo(String remittanceReceiptNo) {
+		this.remittanceReceiptNo = remittanceReceiptNo;
+	}
+	
+	
+	
+
+}
