304b04e13f7d624b6e7b4dd29119009461cf2d60
Ajay Kumar Bairwa <122883638+ajaybairwa7877@users.noreply.github.com>
2026-05-19 11:00:26 +0530

    Change the error message for Rear abutting road

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/feature/SetBackService.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/feature/SetBackService.java
@@ -243,8 +243,10 @@ public class SetBackService extends FeatureProcess {
 		            for (Block block : pl.getBlocks()) {
 		                for (SetBack setback : block.getSetBacks()) {
 		                    if (setback.getRearYard() == null) {
-		                        errors.put("rearyardNodeDefined","Rear Setback of " + block.getName() 
-		                                        + " is mandatory when rear road width is present.");
+//		                        errors.put("rearyardNodeDefined","Rear Setback of " + block.getName() 
+//		                                        + " is mandatory when rear road width is present.");
+		                    	errors.put("rearyardNodeDefined",
+		                    	"Rear abutting road present. Kindly provide mandatory rear setback for Block " + block.getNumber());
 		                        pl.addErrors(errors);
 		                    }
 		                }
