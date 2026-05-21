11b38b2b9629ad72506958384e66cbf7678034a4
ashutoshpatnaik12 <111061945+ashutoshpatnaik12@users.noreply.github.com>
2026-05-21 13:16:44 +0530

    Update ActuatorSecurityConfig.java

--- core-services/egov-user/src/main/java/org/egov/user/config/ActuatorSecurityConfig.java
+++ core-services/egov-user/src/main/java/org/egov/user/config/ActuatorSecurityConfig.java
@@ -3,6 +3,7 @@ package org.egov.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ActuatorSecurityConfig {
 
     @Bean
-    @Order(1)
+    @Order(0)
     public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http
@@ -18,7 +19,8 @@ public class ActuatorSecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             )
-            .csrf(csrf -> csrf.disable());
+            .csrf(csrf -> csrf.disable())
+            .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
