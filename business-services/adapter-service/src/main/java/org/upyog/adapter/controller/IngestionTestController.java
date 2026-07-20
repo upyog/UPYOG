4020de8395f4aa38095a00dded4c93e4ce5eb97b
goutam-niua <goutam@niua.org>
2026-07-20 16:35:04 +0530

    integration test case added

--- business-services/adapter-service/src/main/java/org/upyog/adapter/controller/IngestionTestController.java
+++ business-services/adapter-service/src/main/java/org/upyog/adapter/controller/IngestionTestController.java
@@ -1,12 +1,15 @@
 package org.upyog.adapter.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.upyog.adapter.model.IngestionResult;
 import org.upyog.adapter.service.DailyIngestionService;
@@ -23,12 +26,17 @@ public class IngestionTestController {
 
     /**
      * Manually triggers multi-module metrics extraction and ingestion.
+     * Option to pass a date parameter to execute extraction, transformation, and ingestion for a specific date.
      * 
+     * @param date optional target date (YYYY-MM-DD) for single-date ingestion
      * @return ResponseEntity containing list of IngestionResult payloads and HTTP 200 OK
      */
     @GetMapping
-    public ResponseEntity<List<IngestionResult>> pushData() {
-        List<IngestionResult> results = service.ingestDailyData();
+    public ResponseEntity<List<IngestionResult>> pushData(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        List<IngestionResult> results = (date != null)
+                ? service.ingestDailyData(date)
+                : service.ingestDailyData();
         return new ResponseEntity<>(results, HttpStatus.OK);
     }
 }
