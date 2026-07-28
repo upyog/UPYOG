7e9f56657b50356aa28d31580aab74aad4bca353
Hema <hema.passi@niua.org>
2026-07-28 13:42:59 +0530

    todo added default admin values will change later - as per discussion

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/useLocalities.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/useLocalities.js
@@ -2,7 +2,9 @@ import { queryTemplate } from "../common/queryTemplate";
 import { getLocalities } from "../services/molecules/getLocalities";
 import { LocalityService } from "../services/elements/Localities";
 
-const useLocalities = (tenant, boundaryType = "revenue", config, t) => {
+//TODO : as per discusion with team, we will change the default value which is admin from boundaryType from useLocalities hook from pgr and use this hook instead. This is because this hook is generic and can be used in any module. The useLocalities hook in pgr is specific to pgr module and cannot be used in other modules. Hence we are creating a generic useLocalities hook which can be used in any module.
+
+const useLocalities = (tenant, boundaryType = "admin", config, t) => {
   boundaryType = boundaryType.toLocaleLowerCase();
   return queryTemplate({
     queryKey: ["BOUNDARY_DATA", tenant, boundaryType],
