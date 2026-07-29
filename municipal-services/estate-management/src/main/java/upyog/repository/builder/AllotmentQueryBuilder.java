8507d2b140a722f1f77abe1184b1693d4dd91e5f
Nikhil5922 <nikhilkumar7800@gmail.com>
2026-07-29 16:33:11 +0530

    added additionalDetails in allotment search

--- municipal-services/estate-management/src/main/java/upyog/repository/builder/AllotmentQueryBuilder.java
+++ municipal-services/estate-management/src/main/java/upyog/repository/builder/AllotmentQueryBuilder.java
@@ -17,7 +17,7 @@ public class AllotmentQueryBuilder {
             "duration, rate, monthly_rent, advance_payment, allotment_date, " +
             "advance_payment_date, eoffice_file_no, asset_reference_no, property_type, " +
             "citizen_request_letter, allotment_letter, signed_deed, billing_cycle, status, " +
-            "createdby, lastmodifiedby, createdtime, lastmodifiedtime " +
+            "createdby, lastmodifiedby, createdtime, lastmodifiedtime, additional_details " +
             "FROM ug_em_allotment_details";
     
     /**
