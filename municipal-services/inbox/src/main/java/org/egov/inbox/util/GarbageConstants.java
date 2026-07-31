c17949ec64e8bd0b13e4bfddaae41b682bbbf797
Nikhil5922 <nikhilkumar7800@gmail.com>
2026-07-31 17:03:22 +0530

    added Status filter in GcInbox

--- municipal-services/inbox/src/main/java/org/egov/inbox/util/GarbageConstants.java
+++ municipal-services/inbox/src/main/java/org/egov/inbox/util/GarbageConstants.java
@@ -4,5 +4,6 @@ public class GarbageConstants extends CommonConstants {
 
     public static final String GARBAGE_SERVICE = "garbage-service";
     public static final String GC_APPLICATION_NUMBER_PARAM = "applicationNumber";
+    public static final String APPLICATION_STATUS = "applicationStatus";
 
 }
