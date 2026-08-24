2c163302b0f5c40defb0255c183b18f788ffcc21
Kunal <kunalgaurav1420@gmail.com>
2026-08-24 11:18:23 +0530

    fix: add missing Spring Service annotation to PaymentService class

--- finance/egov/egov-egf/src/main/java/org/egov/services/payment/PaymentService.java
+++ finance/egov/egov-egf/src/main/java/org/egov/services/payment/PaymentService.java
@@ -133,6 +133,7 @@ import org.hibernate.type.StandardBasicTypes;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.exilant.GLEngine.ChartOfAccounts;
